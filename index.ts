50a7d8c824356b74e15a0ef6bba4643a4e521f4c
larshp <larshp@hotmail.com>
2015-12-23 08:35:17 +0000

    testing

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
+/// <reference path="typings/node/node.d.ts" />
+
+import Lexer from "./src/lexer";
+
 export default class Calculator {
     add(x: number, y: number): number {
         return x + y;
     }
 }
+
+console.log("hello world");
+let argv = require("minimist")(process.argv.slice(2));
+console.dir(argv);
+console.log(argv._[0]);
+
+let lexer: Lexer;
+lexer = new Lexer();
+lexer.foo();
---
+++ src/lexer.ts
@@ -0,0 +1,5 @@
+export default class Lexer {
+    foo() {
+        console.log("foo");
+    }
+}
