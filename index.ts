692a19a5662098c163bec5a322e53f25ee02f4d1
larshp <larshp@hotmail.com>
2015-12-25 14:39:40 +0000

    exit with error

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import Lexer from "./src/lexer";
 import Parser from "./src/parser";
 import Report from "./src/report";
 import Check01 from "./src/check01";
-// import * as glob from "glob";
 
 let fs = require("fs");
 let glob = require("glob");
@@ -20,6 +19,7 @@ function process_file(filename: string) {
 
     if (report.get_count() > 0) {
         console.error("abap-open-checks\t" + filename + "\t" + report.get_count() + " error");
+        process.exit(1);
     } else {
         console.log("abap-open-checks\t" + filename + "\tno errors");
     }
@@ -29,7 +29,6 @@ let argv = require("minimist")(process.argv.slice(2));
 if (argv._[0] === undefined) {
     console.log("Supply filename");
 } else {
-//    const files = ;
     for (const file of argv._) {
         glob.sync(file).forEach(process_file);
     }
