aaef5436c575c7176e6ad9513c0fd4fbc16384da
larshp <larshp@hotmail.com>
2016-01-05 16:44:27 +0000

    hmm

--- src/statements/data.ts
+++ src/statements/data.ts
@@ -3,10 +3,43 @@ import { Token } from "../tokens/tokens";
 
 export class Data extends Statement {
 
+    constructor(tokens: Array<Token>, private name: string) {
+        super(tokens);
+    }
+
     public static match(tokens: Array<Token>): Statement {
         let str = Statement.concat(tokens).toUpperCase();
         if (/^(CLASS-)?DATA /.test(str)) {
-            return new Data(tokens);
+            return new Data(tokens, "foo");
+/*
+    str.start().match("CLASS-DATA").match("DATA");
+    if(!str.matched()) {
+        return undefined;
+    }
+    str.match_var();
+    str.start().match("TYPE").match("LIKE").match("LIKE LINE OF").match("TYPE REF TO");
+    if(str.matched()) {
+        str.match_type();
+    } else {
+        str.start().match("TYPE TABLE OF").match("TYPE STANDARD TABLE OF");
+    }
+
+
+            let foo = /^(CLASS-)?DATA /.exec(str);
+            console.dir(foo);
+            console.dir(str.substr(foo[0].length));
+
+            let match = /^(CLASS-)?DATA (\w+) (TYPE|LIKE|LIKE LINE OF|TYPE REF TO) ([\w=>-]+)[.,]$/.exec(str);
+            if (!!match) {
+                return new Data(tokens, "foo");
+            }
+            match = /^(CLASS-)?DATA (\w+) (TYPE TABLE OF|TYPE STANDARD TABLE OF) ([\w=>-]+)[.,]$/.exec(str);
+            if (!!match) {
+                return new Data(tokens, "foo");
+            }
+
+            console.dir(str);
+*/
         }
         return undefined;
     }
--- test/abap/zno_unknown.prog.abap
+++ test/abap/zno_unknown.prog.abap
@@ -5,3 +5,9 @@ WRITE: 'foobar'.
 WRITE: 'foobar', 'bar'.
 
 DATA lv_foo TYPE i.
+DATA lv_foo LIKE LINE OF foo.
+DATA lv_foo LIKE lv_foo.
+DATA lv_foo TYPE REF TO cl_foobar.
+DATA lv_foo TYPE TABLE OF i.
+DATA lv_foo TYPE zcl_class=>typedef.
+DATA lv_foo LIKE sy-tabix.
