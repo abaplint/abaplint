e9279e5f396f6cb7d4aa4f13249e278b4abbc713
larshp <larshp@hotmail.com>
2016-05-11 07:32:31 +0000

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-// todo, testing
-export * from "./file";
-export * from "./runner";
+import File from "./file";
+import Runner from "./runner";
+import Issue from "./issue";
+
+class Linter {
+  public static run(filename: string, contents: string): Issue[] {
+    let file = new File(filename, contents);
+    Runner.run([file]);
+    return file.get_issues();
+  }
+}
+
+namespace Linter {}
+export = Linter;
