3ed4bf29fcbb82336e42d951c3d3c4052b19c713
Lars Hvam <larshp@hotmail.com>
2016-07-24 16:07:57 +0000

    parse CASE and WHEN

--- src/statements/case.ts
+++ src/statements/case.ts
@@ -1,14 +1,23 @@
 import { Statement } from "./statement";
 import { Token } from "../tokens/";
+import * as Combi from "../combi";
+import Reuse from "./reuse";
+
+let str = Combi.str;
+let seq = Combi.seq;
 
 export class Case extends Statement {
 
-    public static match(tokens: Array<Token>): Statement {
-        let str = Statement.concat(tokens).toUpperCase();
-        if (/^CASE /.test(str)) {
-            return new Case(tokens);
-        }
-        return undefined;
+  public static get_matcher(): Combi.IRunnable {
+    return seq(str("CASE"), Reuse.source());
+  }
+
+  public static match(tokens: Array<Token>): Statement {
+    let result = Combi.Combi.run(this.get_matcher( ), tokens, true);
+    if (result === true) {
+      return new Case(tokens);
     }
+    return undefined;
+  }
 
 }
--- src/statements/when.ts
+++ src/statements/when.ts
@@ -1,14 +1,24 @@
 import { Statement } from "./statement";
 import { Token } from "../tokens/";
+import * as Combi from "../combi";
+import Reuse from "./reuse";
+
+let str = Combi.str;
+let seq = Combi.seq;
+let star = Combi.star;
 
 export class When extends Statement {
 
-    public static match(tokens: Array<Token>): Statement {
-        let str = Statement.concat(tokens).toUpperCase();
-        if (/^WHEN /.test(str)) {
-            return new When(tokens);
-        }
-        return undefined;
+  public static get_matcher(): Combi.IRunnable {
+    return seq(str("WHEN"), Reuse.source(), star(seq(str("OR"), Reuse.source())));
+  }
+
+  public static match(tokens: Array<Token>): Statement {
+    let result = Combi.Combi.run(this.get_matcher( ), tokens, true);
+    if (result === true) {
+      return new When(tokens);
     }
+    return undefined;
+  }
 
 }
