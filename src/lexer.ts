ba33648771c0cedbc627d4dbc7e538dc23826e3f
Lars Hvam <larshp@hotmail.com>
2016-08-01 15:58:20 +0000

    clean it up

--- src/lexer.ts
+++ src/lexer.ts
@@ -10,20 +10,60 @@ enum Mode {
   Comment
 }
 
+class Buffer {
+  private buf: string;
+
+  public constructor() {
+    this.buf = "";
+  }
+
+  public add(s: string): void {
+    this.buf = this.buf + s;
+  }
+
+  public get(): string {
+    return this.buf;
+  }
+
+  public clear(): void {
+    this.buf = "";
+  }
+}
+
 class Stream {
   private raw: string;
   private offset = -1;
+  private row = 0;
+  private col = 0;
 
   public constructor(raw: string) {
     this.raw = raw;
+    this.row = 0;
+    this.col = 0;
   }
 
   public advance(): boolean {
+    if (this.currentChar() === "\n") {
+      this.col = 1;
+      this.row = this.row + 1;
+    }
+
     if (this.offset === this.raw.length) {
       return false;
     }
+
+    this.col = this.col + 1;
+
     this.offset = this.offset + 1;
     return true;
+  }
+
+  public getCol(): number {
+    return this.col;
+  }
+
+  public getRow(): number {
+    return this.row;
   }
 
   public prevChar(): string {
@@ -50,6 +90,7 @@ export default class Lexer {
   private static tokens: Array<Tokens.Token>;
   private static m: Mode;
   private static stream: Stream;
+  private static buffer: Buffer;
 
   public static run(file: File): Array<Tokens.Token> {
     this.tokens = [];
@@ -60,10 +101,12 @@ export default class Lexer {
     return this.tokens;
   }
 
-  private static add(input: string, row: number, col: number) {
-    let s = input.trim();
+  private static add() {
+    let s = this.buffer.get().trim();
     if (s.length > 0) {
-// console.log("add " + col + " " + s);
+      let col = this.stream.getCol();
+      let row = this.stream.getRow();
+
       let pos = new Position(row, col - s.length);
       if (this.m === Mode.Comment) {
         this.tokens.push(new Tokens.Comment(pos, s));
@@ -91,65 +134,55 @@ export default class Lexer {
         this.tokens.push(new Tokens.Identifier(pos, s));
       }
     }
+    this.buffer.clear();
   }
 
   private static process(raw: string) {
-    let buffer = "";
-    let row = 0;
-    let col = 0;
-
     this.stream = new Stream(raw);
+    this.buffer = new Buffer();
 
     while (true) {
       let current = this.stream.currentChar();
-      buffer = buffer + current;
+      this.buffer.add(current);
+      let buf = this.buffer.get();
       let ahead = this.stream.nextChar();
       let aahead = this.stream.nextNextChar();
       let prev = this.stream.prevChar();
 
-// console.log("\"" + current.trim() + "\"\t\"" + ahead.trim() + "\"\t" + col);
-
       if (ahead === "'" && this.m === Mode.Normal) {
 // start string
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Str;
       } else if (ahead === "|" && this.m === Mode.Normal) {
 // start template
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Template;
       } else if (ahead === "`" && this.m === Mode.Normal) {
 // start ping
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Ping;
       } else if ((ahead === "\"" || (ahead === "*" && current === "\n"))
           && this.m === Mode.Normal) {
 // start comment
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Comment;
 
-      } else if (buffer.length > 1 && current === "`" && this.m === Mode.Ping) {
+      } else if (buf.length > 1 && current === "`" && this.m === Mode.Ping) {
 // end of ping
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Normal;
-      } else if (buffer.length > 1 && current === "|" && this.m === Mode.Template) {
+      } else if (buf.length > 1 && current === "|" && this.m === Mode.Template) {
 // end of template
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Normal;
       } else if (this.m === Mode.Str
-          && buffer.length > 1
+          && buf.length > 1
           && current === "'"
           && aahead !== "''"
-          && (buffer.match(/'/g) || []).length % 2 === 0
-          && (buffer.concat(aahead).match(/'/g) || []).length % 2 === 0) {
+          && (buf.match(/'/g) || []).length % 2 === 0
+          && (buf.concat(aahead).match(/'/g) || []).length % 2 === 0) {
 // end of string
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Normal;
 
       } else if ((ahead === " "
@@ -167,46 +200,35 @@ export default class Lexer {
           || ahead === "\t"
           || ahead === "\n")
           && this.m === Mode.Normal) {
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
       } else if (ahead === "\n" && this.m !== Mode.Template) {
-        this.add(buffer, row, col);
-        buffer = "";
+        this.add();
         this.m = Mode.Normal;
       } else if (current === ">"
           && (prev === "-" || prev === "=" )
           && ahead !== " "
           && this.m === Mode.Normal) {
 // arrows
-        this.add(buffer, row, col);
-        buffer = "";
-      } else if ((buffer === "."
-          || buffer === ","
-          || buffer === ":"
-          || buffer === "("
-          || buffer === ")"
-          || buffer === "["
-          || buffer === "]"
-          || buffer === "+"
-          || ( buffer === "-" && ahead !== ">" ) )
+        this.add();
+      } else if ((buf === "."
+          || buf === ","
+          || buf === ":"
+          || buf === "("
+          || buf === ")"
+          || buf === "["
+          || buf === "]"
+          || buf === "+"
+          || ( buf === "-" && ahead !== ">" ) )
           && this.m === Mode.Normal) {
-        this.add(buffer, row, col);
-        buffer = "";
-      }
-
-      if (current === "\n") {
-        col = 1;
-        row = row + 1;
+        this.add();
       }
 
       if (!this.stream.advance()) {
         break;
       }
-
-      col = col + 1;
     }
 
-    this.add(buffer, row, col - 1);
+    this.add();
   }
 
 }
