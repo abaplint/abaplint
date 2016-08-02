cdaae8cc09a029ca47cd0fafdd4c1f2c0785cac0
Lars Hvam <larshp@hotmail.com>
2016-08-02 16:14:17 +0000

    last one

--- src/statements/selectoption.ts
+++ src/statements/selectoption.ts
@@ -1,14 +1,36 @@
 import { Statement } from "./statement";
 import { Token } from "../tokens/";
+import * as Combi from "../combi";
+import Reuse from "./reuse";
 
-export class Selectoption extends Statement {
+let str = Combi.str;
+let seq = Combi.seq;
+let opt = Combi.opt;
+let alt = Combi.alt;
 
-    public static match(tokens: Array<Token>): Statement {
-        let str = Statement.concat(tokens).toUpperCase();
-        if (/^SELECT-OPTION(S)? /.test(str)) {
-            return new Selectoption(tokens);
-        }
-        return undefined;
+export class SelectOption extends Statement {
+
+  public static get_matcher(): Combi.IRunnable {
+    let def = seq(str("DEFAULT"), alt(Reuse.constant(), Reuse.field_chain()));
+
+    let option = seq(str("OPTION"), Reuse.field());
+
+    let ret = seq(str("SELECT-OPTIONS"),
+                  Reuse.field(),
+                  str("FOR"),
+                  Reuse.field_sub(),
+                  opt(def),
+                  opt(option));
+
+    return ret;
+  }
+
+  public static match(tokens: Array<Token>): Statement {
+    let result = Combi.Combi.run(this.get_matcher(), tokens, true);
+    if (result === true) {
+      return new SelectOption(tokens);
     }
+    return undefined;
+  }
 
 }
---
+++ test/statements/selectoption.ts
@@ -0,0 +1,9 @@
+import {statementType} from "../utils";
+import * as Statements from "../../src/statements/";
+
+let tests = [
+  "SELECT-OPTIONS s_trkorr FOR e070-trkorr.",
+  "SELECT-OPTIONS s_icon FOR icon-name DEFAULT 'foo' OPTION CP.",
+];
+
+statementType(tests, "SELECT-OPTIONS", Statements.SelectOption);
