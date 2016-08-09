90a67eeb64f844634a9a73d72870126c50a34c0f
Lars Hvam <larshp@hotmail.com>
2016-08-09 13:24:25 +0000

    prepare 0.27.1

--- test/statements/assign.ts
+++ test/statements/assign.ts
@@ -1,14 +1,14 @@
 import {statementType} from "../utils";
 import * as Statements from "../../src/statements/";
 
-  let tests = [
-    "ASSIGN COMPONENT ls_field-name OF STRUCTURE ig_file TO <lv_field>.",
-    "ASSIGN ('(SAPLSIFP)TTAB') TO <lg_any>.",
-    "ASSIGN cs_tstcp-param(sy-fdpos) TO <lg_f>.",
-    "ASSIGN cs_tstcp-param(sdf) TO <lg_f>.",
-    "ASSIGN cs_tstcp-param TO <lg_f>.",
-    "assign lt_alv->* to <f_alv_tab>.",
-    "ASSIGN lv_x TO <lv_y> CASTING.",
-    ];
+let tests = [
+  "ASSIGN COMPONENT ls_field-name OF STRUCTURE ig_file TO <lv_field>.",
+  "ASSIGN ('(SAPLSIFP)TTAB') TO <lg_any>.",
+  "ASSIGN cs_tstcp-param(sy-fdpos) TO <lg_f>.",
+  "ASSIGN cs_tstcp-param(sdf) TO <lg_f>.",
+  "ASSIGN cs_tstcp-param TO <lg_f>.",
+  "assign lt_alv->* to <f_alv_tab>.",
+  "ASSIGN lv_x TO <lv_y> CASTING.",
+];
 
 statementType(tests, "ASSIGN", Statements.Assign);
