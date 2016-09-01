4503896bfc70aa4f0e95566c09ab5eb73247406a
larshp <larshp@hotmail.com>
2016-09-01 13:29:15 +0000

    new user interface for syntax diagrams

--- web/script/generate.js
+++ web/script/generate.js
@@ -1,18 +1,21 @@
+"use strict";
+
 var Railroad = require("railroad-diagrams");
 var fs = require("fs");
 
-var statements = "";
-var reuse = "";
-
 function run() {
-  var folder = "./web/viz/";
-  var files = fs.readdirSync(folder);
+  let json = {};
+  json.statements = [];
+  json.reuse = [];
+  
+  let folder = "./web/viz/";
+  let files = fs.readdirSync(folder);
 
-  for (var file of files) {
+  for (let file of files) {
     if (/\.txt$/.test(file)) {
-      var contents = fs.readFileSync(folder + file,"utf8");
+      let contents = fs.readFileSync(folder + file,"utf8");
 
-      var css = "<defs>\n" +
+      let css = "<defs>\n" +
         "<style type=\"text/css\"><![CDATA[\n" +
         "path {\n" +
         "stroke-width: 3;\n" +
@@ -52,44 +55,29 @@ function run() {
         "]]></style>\n" +
         "</defs>\n";
 //      console.log(file);
-      var result = eval(contents);
+      let result = eval(contents);
       result = result.replace(/<svg /, "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ");
       result = result.replace(/<g transform/, css + "<g transform");
-      var target = folder + file.split(".")[0] + ".svg";
+      let target = folder + file.split(".")[0] + ".svg";
       fs.writeFileSync(target, result, "utf8");
 
-      var svg = file.split(".")[0] + ".svg";
-      var html = "<div style=\"float:left;text-align:center;\">" +
-        "<u>" + svg + "</u><br>\n" +
-        "<a href=\"" + svg + "\"><img src=\"" + svg + "\" width=\"300\"></a><br><br>\n" +
-        "</div>\n";
+      let name = file.split(".")[0];
 
-      if (/^reuse_/.test(svg)) {
-        reuse = reuse + html;
+      if (/^reuse_/.test(name)) {
+        json.reuse.push(name);
       } else {
-        statements = statements + html;
+        json.statements.push(name);
       }
     }
   }
+  
+  return json;
 }
 
 function generate() {
-  run();
-
-  var index = "<html>\n" +
-    "<head>\n" +
-    "<title>abaplint syntax diagrams</title>\n" +
-    "<link rel=\"stylesheet\" type=\"text/css\" href=\"../style/style.css\">\n" +
-    "</head>\n" +
-    "<body>\n" +
-    "<h1>Statements</h1>\n";
-  index = index + statements;
-  index = index + "<div style=\"clear:both;\">\n<h1>Reuse</h1>\n";
-  index = index + reuse;
-  index = index + "</div>\n";
-
-  index = index + "</body>\n</html>\n";
-  fs.writeFileSync("./web/viz/index.html", index, "utf8");
+  let json = run();
+  
+  fs.writeFileSync("./web/viz/data.json", JSON.stringify(json), "utf8");
 }
 
 generate();
