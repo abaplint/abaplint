10a56b569da0a72e2d12d51ff8808e1c1fdd5faf
larshp <larshp@hotmail.com>
2016-11-27 12:26:27 +0000

    test typings

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-/*
 import {File} from "./file";
 import Runner from "./runner";
-import Issue from "./issue";
+import {Issue} from "./issue";
 
-// todo, can this file be removed?
+// file used to build typings, index.d.ts
 
-class Linter {
-  public static run(filename: string, contents: string): Issue[] {
-    let file = new File(filename, contents);
-    Runner.run([file]);
-    return file.getIssues();
-  }
-}
-
-namespace Linter {}
-export = Linter;
-*/
+// namespace Linter {}
+export = {File, Issue, Runner};
