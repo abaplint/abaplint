776ef49e6ecc6f90443a762c767a7debeb8bbb11
larshp <larshp@hotmail.com>
2017-01-20 16:53:30 +0000

    minor adjustment

--- src/statements/reuse.ts
+++ src/statements/reuse.ts
@@ -246,7 +246,7 @@ export class FunctionParameters extends Combi.Reuse {
     let importing = seq(str("IMPORTING"), new ParameterListT());
     let changing = seq(str("CHANGING"), new ParameterListT());
     let tables = seq(str("TABLES"), new ParameterListT());
-    let exceptions = seq(str("EXCEPTIONS"), opt(new ParameterListExceptions()), opt(new Field()));
+    let exceptions = seq(str("EXCEPTIONS"), opt(alt(new ParameterListExceptions(), new Field())));
     let long = seq(opt(exporting),
                    opt(importing),
                    opt(tables),
