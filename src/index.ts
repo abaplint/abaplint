5eab6298470711a678971cc8c2e32bcb9f48c601
larshp <larshp@hotmail.com>
2018-09-16 08:24:18 +0000

    export more

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {File} from "./file";
 import Runner from "./runner";
 import {Issue} from "./issue";
+import Config from "./config";
+import {Version} from "./version";
 
 // file used to build typings, index.d.ts
 
 // namespace Linter {}
-export = {File, Issue, Runner};
+export = {File, Issue, Runner, Config, Version};
