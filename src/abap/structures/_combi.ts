9c883b9f60529f8d4900feb67e79cdb7f8b99eb8
larshp <larshp@hotmail.com>
2018-10-13 15:20:40 +0200

    structure parser combinator impl + tests

--- src/abap/structures/_combi.ts
+++ src/abap/structures/_combi.ts
@@ -1,7 +1,15 @@
 import {Structure} from "./_structure";
+import {Statement} from "../statements/statement";
+
+export interface IMatch {
+  matched: Array<Statement>;
+  unmatched: Array<Statement>;
+  error: boolean;
+}
 
 export interface IStructureRunnable {
   toRailroad(): string;
+  run(statements: Array<Statement>): IMatch;
 }
 
 class Sequence implements IStructureRunnable {
@@ -17,6 +25,20 @@ class Sequence implements IStructureRunnable {
   public toRailroad() {
     const children = this.list.map((e) => { return e.toRailroad(); });
     return "Railroad.Sequence(" + children.join() + ")";
+  }
+
+  public run(statements: Array<Statement>): IMatch {
+    let inn = statements;
+    let out: Array<Statement> = [];
+    for (let i of this.list) {
+      let match = i.run(inn);
+      if (match.error) {
+        return {matched: [], unmatched: statements, error: true};
+      }
+      out = out.concat(match.matched);
+      inn = match.unmatched;
+    }
+    return {matched: out, unmatched: inn, error: false};
   }
 }
 
@@ -34,6 +56,16 @@ class Alternative implements IStructureRunnable {
     let children = this.list.map((e) => { return e.toRailroad(); });
     return "Railroad.Choice(0, " + children.join() + ")";
   }
+
+  public run(statements: Array<Statement>): IMatch {
+    for (let i of this.list) {
+      const match = i.run(statements);
+      if (match.error === false) {
+        return match;
+      }
+    }
+    return {matched: [], unmatched: statements, error: true};
+  }
 }
 
 class Optional implements IStructureRunnable {
@@ -45,6 +77,12 @@ class Optional implements IStructureRunnable {
 
   public toRailroad() {
     return "Railroad.Optional(" + this.obj.toRailroad() + ")";
+  }
+
+  public run(statements: Array<Statement>): IMatch {
+    let ret = this.obj.run(statements);
+    ret.error = false;
+    return ret;
   }
 }
 
@@ -61,6 +99,20 @@ class Star implements IStructureRunnable {
   public toRailroad() {
     return "Railroad.ZeroOrMore(" + this.obj.toRailroad() + ")";
   }
+
+  public run(statements: Array<Statement>): IMatch {
+    let inn = statements;
+    let out: Array<Statement> = [];
+    // tslint:disable-next-line:no-constant-condition
+    while (true) {
+      let match = this.obj.run(inn);
+      if (match.error === true || inn.length === 0) {
+        return {matched: out, unmatched: inn, error: false};
+      }
+      out = out.concat(match.matched);
+      inn = match.unmatched;
+    }
+  }
 }
 
 class SubStructure implements IStructureRunnable {
@@ -74,9 +126,16 @@ class SubStructure implements IStructureRunnable {
     return "Railroad.NonTerminal('" + this.s.constructor.name + "', 'structure_" + this.s.constructor.name + ".svg')";
   }
 
+  public run(statements: Array<Statement>): IMatch {
+    let ret = this.s.getMatcher().run(statements);
+    if (ret.matched.length === 0) {
+      ret.error = true;
+    }
+    return ret;
+  }
 }
 
-class Statement implements IStructureRunnable {
+class SubStatement implements IStructureRunnable {
   private obj: any;
 
   constructor(obj: any) {
@@ -91,22 +150,14 @@ class Statement implements IStructureRunnable {
     return (cla + "").match(/\w+/g)[1];
   }
 
-}
-
-class BeginEnd implements IStructureRunnable {
-  private begin: IStructureRunnable;
-  private end: IStructureRunnable;
-  private body: IStructureRunnable;
-
-  constructor(begin: IStructureRunnable, body: IStructureRunnable, end: IStructureRunnable) {
-    this.begin = begin;
-    this.body = body;
-    this.end = end;
-  }
-
-  public toRailroad() {
-    let children = [this.begin.toRailroad(), this.body.toRailroad(), this.end.toRailroad()];
-    return "Railroad.Sequence(" + children + ")";
+  public run(statements: Array<Statement>): IMatch {
+    if (statements.length === 0) {
+      return {matched: [], unmatched: [], error: true};
+    } else if (statements[0] instanceof this.obj) {
+      return {matched: [statements[0]], unmatched: statements.splice(1), error: false};
+    } else {
+      return {matched: [], unmatched: statements, error: true};
+    }
   }
 }
 
@@ -118,8 +169,9 @@ export function alt(first: IStructureRunnable, ...rest: IStructureRunnable[]): I
   return new Alternative([first].concat(rest));
 }
 
+// todo, is this obsolete, just use Sequence instead?
 export function beginEnd(begin: IStructureRunnable, body: IStructureRunnable, end: IStructureRunnable): IStructureRunnable {
-  return new BeginEnd(begin, body, end);
+  return new Sequence([begin, body, end]);
 }
 
 export function opt(o: IStructureRunnable): IStructureRunnable {
@@ -131,7 +183,7 @@ export function star(s: IStructureRunnable): IStructureRunnable {
 }
 
 export function sta(s: Object): IStructureRunnable {
-  return new Statement(s);
+  return new SubStatement(s);
 }
 
 export function sub(s: Structure): IStructureRunnable {
---
+++ test/abap/structures/_combi.ts
@@ -0,0 +1,214 @@
+import {expect} from "chai";
+import {opt, sta, star, seq, alt, sub, beginEnd} from "../../../src/abap/structures/_combi";
+import * as Statements from "../../../src/abap/statements";
+import * as Structures from "../../../src/abap/structures";
+
+describe("structure combi statement", function() {
+  let sta1 = sta(Statements.Move);
+
+  it("sta1 match", function() {
+    let match = sta1.run([new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("sta1 not match", function() {
+    let match = sta1.run([new Statements.Do()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(true);
+  });
+
+  it("sta1 multi", function() {
+    let match = sta1.run([new Statements.Move(), new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("sta1 none", function() {
+    let match = sta1.run([]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(true);
+  });
+});
+
+describe("structure combi opt", function() {
+  let opt1 = opt(sta(Statements.Move));
+
+  it("opt1 match", function() {
+    let match = opt1.run([new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("opt1 not match", function() {
+    let match = opt1.run([new Statements.Do()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("opt1 multi", function() {
+    let match = opt1.run([new Statements.Move(), new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("opt1 none", function() {
+    let match = opt1.run([]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+});
+
+describe("structure combi star", function() {
+  let star1 = star(sta(Statements.Move));
+
+  it("star1 match", function() {
+    let match = star1.run([new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("star1 not match", function() {
+    let match = star1.run([new Statements.Do()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("star1 multi1", function() {
+    let match = star1.run([new Statements.Move(), new Statements.Move()]);
+    expect(match.matched.length).to.equal(2);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("star1 multi2", function() {
+    let match = star1.run([new Statements.Move(), new Statements.Move(), new Statements.Do()]);
+    expect(match.matched.length).to.equal(2);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("star1 none", function() {
+    let match = star1.run([]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+});
+
+describe("structure combi seq", function() {
+  let seq1 = seq(sta(Statements.Move), sta(Statements.Do));
+
+  it("seq1 match", function() {
+    let match = seq1.run([new Statements.Move(), new Statements.Do()]);
+    expect(match.matched.length).to.equal(2);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("seq1 not match", function() {
+    let match = seq1.run([new Statements.Do()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(true);
+  });
+
+  it("seq1 multi2", function() {
+    let match = seq1.run([new Statements.Move(), new Statements.Do(), new Statements.Do()]);
+    expect(match.matched.length).to.equal(2);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("seq1 none", function() {
+    let match = seq1.run([]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(true);
+  });
+});
+
+describe("structure combi alt", function() {
+  let alt1 = alt(sta(Statements.Move), sta(Statements.Do));
+
+  it("alt1 match1", function() {
+    let match = alt1.run([new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("alt1 match2", function() {
+    let match = alt1.run([new Statements.Do()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("alt1 not match", function() {
+    let match = alt1.run([new Statements.Call()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(true);
+  });
+
+  it("alt1 multi2", function() {
+    let match = alt1.run([new Statements.Move(), new Statements.Do()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(false);
+  });
+
+  it("alt1 none", function() {
+    let match = alt1.run([]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(true);
+  });
+});
+
+describe("structure combi sub structure", function() {
+  let sub1 = sub(new Structures.Normal());
+
+  it("sub1 match", function() {
+    let match = sub1.run([new Statements.Move()]);
+    expect(match.matched.length).to.equal(1);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("sub1 no match", function() {
+    let match = sub1.run([new Statements.ClassDefinition()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(true);
+  });
+});
+
+describe("structure combi beginEnd", function() {
+  let sub1 = beginEnd(sta(Statements.Do), sta(Statements.EndDo), sta(Statements.EndDo));
+
+  it("beginEnd, match", function() {
+    let match = sub1.run([new Statements.Do(), new Statements.EndDo(), new Statements.EndDo()]);
+    expect(match.matched.length).to.equal(3);
+    expect(match.unmatched.length).to.equal(0);
+    expect(match.error).to.equal(false);
+  });
+
+  it("beginEnd, no match", function() {
+    let match = sub1.run([new Statements.ClassDefinition()]);
+    expect(match.matched.length).to.equal(0);
+    expect(match.unmatched.length).to.equal(1);
+    expect(match.error).to.equal(true);
+  });
+});
