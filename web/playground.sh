659b44e173c648a10c6188ab793ac07227df4725
larshp <larshp@hotmail.com>
2018-10-30 13:51:36 +0000

    use codemirror from CDN

--- web/playground.sh
+++ web/playground.sh
@@ -2,8 +2,4 @@
 set -x #echo on
 cd "${0%/*}"
 cp ../build/bundle.js playground/script/bundle.js
-cp ../node_modules/codemirror/lib/codemirror.js playground/script/
-cp ../node_modules/codemirror/addon/selection/mark-selection.js playground/script/
-cp ../node_modules/codemirror/lib/codemirror.css playground/style/
-cp ../node_modules/codemirror/theme/mbo.css playground/style/
 cp ../node_modules/codemirror-abap/build/abap.js playground/script/
