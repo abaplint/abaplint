5bcc8ba1c8f72e2a15a5a8a00ed732cb449f77a4
larshp <larshp@hotmail.com>
2018-11-01 07:06:16 +0100

    syntax: generate all in single json file

--- src/abap/graphs.ts
+++ src/abap/graphs.ts
@@ -4,38 +4,59 @@ import {Artifacts} from "./artifacts";
 
 // todo, move this method to somewhere under web/syntax?
 
-function className(cla: any): string {
-  return (cla.constructor + "").match(/\w+/g)[1];
+function sort(data: string[]): string[] {
+  let unique = data.filter((v, i, a) => { return a.indexOf(v) === i; });
+  return unique.sort();
+}
+
+interface IData {
+  name: string;
+  railroad: string;
+  type: string;
+  using: string[];
 }
 
 class Graph {
 
   public static run(): void {
+    this.writeFile(this.buildData());
+  }
+
+  private static buildData(): IData[] {
+    let res: IData[] = [];
+
     for (let expr of Artifacts.getExpressions()) {
-      this.writeRunnable("expression_" + className(expr), expr.getRunnable(), true);
+      res.push(this.buildRunnable(expr.constructor.name, "expression", expr.getRunnable(), true));
     }
 
     for (let stat of Artifacts.getStatements()) {
-      this.writeRunnable("statement_" + className(stat), stat.getMatcher(), false);
+      res.push(this.buildRunnable(stat.constructor.name, "statement", stat.getMatcher(), false));
     }
 
     for (let stru of Artifacts.getStructures()) {
       let str = "Railroad.Diagram.INTERNAL_ALIGNMENT = 'left';\n" +
         "Railroad.Diagram(" + stru.getMatcher().toRailroad() + ").toString();";
       let using = stru.getMatcher().getUsing();
-      this.writeFile("structure_" + className(stru), str, using);
+      res.push({
+        name: "structure_" + stru.constructor.name,
+        type: "structure",
+        railroad: str,
+        using: sort(using)});
     }
+
+    return res;
   }
 
-  private static writeRunnable(name: string, runnable: Combi.IRunnable, complex: boolean): void {
-    let str = Combi.Combi.railroad(runnable, complex);
-    this.writeFile(name, str, runnable.getUsing());
+  private static buildRunnable(name: string, type: string, runnable: Combi.IRunnable, complex: boolean): IData {
+    return {
+      name: type + "_" + name,
+      type: type,
+      railroad: Combi.Combi.railroad(runnable, complex),
+      using: sort(runnable.getUsing())};
   }
 
-  private static writeFile(name: string, railroad: string, using: string[]) {
-    let unique = using.filter((v, i, a) => { return a.indexOf(v) === i; });
-    let json = {railroad, using: unique.sort()};
-    fs.writeFileSync("./syntax/" + name + ".json", JSON.stringify(json, undefined, 2), "utf8");
+  private static writeFile(data: IData[]) {
+    fs.writeFileSync("./syntax/generated.json", JSON.stringify(data, undefined, 2), "utf8");
   }
 
 }
--- web/generate.js
+++ web/generate.js
@@ -12,67 +12,67 @@ function run() {
   json.structures = [];
 
   let folder = "./syntax/";
-  let files = fs.readdirSync(folder);
+  let files = JSON.parse(fs.readFileSync(folder + "generated.json", "utf8"));
 
   for (let file of files) {
-    if (/\.json$/.test(file)) {
-      let contents = JSON.parse(fs.readFileSync(folder + file,"utf8"));
+    let css = "<defs>\n" +
+      "<style type=\"text/css\"><![CDATA[\n" +
+      "path {\n" +
+      "stroke-width: 3;\n" +
+      "stroke: black;\n" +
+      "fill: rgba(0,0,0,0);\n" +
+      "}\n" +
+      "text {\n" +
+      "font: bold 14px monospace;\n" +
+      "text-anchor: middle;\n" +
+      "}\n" +
+      "text.diagram-text {\n" +
+      "font-size: 12px;\n" +
+      "}\n" +
+      "a {\n" +
+      "fill: blue;\n" +
+      "}\n" +
+      "text.diagram-arrow {\n" +
+      "font-size: 16px;\n" +
+      "}\n" +
+      "text.label {\n" +
+      "text-anchor: start;\n" +
+      "}\n" +
+      "text.comment {\n" +
+      "font: italic 12px monospace;\n" +
+      "}\n" +
+      "rect {\n" +
+      "stroke-width: 3;\n" +
+      "stroke: black;\n" +
+      "fill: #BCBCBC;\n" +
+      "}\n" +
+      "path.diagram-text {\n" +
+      "stroke-width: 3;\n" +
+      "stroke: black;\n" +
+      "fill: #BCBCBC;\n" +
+      "cursor: help;\n" +
+      "}\n" +
+      "]]></style>\n" +
+      "</defs>\n";
 
-      let css = "<defs>\n" +
-        "<style type=\"text/css\"><![CDATA[\n" +
-        "path {\n" +
-        "stroke-width: 3;\n" +
-        "stroke: black;\n" +
-        "fill: rgba(0,0,0,0);\n" +
-        "}\n" +
-        "text {\n" +
-        "font: bold 14px monospace;\n" +
-        "text-anchor: middle;\n" +
-        "}\n" +
-        "text.diagram-text {\n" +
-        "font-size: 12px;\n" +
-        "}\n" +
-        "a {\n" +
-        "fill: blue;\n" +
-        "}\n" +
-        "text.diagram-arrow {\n" +
-        "font-size: 16px;\n" +
-        "}\n" +
-        "text.label {\n" +
-        "text-anchor: start;\n" +
-        "}\n" +
-        "text.comment {\n" +
-        "font: italic 12px monospace;\n" +
-        "}\n" +
-        "rect {\n" +
-        "stroke-width: 3;\n" +
-        "stroke: black;\n" +
-        "fill: #BCBCBC;\n" +
-        "}\n" +
-        "path.diagram-text {\n" +
-        "stroke-width: 3;\n" +
-        "stroke: black;\n" +
-        "fill: #BCBCBC;\n" +
-        "cursor: help;\n" +
-        "}\n" +
-        "]]></style>\n" +
-        "</defs>\n";
-//      console.log(file);
-      let result = eval(contents.railroad);
-      result = result.replace(/<svg /, "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ");
-      result = result.replace(/<g transform/, css + "<g transform");
-      let target = folder + file.split(".")[0] + ".svg";
-      fs.writeFileSync(target, result, "utf8");
+    let result = eval(file.railroad);
+    result = result.replace(/<svg /, "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ");
+    result = result.replace(/<g transform/, css + "<g transform");
+    let target = folder + file.name + ".svg";
+    fs.writeFileSync(target, result, "utf8");
 
-      let name = file.split(".")[0];
-
-      if (/^expression_/.test(name)) {
-        json.expressions.push(name);
-      } else if (/^structure_/.test(name)) {
-        json.structures.push(name);
-      } else {
-        json.statements.push(name);
-      }
+    switch (file.type) {
+      case "expression":
+        json.expressions.push(file.name);
+        break;
+      case "structure":
+        json.structures.push(file.name);
+        break;
+      case "statement":
+        json.statements.push(file.name);
+        break;
+      default:
+        throw "unknown type";
     }
   }
 
