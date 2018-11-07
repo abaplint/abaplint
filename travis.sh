43ecc48fc62db1f11bd2b3ab559c7a9568965912
larshp <larshp@hotmail.com>
2018-11-07 09:17:23 +0100

    rename

--- travis.sh
+++ travis.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 cd ..
-git clone https://github.com/abaplint/web.playground.git
-git clone https://github.com/abaplint/web.syntax.git
+git clone https://github.com/abaplint/playground.abaplint.org.git
+git clone https://github.com/abaplint/syntax.abaplint.org.git
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
-cp -r abaplint/web/playground/* web.playground/
-cd web.playground
+cp -r abaplint/web/playground/* playground.abaplint.org/
+cd playground.abaplint.org
 git status
 git add -A
 git status
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -q https://$GITHUB_API_KEY@github.com/abaplint/web.playground.git > /dev/null 2>&1
+git push -q https://$GITHUB_API_KEY@github.com/abaplint/playground.abaplint.org.git > /dev/null 2>&1
 
 cd ..
 
-cp -r abaplint/web/syntax/* web.syntax/
-cd web.syntax
+cp -r abaplint/web/syntax/* syntax.abaplint.org/
+cd syntax.abaplint.org
 git status
 git add -A
 git status
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -q https://$GITHUB_API_KEY@github.com/abaplint/web.syntax.git > /dev/null 2>&1
+git push -q https://$GITHUB_API_KEY@github.com/abaplint/syntax.abaplint.org.git > /dev/null 2>&1
