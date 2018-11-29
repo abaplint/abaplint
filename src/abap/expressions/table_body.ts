42b71a5b6bec24daadf0457fdcacf957dc3923f0
larshp <larshp@hotmail.com>
2018-11-29 15:37:07 +0100

    fix

--- src/abap/expressions/table_body.ts
+++ src/abap/expressions/table_body.ts
@@ -1,9 +1,9 @@
-import {seq, tok, alt, Expression, IStatementRunnable} from "../combi";
-import {BracketLeft, BracketRight, BracketRightW} from "../tokens/";
+import {seq, tok, Expression, IStatementRunnable} from "../combi";
+import {BracketLeft, BracketRightW} from "../tokens/";
 
 export class TableBody extends Expression {
   public getRunnable(): IStatementRunnable {
-    const ret = seq(tok(BracketLeft), alt(tok(BracketRight), tok(BracketRightW)));
+    const ret = seq(tok(BracketLeft), tok(BracketRightW));
     return ret;
   }
 }
