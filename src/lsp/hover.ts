6f922d17eacad5b40c24eb3c5aa571580e1cf949
larshp <larshp@hotmail.com>
2018-12-27 09:49:44 +0100

    stats unit test + hover changes

--- src/lsp/hover.ts
+++ src/lsp/hover.ts
@@ -1,5 +1,9 @@
 import * as LServer from "vscode-languageserver-protocol";
 import {Registry} from "../registry";
+import {ABAPFile} from "../files";
+import {Token} from "../abap/tokens/_token";
+import {INode} from "../abap/nodes/_inode";
+import {StructureNode, StatementNode, ExpressionNode, TokenNode} from "../abap/nodes";
 
 export class Hover {
   public static find(reg: Registry, uri: string, line: number, character: number): LServer.MarkupContent | undefined {
@@ -8,21 +12,73 @@ export class Hover {
       return undefined;
     }
 
+    let ret: LServer.MarkupContent | undefined = undefined;
+
     for (const statement of file.getStatements()) {
       for (const token of statement.getTokens()) {
 // assumption: no tokens span multiple lines
         if (token.getRow() - 1 === line
             && token.getCol() - 1 <= character
-            && token.getCol() - 1 + token.getStr().length >= character) {
-          const value =
+            && token.getCol() - 1 + token.getStr().length > character) {
+          let value =
             "```abap\n" + token.getStr() + "\n```\n" +
             "Statement: " + statement.get().constructor.name + "\n\n" +
             "Token: " + token.constructor.name;
-          return {kind: LServer.MarkupKind.Markdown, value};
+
+          value = value + this.fullPath(file, token);
+
+          ret = {kind: LServer.MarkupKind.Markdown, value};
+          break;
         }
       }
+      if (ret !== undefined) {
+        break;
+      }
+    }
+
+    return ret;
+  }
+
+  private static fullPath(file: ABAPFile, token: Token): string {
+    const structure = file.getStructure();
+
+    if (structure === undefined) {
+      return "";
+    }
+
+    const found = this.traverse(structure, "", token);
+    if (found === undefined) {
+      return "";
+    }
+
+    return "\n\n" + found;
+  }
+
+  private static traverse(node: INode, parents: string, search: Token): string | undefined {
+    let local = parents;
+    if (local !== "") {
+      local = local + " -> ";
+    }
+    if (node instanceof StructureNode) {
+      local = local + "Structure: " + node.get().constructor.name;
+    } else if (node instanceof StatementNode) {
+      local = local + "Statement: " + node.get().constructor.name;
+    } else if (node instanceof ExpressionNode) {
+      local = local + "Expression: " + node.get().constructor.name;
+    } else if (node instanceof TokenNode) {
+      local = local + "Token: " + node.get().constructor.name;
+      if (node.get() === search) {
+        return local;
+      }
+    } else {
+      throw new Error("hover, traverse, unexpected node type");
+    }
+
+    for (const child of node.getChildren()) {
+      return this.traverse(child, local, search);
     }
 
     return undefined;
   }
+
 }
--- test/lsp/hover.ts
+++ test/lsp/hover.ts
@@ -2,6 +2,7 @@ import {expect} from "chai";
 import {MemoryFile} from "../../src/files";
 import {Registry} from "../../src/registry";
 import {Hover} from "../../src/lsp/hover";
+import {MarkupContent} from "vscode-languageserver-types";
 
 describe("LSP, hover", () => {
 
@@ -9,15 +10,30 @@ describe("LSP, hover", () => {
     const file = new MemoryFile("foobar.prog.abap", "DO.");
     const reg = new Registry().addFile(file).parse();
 
-    let hover = Hover.find(reg, file.getFilename(), 0 , 0);
+    let hover: MarkupContent | undefined = undefined;
+    for (let i = 0; i < 2; i++) {
+      hover = Hover.find(reg, file.getFilename(), 0 , i);
+      expect(hover).to.not.equal(undefined);
+      expect(hover!.value).to.contain("```abap\nDO\n```\nStatement: Do\n\nToken: Identifier");
+    }
+
+    hover = Hover.find(reg, file.getFilename(), 0 , 2);
     expect(hover).to.not.equal(undefined);
-    expect(hover!.value).to.equal("```abap\nDO\n```\nStatement: Do\n\nToken: Identifier");
+    expect(hover!.value).to.contain("```abap\n.\n```\nStatement: Do\n\nToken: Punctuation");
 
     hover = Hover.find(reg, file.getFilename(), 1 , 0);
     expect(hover).to.equal(undefined);
 
-    hover = Hover.find(reg, file.getFilename(), 0 , 4);
+    hover = Hover.find(reg, file.getFilename(), 0 , 3);
     expect(hover).to.equal(undefined);
+  });
+
+  it("full structure", () => {
+    const file = new MemoryFile("foobar.prog.abap", "DO. ENDDO.");
+    const reg = new Registry().addFile(file).parse();
+    const hover = Hover.find(reg, file.getFilename(), 0 , 0);
+    expect(hover).to.not.equal(undefined);
+    expect(hover!.value).to.contain("Structure: Any");
   });
 
 });
