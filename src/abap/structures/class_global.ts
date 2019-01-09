575793a428b215b048c89140260657dd415e7b01
larshp <larshp@hotmail.com>
2019-01-09 08:02:25 +0100

    more parsing

--- src/abap/structures/class_global.ts
+++ src/abap/structures/class_global.ts
@@ -1,11 +1,14 @@
 import * as Structures from "./";
 import {Structure} from "./_structure";
-import {seq, IStructureRunnable, sub} from "./_combi";
+import * as Statements from "../statements";
+import {seq, IStructureRunnable, sub, star, sta} from "./_combi";
 
 export class ClassGlobal extends Structure {
 
   public getMatcher(): IStructureRunnable {
-    return seq(sub(new Structures.ClassDefinition()), sub(new Structures.ClassImplementation()));
+    return seq(star(sta(Statements.TypePools)),
+               sub(new Structures.ClassDefinition()),
+               sub(new Structures.ClassImplementation()));
   }
 
 }
--- src/abap/structures/interface.ts
+++ src/abap/structures/interface.ts
@@ -1,14 +1,18 @@
 import * as Statements from "../statements";
-import {star, beginEnd, sta, sub, IStructureRunnable} from "./_combi";
+import {star, beginEnd, seq, sta, sub, IStructureRunnable} from "./_combi";
 import {Structure} from "./_structure";
 import {SectionContents} from "./class_section";
 
 export class Interface extends Structure {
 
   public getMatcher(): IStructureRunnable {
-    return beginEnd(sta(Statements.Interface),
-                    star(sub(new SectionContents())),
-                    sta(Statements.EndInterface));
+    const intf = beginEnd(sta(Statements.Interface),
+                          star(sub(new SectionContents())),
+                          sta(Statements.EndInterface));
+
+    return seq(star(sta(Statements.TypePools)),
+               star(sta(Statements.InterfaceLoad)),
+               intf);
   }
 
 }
