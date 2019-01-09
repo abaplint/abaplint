196466a05510905f0e3f2c1c0a1e08fa3d761b7d
larshp <larshp@hotmail.com>
2019-01-09 14:59:25 +0100

    basic TABL parsing

--- src/objects/table.ts
+++ src/objects/table.ts
@@ -1,9 +1,41 @@
 import {AbstractObject} from "./_abstract_object";
+import * as xmljs from "xml-js";
+import {xmlToArray} from "../xml_utils";
 
 export class Table extends AbstractObject {
 
   public getType(): string {
     return "TABL";
+  }
+
+  public getFields(): string[] {
+    const xml = this.getXML();
+    if (xml === undefined) {
+      return [];
+    }
+    const parsed: any = xmljs.xml2js(xml, {compact: true});
+
+    return this.parse(parsed);
+  }
+
+  private parse(data: any): string[] {
+    const ret: string[] = [];
+
+    const fields = data.abapGit["asx:abap"]["asx:values"].DD03P_TABLE;
+    for (const field of xmlToArray(fields.DD03P)) {
+      ret.push(field.FIELDNAME._text);
+    }
+
+    return ret;
+  }
+
+  private getXML(): string | undefined {
+    for (const file of this.getFiles()) {
+      if (file.getFilename().match(/\.tabl\.xml$/i)) {
+        return file.getRaw();
+      }
+    }
+    return undefined;
   }
 
 }
---
+++ test/objects/table.ts
@@ -0,0 +1,72 @@
+import {expect} from "chai";
+import {Registry} from "../../src/registry";
+import {MemoryFile} from "../../src/files/memory_file";
+import {Table} from "../../src/objects";
+
+describe("Table, parse XML", () => {
+  it("test", () => {
+    const xml =
+      "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+      "<abapGit version=\"v1.0.0\" serializer=\"LCL_OBJECT_TABL\" serializer_version=\"v1.0.0\">\n" +
+      " <asx:abap xmlns:asx=\"http://www.sap.com/abapxml\" version=\"1.0\">\n" +
+      "  <asx:values>\n" +
+      "   <DD02V>\n" +
+      "    <TABNAME>ZABAPGIT_UNIT_T2</TABNAME>\n" +
+      "    <DDLANGUAGE>E</DDLANGUAGE>\n" +
+      "    <TABCLASS>TRANSP</TABCLASS>\n" +
+      "    <CLIDEP>X</CLIDEP>\n" +
+      "    <DDTEXT>testing</DDTEXT>\n" +
+      "    <CONTFLAG>A</CONTFLAG>\n" +
+      "    <EXCLASS>1</EXCLASS>\n" +
+      "   </DD02V>\n" +
+      "   <DD09L>\n" +
+      "    <TABNAME>ZABAPGIT_UNIT_T2</TABNAME>\n" +
+      "    <AS4LOCAL>A</AS4LOCAL>\n" +
+      "    <TABKAT>0</TABKAT>\n" +
+      "    <TABART>APPL0</TABART>\n" +
+      "    <BUFALLOW>N</BUFALLOW>\n" +
+      "   </DD09L>\n" +
+      "   <DD03P_TABLE>\n" +
+      "    <DD03P>\n" +
+      "     <TABNAME>ZABAPGIT_UNIT_T2</TABNAME>\n" +
+      "     <FIELDNAME>MANDT</FIELDNAME>\n" +
+      "     <POSITION>0001</POSITION>\n" +
+      "     <KEYFLAG>X</KEYFLAG>\n" +
+      "     <ROLLNAME>MANDT</ROLLNAME>\n" +
+      "     <ADMINFIELD>0</ADMINFIELD>\n" +
+      "     <NOTNULL>X</NOTNULL>\n" +
+      "     <COMPTYPE>E</COMPTYPE>\n" +
+      "    </DD03P>\n" +
+      "    <DD03P>\n" +
+      "     <TABNAME>ZABAPGIT_UNIT_T2</TABNAME>\n" +
+      "     <FIELDNAME>XUBNAME</FIELDNAME>\n" +
+      "     <POSITION>0002</POSITION>\n" +
+      "     <KEYFLAG>X</KEYFLAG>\n" +
+      "     <ROLLNAME>XUBNAME</ROLLNAME>\n" +
+      "     <ADMINFIELD>0</ADMINFIELD>\n" +
+      "     <NOTNULL>X</NOTNULL>\n" +
+      "     <COMPTYPE>E</COMPTYPE>\n" +
+      "    </DD03P>\n" +
+      "    <DD03P>\n" +
+      "     <TABNAME>ZABAPGIT_UNIT_T2</TABNAME>\n" +
+      "     <FIELDNAME>NAME</FIELDNAME>\n" +
+      "     <POSITION>0003</POSITION>\n" +
+      "     <ROLLNAME>XUBNAME</ROLLNAME>\n" +
+      "     <ADMINFIELD>0</ADMINFIELD>\n" +
+      "     <COMPTYPE>E</COMPTYPE>\n" +
+      "    </DD03P>\n" +
+      "   </DD03P_TABLE>\n" +
+      "  </asx:values>\n" +
+      " </asx:abap>\n" +
+      "</abapGit>";
+
+    const reg = new Registry().addFile(new MemoryFile("zabapgit_unit_t2.tabl.xml", xml)).parse();
+    const tabl = reg.getObjects()[0] as Table;
+    expect(tabl.getName()).to.equal("ZABAPGIT_UNIT_T2");
+    const fields = tabl.getFields();
+    expect(fields.length).to.equal(3);
+    expect(fields).to.contain("MANDT");
+    expect(fields).to.contain("XUBNAME");
+    expect(fields).to.contain("NAME");
+  });
+});
