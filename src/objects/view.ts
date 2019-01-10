73363e90ea51a608b823273c08949c9eab270512
larshp <larshp@hotmail.com>
2019-01-10 08:46:58 +0100

    basic VIEW parsing

--- src/objects/view.ts
+++ src/objects/view.ts
@@ -1,4 +1,6 @@
 import {AbstractObject} from "./_abstract_object";
+import * as xmljs from "xml-js";
+import {xmlToArray} from "../xml_utils";
 
 export class View extends AbstractObject {
 
@@ -7,8 +9,33 @@ export class View extends AbstractObject {
   }
 
   public getFields(): string[] {
-// todo
-    return [];
+    const xml = this.getXML();
+    if (xml === undefined) {
+      return [];
+    }
+    const parsed: any = xmljs.xml2js(xml, {compact: true});
+
+    return this.parse(parsed);
+  }
+
+  private parse(data: any): string[] {
+    const ret: string[] = [];
+
+    const fields = data.abapGit["asx:abap"]["asx:values"].DD27P_TABLE;
+    for (const field of xmlToArray(fields.DD27P)) {
+      ret.push(field.VIEWFIELD._text);
+    }
+
+    return ret;
+  }
+
+  private getXML(): string | undefined {
+    for (const file of this.getFiles()) {
+      if (file.getFilename().match(/\.view\.xml$/i)) {
+        return file.getRaw();
+      }
+    }
+    return undefined;
   }
 
 }
---
+++ test/objects/view.ts
@@ -0,0 +1,94 @@
+import {expect} from "chai";
+import {Registry} from "../../src/registry";
+import {MemoryFile} from "../../src/files/memory_file";
+import {View} from "../../src/objects";
+
+describe("View, parse XML", () => {
+  it("test", () => {
+    const xml =
+      "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+      "<abapGit version=\"v1.0.0\" serializer=\"LCL_OBJECT_VIEW\" serializer_version=\"v1.0.0\">\n" +
+      " <asx:abap xmlns:asx=\"http://www.sap.com/abapxml\" version=\"1.0\">\n" +
+      "  <asx:values>\n" +
+      "   <DD25V>\n" +
+      "    <VIEWNAME>ZAG_UNIT_TESTV</VIEWNAME>\n" +
+      "    <AS4LOCAL>A</AS4LOCAL>\n" +
+      "    <DDLANGUAGE>E</DDLANGUAGE>\n" +
+      "    <AGGTYPE>V</AGGTYPE>\n" +
+      "    <ROOTTAB>TADIR</ROOTTAB>\n" +
+      "    <DDTEXT>unit test</DDTEXT>\n" +
+      "    <VIEWCLASS>D</VIEWCLASS>\n" +
+      "    <VIEWGRANT>R</VIEWGRANT>\n" +
+      "   </DD25V>\n" +
+      "   <DD26V_TABLE>\n" +
+      "    <DD26V>\n" +
+      "     <VIEWNAME>ZAG_UNIT_TESTV</VIEWNAME>\n" +
+      "     <TABNAME>TADIR</TABNAME>\n" +
+      "     <TABPOS>0001</TABPOS>\n" +
+      "     <FORTABNAME>TADIR</FORTABNAME>\n" +
+      "    </DD26V>\n" +
+      "   </DD26V_TABLE>\n" +
+      "   <DD27P_TABLE>\n" +
+      "    <DD27P>\n" +
+      "     <VIEWNAME>ZAG_UNIT_TESTV</VIEWNAME>\n" +
+      "     <OBJPOS>0001</OBJPOS>\n" +
+      "     <DDLANGUAGE>E</DDLANGUAGE>\n" +
+      "     <VIEWFIELD>PGMID</VIEWFIELD>\n" +
+      "     <TABNAME>TADIR</TABNAME>\n" +
+      "     <FIELDNAME>PGMID</FIELDNAME>\n" +
+      "     <KEYFLAG>X</KEYFLAG>\n" +
+      "     <ROLLNAME>PGMID</ROLLNAME>\n" +
+      "     <ROLLNAMEVI>PGMID</ROLLNAMEVI>\n" +
+      "    </DD27P>\n" +
+      "    <DD27P>\n" +
+      "     <VIEWNAME>ZAG_UNIT_TESTV</VIEWNAME>\n" +
+      "     <OBJPOS>0002</OBJPOS>\n" +
+      "     <DDLANGUAGE>E</DDLANGUAGE>\n" +
+      "     <VIEWFIELD>OBJECT</VIEWFIELD>\n" +
+      "     <TABNAME>TADIR</TABNAME>\n" +
+      "     <FIELDNAME>OBJECT</FIELDNAME>\n" +
+      "     <KEYFLAG>X</KEYFLAG>\n" +
+      "     <ROLLNAME>TROBJTYPE</ROLLNAME>\n" +
+      "     <ROLLNAMEVI>TROBJTYPE</ROLLNAMEVI>\n" +
+      "     <SHLPORIGIN>X</SHLPORIGIN>\n" +
+      "     <SHLPNAME>SCTSOBJECT</SHLPNAME>\n" +
+      "     <SHLPFIELD>OBJECT</SHLPFIELD>\n" +
+      "    </DD27P>\n" +
+      "    <DD27P>\n" +
+      "     <VIEWNAME>ZAG_UNIT_TESTV</VIEWNAME>\n" +
+      "     <OBJPOS>0003</OBJPOS>\n" +
+      "     <DDLANGUAGE>E</DDLANGUAGE>\n" +
+      "     <VIEWFIELD>OBJ_NAME</VIEWFIELD>\n" +
+      "     <TABNAME>TADIR</TABNAME>\n" +
+      "     <FIELDNAME>OBJ_NAME</FIELDNAME>\n" +
+      "     <KEYFLAG>X</KEYFLAG>\n" +
+      "     <ROLLNAME>SOBJ_NAME</ROLLNAME>\n" +
+      "     <ROLLNAMEVI>SOBJ_NAME</ROLLNAMEVI>\n" +
+      "    </DD27P>\n" +
+      "    <DD27P>\n" +
+      "     <VIEWNAME>ZAG_UNIT_TESTV</VIEWNAME>\n" +
+      "     <OBJPOS>0004</OBJPOS>\n" +
+      "     <DDLANGUAGE>E</DDLANGUAGE>\n" +
+      "     <VIEWFIELD>KORRNUM</VIEWFIELD>\n" +
+      "     <TABNAME>TADIR</TABNAME>\n" +
+      "     <FIELDNAME>KORRNUM</FIELDNAME>\n" +
+      "     <ROLLNAME>TRKORR_OLD</ROLLNAME>\n" +
+      "     <ROLLNAMEVI>TRKORR_OLD</ROLLNAMEVI>\n" +
+      "    </DD27P>\n" +
+      "   </DD27P_TABLE>\n" +
+      "  </asx:values>\n" +
+      " </asx:abap>\n" +
+      "</abapGit>";
+
+
+    const reg = new Registry().addFile(new MemoryFile("zag_unit_testv.view.xml", xml)).parse();
+    const tabl = reg.getObjects()[0] as View;
+    expect(tabl.getName()).to.equal("ZAG_UNIT_TESTV");
+    const fields = tabl.getFields();
+    expect(fields.length).to.equal(4);
+    expect(fields).to.contain("PGMID");
+    expect(fields).to.contain("OBJECT");
+    expect(fields).to.contain("OBJ_NAME");
+    expect(fields).to.contain("KORRNUM");
+  });
+});
