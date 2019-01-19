7fa0eb93799986efd07c19583b3469ebcb37be08
Steffen <sb@sbuehl.com>
2019-01-19 20:25:29 +0100

    fix tslint issues

--- src/formatters/junit.ts
+++ src/formatters/junit.ts
@@ -1,58 +1,57 @@
 import {Issue} from "../issue";
 import {IFormatter} from "./_iformatter";
-import {js2xml} from 'xml-js';
+import {js2xml} from "xml-js";
 
 export class Junit implements IFormatter {
 
   public output(issues: Issue[], _fileCount: number): string {
-    let outputObj = {
+    const outputObj = {
       _declaration: {
         _attributes: {
-          version: '1.0',
-          encoding: 'UTF-8'
-        }
+          version: "1.0",
+          encoding: "UTF-8",
+        },
       },
       testsuites: {
         testsuite: {
           _attributes: {
-            name: 'abaplint',
+            name: "abaplint",
             tests: issues.length || 1,
             failures: issues.length,
             errors: 0,
-            skipped: 0
+            skipped: 0,
           },
-          testcase: [] as {}[]
-        }
-      }
+          testcase: [] as {}[],
+        },
+      },
     };
 
-    if(issues.length > 0) {
+    if (issues.length > 0) {
       for (const issue of issues) {
         outputObj.testsuites.testsuite.testcase.push({
           _attributes: {
             classname: issue.getFile().getObjectName(),
             file: issue.getFile().getFilename(),
-            name: `${issue.getFile().getFilename()}: [${issue.getStart().getRow()}, ${issue.getStart().getCol()}] - ${issue.getKey()}`
+            name: `${issue.getFile().getFilename()}: [${issue.getStart().getRow()}, ${issue.getStart().getCol()}] - ${issue.getKey()}`,
           },
           failure: {
             _attributes: {
               message: issue.getKey(),
             },
-            _cdata: `${issue.getMessage()}`
-          }
+            _cdata: `${issue.getMessage()}`,
+          },
         });
       }
-    }
-    else {
+    } else {
       outputObj.testsuites.testsuite.testcase.push({
         _attributes: {
-          classname: 'none',
-          name: 'OK'
+          classname: "none",
+          name: "OK",
         },
       });
     }
 
-    let xml = js2xml(outputObj, { compact: true, spaces: 2 });
+    const xml = js2xml(outputObj, {compact: true, spaces: 2});
 
     return xml;
   }
