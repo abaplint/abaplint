f699ac08d89511203b5ccb5c3f9bfb795c47a3bd
PCF <32403010+pcf0@users.noreply.github.com>
2019-02-17 17:39:53 +0100

    add FIXED-POINT ARITHMETIC to INSERT REPORT
    fixes #364

--- src/abap/statements/insert_report.ts
+++ src/abap/statements/insert_report.ts
@@ -11,6 +11,7 @@ export class InsertReport extends Statement {
                         seq(str("DIRECTORY ENTRY"), new Source()),
                         seq(str("UNICODE ENABLING"), new Source()),
                         seq(str("PROGRAM TYPE"), new Source()),
+                        seq(str("FIXED-POINT ARITHMETIC"), new Source()),
                         str("KEEPING DIRECTORY ENTRY"));
 
     const ret = seq(str("INSERT REPORT"),
