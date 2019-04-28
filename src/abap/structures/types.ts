e794b86702930491390257c9f1ab6bfece0de6fd
larshp <larshp@hotmail.com>
2019-04-28 12:08:39 +0200

    nested TYPES, close #408

--- src/abap/structures/types.ts
+++ src/abap/structures/types.ts
@@ -1,12 +1,12 @@
 import * as Statements from "../statements";
 import {Structure} from "./_structure";
-import {star, IStructureRunnable, sta, alt, beginEnd} from "./_combi";
+import {star, IStructureRunnable, sta, alt, sub, beginEnd} from "./_combi";
 
 export class Types extends Structure {
 
   public getMatcher(): IStructureRunnable {
     return beginEnd(sta(Statements.TypeBegin),
-                    star(alt(sta(Statements.Type), sta(Statements.IncludeType))),
+                    star(alt(sta(Statements.Type), sub(new Types()), sta(Statements.IncludeType))),
                     sta(Statements.TypeEnd));
   }
 
--- test/abap/structures/types.ts
+++ test/abap/structures/types.ts
@@ -3,6 +3,13 @@ import {Types} from "../../../src/abap/structures";
 
 const cases = [
   {abap: "TYPES: BEGIN OF ty_foo, raw TYPE xstring, compressed_len TYPE i, END OF ty_foo."},
+  {abap: "TYPES:\n" +
+    "BEGIN OF ty_result,\n" +
+    "  ci_has_errors TYPE abap_bool,\n" +
+    "  BEGIN OF statistics,\n" +
+    "    finish_timestamp TYPE timestampl,\n" +
+    "  END OF statistics,\n" +
+    "END OF ty_result."},
 ];
 
 structureType(cases, new Types());
