07670732caf41dce6f1324d6850d0504321542b7
larshp <larshp@hotmail.com>
2019-06-07 08:19:16 +0200

    add SCP1, prepare release 1.7.1

---
+++ src/objects/business_configuration_set.ts
@@ -0,0 +1,9 @@
+import {AbstractObject} from "./_abstract_object";
+
+export class BusinessConfigurationSet extends AbstractObject {
+
+  public getType(): string {
+    return "SCP1";
+  }
+
+}
--- src/objects/index.ts
+++ src/objects/index.ts
@@ -4,6 +4,7 @@ export * from "./authorization_object_class";
 export * from "./authorization_object";
 export * from "./bsp_application";
 export * from "./business_add_in_implementation";
+export * from "./business_configuration_set";
 export * from "./chapter_of_book_structure";
 export * from "./checkpoint_group";
 export * from "./class";
