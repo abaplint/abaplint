c70ff3ca4f7c34200e1ee21616dd7f3bcbcc5cae
larshp <larshp@hotmail.com>
2019-07-11 10:49:57 +0200

    fix DESTINATION DEFAULT

--- src/abap/expressions/destination.ts
+++ src/abap/expressions/destination.ts
@@ -1,8 +1,8 @@
-import {seq, str, opt, Expression, IStatementRunnable} from "../combi";
+import {seq, str, opt, Expression, IStatementRunnable, altPrio} from "../combi";
 import {Source} from ".";
 
 export class Destination extends Expression {
   public getRunnable(): IStatementRunnable {
-    return seq(str("DESTINATION"), opt(str("IN GROUP")), new Source());
+    return seq(str("DESTINATION"), opt(str("IN GROUP")), altPrio(str("DEFAULT"), new Source()));
   }
 }
