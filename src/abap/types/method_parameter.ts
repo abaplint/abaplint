34c5ce2bb3e59e3ba0f26273f5118bbfcd52a9f4
Pascal Erni <pascal.erni@outlook.com>
2019-08-16 18:26:17 +0200

    new methods getTypeName and isReferenceTo

--- src/abap/types/method_parameter.ts
+++ src/abap/types/method_parameter.ts
@@ -1,8 +1,10 @@
 import {ExpressionNode} from "../../abap/nodes";
-import {MethodParam, MethodParamName} from "../../abap/expressions";
+import {MethodParam, MethodParamName, TypeName, TypeParam} from "../../abap/expressions";
 import {TypedIdentifier} from "./_typed_identifier";
 
 export class MethodParameter extends TypedIdentifier {
+  private readonly typeName: ExpressionNode | undefined;
+  private hasRefToToken: boolean;
 
   constructor(node: ExpressionNode) {
     if (!(node.get() instanceof MethodParam)) {
@@ -13,6 +15,25 @@ export class MethodParameter extends TypedIdentifier {
       throw new Error("method_parameter.ts, todo, handle pass by value and reference");
     }
     super(name.getFirstToken(), node);
+
+    const typeParam = node.findFirstExpression(TypeParam);
+    if (!typeParam) {
+      throw new Error("MethodParameter expected a TypeParam as a child");
+    }
+    this.typeName = typeParam.findFirstExpression(TypeName);
+    this.hasRefToToken = (typeParam.findDirectTokenByText("REF") !== undefined);
+  }
+
+  public getTypeName(): string {
+    let name: string = "";
+    for(const token of this.typeName!.getAllTokens()) {
+      name = name.concat(token.getStr());
+    }
+    return name;
+  }
+
+  public isReferenceTo(): boolean {
+    return this.hasRefToToken;
   }
 
 // todo: pass by reference / pass by value / write protected
