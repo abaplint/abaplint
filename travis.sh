ecf39dd26bd45bb0e245a7cf520ff840f2d45407
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2019-09-29 19:28:46 +0300

    playground - travis rm -Rf old deploy before copying new

--- travis.sh
+++ travis.sh
@@ -6,6 +6,7 @@ git clone https://github.com/abaplint/syntax.abaplint.org.git
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
+rm -Rf playground.abaplint.org/*
 cp abaplint/web/playground/build/* playground.abaplint.org/
 cd playground.abaplint.org
 git status
