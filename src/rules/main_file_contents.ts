059638f93e4ba629452b0ebd813fe8722cdb5981
larshp <larshp@hotmail.com>
2019-09-29 09:07:13 +0200

    Allow PROGRAM, closes #557

--- src/rules/main_file_contents.ts
+++ src/rules/main_file_contents.ts
@@ -54,21 +54,22 @@ export class MainFileContents implements IRule {
         count = count + 1;
         first = main.getStatements()[count];
       }
-      if (first === undefined || !(first.get() instanceof Statements.Report)) {
+      if (first === undefined || !(first.get() instanceof Statements.Report
+          || first.get() instanceof Statements.Program)) {
         return [new Issue({file: main,
-          message: this.getDescription("Report must begin with REPORT"),
+          message: this.getDescription("Report must begin with REPORT or PROGRAM"),
           key: this.getKey(),
           start: new Position(1, 1)})];
       }
       const name = first.findFirstExpression(Expressions.ReportName);
       if (name === undefined) {
         return [new Issue({file: main,
-          message: this.getDescription("Add report name to REPORT statement"),
+          message: this.getDescription("Add report name to REPORT or PROGRAM statement"),
           key: this.getKey(),
           start: new Position(1, 1)})];
       } else if (name.getFirstToken().getStr().toUpperCase() !== obj.getName()) {
         return [new Issue({file: main,
-          message: this.getDescription("REPORT name should match filename"),
+          message: this.getDescription("REPORT or PROGRAM name should match filename"),
           key: this.getKey(),
           start: new Position(1, 1)})];
       }
