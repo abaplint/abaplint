80645c7a5236e00abe55025f436af2fec0a984ed
larshp <larshp@hotmail.com>
2019-10-18 10:54:39 +0200

    basic rule to check INCLUDEs

---
+++ src/rules/syntax/check_include.ts
@@ -0,0 +1,75 @@
+import {Issue} from "../../issue";
+import {ABAPRule} from "../_abap_rule";
+import {ABAPFile} from "../../files";
+import {Registry} from "../../registry";
+import {BasicRuleConfig} from "../_basic_rule_config";
+import {Include} from "../../abap/statements";
+import {IncludeName} from "../../abap/expressions";
+import {ABAPObject} from "../../objects/_abap_object";
+import {FunctionGroup} from "../../objects";
+
+/** Checks INCLUDE statements */
+export class CheckIncludeConf extends BasicRuleConfig {
+}
+
+export class CheckInclude extends ABAPRule {
+
+  private conf = new CheckIncludeConf();
+
+  public getKey(): string {
+    return "check_include";
+  }
+
+  public getDescription(include: string): string {
+    return "Include " + include + " not found";
+  }
+
+  public getConfig() {
+    return this.conf;
+  }
+
+  public setConfig(conf: CheckIncludeConf) {
+    this.conf = conf;
+  }
+
+// todo, check for recursive INCLUDEs, and make sure to start in the main file
+
+  public runParsed(file: ABAPFile, reg: Registry, obj: ABAPObject) {
+    const issues: Issue[] = [];
+
+    for (const statement of file.getStatements()) {
+      if (statement.get() instanceof Include) {
+        if (statement.concatTokens().toUpperCase().includes("IF FOUND")) {
+          continue;
+        }
+        const iexp = statement.findFirstExpression(IncludeName);
+        if (iexp === undefined) {
+          throw new Error("unexpected Include node");
+        }
+        const name = iexp.getFirstToken().getStr();
+
+        if (this.findInclude(name, reg, obj) === false) {
+          issues.push(new Issue({file,
+            message: this.getDescription(name),
+            key: this.getKey(),
+            start: statement.getStart(),
+            end: statement.getEnd()}));
+        }
+      }
+    }
+
+    return issues;
+  }
+
+  private findInclude(name: string, reg: Registry, obj: ABAPObject): boolean {
+    const top = ("L" + obj.getName() + "TOP").toUpperCase();
+    const uxx = ("L" + obj.getName() + "UXX").toUpperCase();
+    if (obj instanceof FunctionGroup && (name.toUpperCase() === top || name.toUpperCase() === uxx)) {
+// todo, this is a workaround, more work needed for FUGR
+      return true;
+    }
+    const res = reg.getObject("PROG", name);
+    return res !== undefined;
+  }
+
+}
---
+++ test/rules/syntax/check_include.ts
@@ -0,0 +1,45 @@
+import {expect} from "chai";
+import {CheckInclude} from "../../../src/rules";
+import {Registry} from "../../../src/registry";
+import {MemoryFile} from "../../../src/files";
+import {Issue} from "../../../src/issue";
+
+function runMulti(files: {filename: string, contents: string}[]): Issue[] {
+  const reg = new Registry();
+  for (const file of files) {
+    reg.addFile(new MemoryFile(file.filename, file.contents));
+  }
+  reg.parse();
+  let issues: Issue[] = [];
+  for (const obj of reg.getObjects()) {
+    issues = issues.concat(new CheckInclude().run(obj, reg));
+  }
+  return issues;
+}
+
+describe("Rules, check_include", function () {
+  it("parser error", () => {
+    const issues = runMulti([{filename: "cl_foo.clas.abap", contents: "parser error"}]);
+    expect(issues.length).to.equals(0);
+  });
+
+  it("not found", () => {
+    const contents = `INCLUDE znot_found.`;
+    const issues = runMulti([{filename: "zfoo.prog.abap", contents}]);
+    expect(issues.length).to.equals(1);
+  });
+
+  it("found", () => {
+    const issues = runMulti([
+      {filename: "zexists.prog.abap", contents: `WRITE 2.`},
+      {filename: "zfoo.prog.abap", contents: `INCLUDE zexists.`}]);
+    expect(issues.length).to.equals(0);
+  });
+
+  it("not found, IF FOUND", () => {
+    const contents = `INCLUDE znot_found IF FOUND.`;
+    const issues = runMulti([{filename: "zfoo.prog.abap", contents}]);
+    expect(issues.length).to.equals(0);
+  });
+
+});
