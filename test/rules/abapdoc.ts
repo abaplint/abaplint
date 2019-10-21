0771a402034dde9bcdcf22272fceb661dbf8b0c8
GB <27279305+g-back@users.noreply.github.com>
2019-10-21 12:29:37 +0200

    modify and add additional tests

--- test/rules/abapdoc.ts
+++ test/rules/abapdoc.ts
@@ -1,10 +1,11 @@
 import {testRule} from "./_utils";
-import {Abapdoc} from "../../src/rules/abapdoc";
+import {Abapdoc, AbapdocConf} from "../../src/rules/abapdoc";
 
 
-const tests = [
+const defaultConfigTests = [
   // all public methods have abapdoc
-  { abap: ` CLASS zcl_foo DEFINITION.
+  {
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
               PUBLIC SECTION.
                 METHODS:
                   "! doc
@@ -16,49 +17,117 @@ const tests = [
                 METHODS:
                   coobar RETURNING VALUE(rv_string) TYPE string,
                   doobar RETURNING VALUE(rv_string) TYPE string.
-            ENDCLASS.`, cnt: 0},
+            ENDCLASS.`, cnt: 0,
+  },
 
   // one method is lacking abapdoc
-  { abap: ` CLASS zcl_foo DEFINITION.
+  {
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
               PUBLIC SECTION.
               METHODS:
                 "! doc
                 "! doc
                 foobar RETURNING VALUE(rv_string) TYPE string,
                 moobar RETURNING VALUE(rv_string) TYPE string.
-          ENDCLASS.`, cnt: 1},
+          ENDCLASS.`, cnt: 1,
+  },
 
   // two methods are lacking abapdoc
-  { abap: ` CLASS zcl_foo DEFINITION.
+  {
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
               PUBLIC SECTION.
                 METHODS:
                   foobar RETURNING VALUE(rv_string) TYPE string,
                   moobar RETURNING VALUE(rv_string) TYPE string.
-            ENDCLASS.`, cnt: 2},
+            ENDCLASS.`, cnt: 2,
+  },
 
   // method-definitions without chaining, one is lacking abapdoc
-  { abap: ` CLASS zcl_foo DEFINITION.
+  {
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
               PUBLIC SECTION.
                   METHODS foobar RETURNING VALUE(rv_string) TYPE string.
                   "! doc
                   METHODS moobar RETURNING VALUE(rv_string) TYPE string.
-            ENDCLASS.`, cnt: 1},
+            ENDCLASS.`, cnt: 1,
+  },
 
   // interface: two method definitions without abapdoc
-  { abap: ` INTERFACE zif_foo.
+  {
+    abap: ` INTERFACE zif_foo PUBLIC.
               METHODS:
                  foobar RETURNING VALUE(rv_string) TYPE string,
                  moobar RETURNING VALUE(rv_string) TYPE string.
-            ENDINTERFACE.`, cnt: 2},
+            ENDINTERFACE.`, cnt: 2,
+  },
 
   // interface: all methods have abapdoc
-  { abap: ` INTERFACE zif_foo.
+  {
+    abap: ` INTERFACE zif_foo PUBLIC.
               METHODS:
                  "! doc
                  foobar RETURNING VALUE(rv_string) TYPE string,
                  "! doc
                  moobar RETURNING VALUE(rv_string) TYPE string.
-            ENDINTERFACE.`, cnt: 0},
+            ENDINTERFACE.`, cnt: 0,
+  },
+
+  // local interface: no check with default config
+  {
+    abap: ` INTERFACE zif_foo.
+              METHODS:
+                 foobar RETURNING VALUE(rv_string) TYPE string,
+                 moobar RETURNING VALUE(rv_string) TYPE string.
+            ENDINTERFACE.`, cnt: 0,
+  },
+
+  // local class: no check with default config
+  {
+    abap: ` CLASS lcl_foo DEFINITION.
+              PUBLIC SECTION.
+                METHODS:
+                  foobar RETURNING VALUE(rv_string) TYPE string,
+                  moobar RETURNING VALUE(rv_string) TYPE string.
+              PROTECTED SECTION.
+                METHODS:
+                  coobar RETURNING VALUE(rv_string) TYPE string,
+                  doobar RETURNING VALUE(rv_string) TYPE string.
+            ENDCLASS.`, cnt: 0,
+  },
 ];
 
-testRule(tests, Abapdoc);
+testRule(defaultConfigTests, Abapdoc);
+
+
+const localCheckActiveTests = [
+  // local class, check active, missing abapdoc
+  {
+    abap: ` CLASS lcl_foo DEFINITION.
+              PUBLIC SECTION.
+                METHODS:
+                  foobar RETURNING VALUE(rv_string) TYPE string,
+                  moobar RETURNING VALUE(rv_string) TYPE string.
+              PROTECTED SECTION.
+                METHODS:
+                  coobar RETURNING VALUE(rv_string) TYPE string,
+                  doobar RETURNING VALUE(rv_string) TYPE string.
+            ENDCLASS.`, cnt: 2,
+  },
+
+  // local class, check active, required abapdoc provided
+  {
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+              METHODS:
+                "! doc
+                foobar RETURNING VALUE(rv_string) TYPE string,
+                "! doc
+                moobar RETURNING VALUE(rv_string) TYPE string.
+          ENDCLASS.`, cnt: 0,
+  },
+];
+
+const localCheckConfig = new AbapdocConf();
+localCheckConfig.checkLocal = true;
+
+testRule(localCheckActiveTests, Abapdoc, localCheckConfig);
