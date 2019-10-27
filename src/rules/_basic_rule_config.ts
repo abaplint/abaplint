8c03244bdae0c6bb19fcc0e9d195827459447b22
larshp <larshp@hotmail.com>
2019-10-27 17:24:16 +0100

    update description for rule exclude

--- src/rules/_basic_rule_config.ts
+++ src/rules/_basic_rule_config.ts
@@ -1,7 +1,7 @@
 export abstract class BasicRuleConfig {
   /** Is the rule enabled? */
   public enabled?: boolean = true;
-  /** List of patterns to exclude */
+  /** List of file regex patterns to exclude */
   public exclude?: string[] = [];
   /** An explanation for why the rule is enforced */
   public reason?: string = "";
