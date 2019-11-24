b7851f92ac6baa1176a46b5925380a6e7d04b54d
g-back <27279305+g-back@users.noreply.github.com>
2019-11-24 17:58:04 +0100

    newline_between_methods: Config Options (#647)
    * Add config options
    
    * Add function to test rules with specific config per case
    
    * Modify tests
    
    * Add uppercase conversion before string comparison
    
    * Remove 'newline' prefix in options

--- src/rules/whitespace/newline_between_methods.ts
+++ src/rules/whitespace/newline_between_methods.ts
@@ -5,7 +5,22 @@ import {ABAPFile} from "../../files";
 import {SequentialBlank} from "./sequential_blank";
 import * as Statements from "../../abap/statements";
 
-export class NewlineBetweenMethodsConf extends BasicRuleConfig {}
+export enum NewlineLogic {
+  Exact = "exact",
+  Less = "less",
+}
+
+/** Checks for newlines between method implementations. */
+export class NewlineBetweenMethodsConf extends BasicRuleConfig {
+  /** Amount of newlines, works in conjunction with "newlineLogic" */
+  public count: number = 3;
+  /**
+   *  Exact: the exact number of required newlines between methods is defined by "newlineAmount"
+   *
+   *  Less: the required number of newlines has to be less than "newlineAmount"
+   */
+  public logic: NewlineLogic = NewlineLogic.Less;
+}
 
 export class NewlineBetweenMethods extends ABAPRule {
   private conf = new NewlineBetweenMethodsConf();
@@ -15,7 +30,11 @@ export class NewlineBetweenMethods extends ABAPRule {
   }
 
   private getDescription(): string {
-    return `A single newline is required in between methods.`;
+    switch (this.conf.logic) {
+      case NewlineLogic.Exact: return `Exactly ${this.conf.count} newlines are required in between methods.`;
+      case NewlineLogic.Less: return `Less than ${this.conf.count} newlines are required in between methods.`;
+      default: return "";
+    }
   }
 
   public getConfig() {
@@ -29,19 +48,37 @@ export class NewlineBetweenMethods extends ABAPRule {
   public runParsed(file: ABAPFile): Issue[] {
     const issues: Issue[] = [];
     const rows = file.getRawRows();
+    if (!this.isConfigurationValid()) {
+      return [];
+    }
     for (const statement of file.getStatements()) {
-      const nextRow = statement.getStart().getRow();
-      if (!(statement.get() instanceof Statements.EndMethod)
-          || (rows[nextRow].includes("ENDCLASS.")
-          || (SequentialBlank.isBlankOrWhitespace(rows[nextRow]) && !SequentialBlank.isBlankOrWhitespace(rows[nextRow + 1])))) {
+      let nextRow = statement.getStart().getRow();
+      if (!(statement.get() instanceof Statements.EndMethod) || (rows[nextRow].toUpperCase().includes("ENDCLASS."))) {
         continue;
       }
-      issues.push(Issue.atStatement(
-        file,
-        statement,
-        this.getDescription(),
-        this.getKey()));
+      let counter = 0;
+      while (SequentialBlank.isBlankOrWhitespace(rows[nextRow]) && (counter <= this.conf.count + 1)) {
+        counter++;
+        nextRow++;
+      }
+      if ((counter !== this.conf.count && this.conf.logic === NewlineLogic.Exact)
+        || (counter >= this.conf.count && this.conf.logic === NewlineLogic.Less)
+        || counter === 0) {
+        issues.push(Issue.atStatement(
+          file,
+          statement,
+          this.getDescription(),
+          this.getKey()));
+      }
     }
     return issues;
+  }
+
+  private isConfigurationValid(): boolean {
+    if (this.conf.count < 1 || (this.conf.count === 1 && this.conf.logic === NewlineLogic.Less)) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
