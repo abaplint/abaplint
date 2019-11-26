bb03363af34dc08a6245535a4b279d10f8c2c328
GB <27279305+g-back@users.noreply.github.com>
2019-11-26 11:31:36 +0100

    Add option for exception naming pattern + refactoring

--- src/rules/naming/local_class_naming.ts
+++ src/rules/naming/local_class_naming.ts
@@ -1,11 +1,6 @@
 import {Issue} from "../../issue";
 import {ABAPRule} from "../_abap_rule";
 import {ABAPFile} from "../../files";
-import {ClassDefinition} from "../../abap/statements";
-import {IObject} from "../../objects/_iobject";
-import {Registry} from "../../registry";
-import {ClassName} from "../../abap/expressions";
-import {Class} from "../../objects";
 import {NamingRuleConfig} from "../_naming_rule_config";
 import {NameValidator} from "../../utils/name_validator";
 
@@ -13,6 +8,8 @@ import {NameValidator} from "../../utils/name_validator";
 export class LocalClassNamingConf extends NamingRuleConfig {
   /** The pattern for local class names */
   public local: string = "^LCL_.+$";
+  /** The pattern for local exception names */
+  public exception: string = "^LCX_.+$";
   /** The pattern for local test class names */
   public test: string = "^LTCL_.+$";
 }
@@ -28,7 +25,7 @@ export class LocalClassNaming extends ABAPRule {
   private getDescription(expected: string, actual: string): string {
     return this.conf.patternKind === "required" ?
       "Local class name does not match pattern " + expected + ": " + actual :
-      "Local class name must not match pattern " + expected + ": " + actual ;
+      "Local class name must not match pattern " + expected + ": " + actual;
   }
 
   public getConfig(): LocalClassNamingConf {
@@ -39,43 +36,34 @@ export class LocalClassNaming extends ABAPRule {
     this.conf = conf;
   }
 
-  public runParsed(file: ABAPFile, _reg: Registry, obj: IObject) {
+  public runParsed(file: ABAPFile): Issue[] {
     const issues: Issue[] = [];
-    const testRegex = new RegExp(this.conf.test, "i");
-    const localRegex = new RegExp(this.conf.local, "i");
 
-    for (const stat of file.getStatements()) {
-      if (!(stat.get() instanceof ClassDefinition)) {
+    for (const classDef of file.getClassDefinitions()) {
+      if (classDef.isGlobal()) {
         continue;
       }
 
-      const expr = stat.findFirstExpression(ClassName);
-      if (!expr) {
-        continue;
-      }
-      const token = expr.getFirstToken();
-      const name = token.getStr();
-      if (obj instanceof Class && name.toUpperCase() === obj.getName().toUpperCase()) {
-        continue;
-      }
-
+      const className = classDef.getName();
       let expected = "";
-      if (stat.concatTokens().toUpperCase().includes("FOR TESTING")) {
-        if (NameValidator.violatesRule(name, testRegex, this.conf)) {
-          expected = this.conf.test;
-        }
-      } else {
-        if (NameValidator.violatesRule(name, localRegex, this.conf)) {
-          expected = this.conf.local;
-        }
-      }
 
-      if (expected.length > 0) {
-        const issue = Issue.atToken(file, token, this.getDescription(expected, name), this.getKey());
-        issues.push(issue);
+      if (classDef.isForTesting()) {
+        expected = this.conf.test;
+      } else if (classDef.isException()) {
+        expected = this.conf.exception;
+      } else {
+        expected = this.conf.local;
+      }
+      const regex = new RegExp(expected, "i");
+
+      if (NameValidator.violatesRule(className, regex, this.conf)) {
+        issues.push(
+          Issue.atIdentifier(
+            classDef,
+            this.getDescription(expected, className),
+            this.getKey()));
       }
     }
-
     return issues;
   }
 
