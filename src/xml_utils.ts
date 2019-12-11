81f26e94aba6b2a30eca051db53a5d833775a25f
larshp <larshp@hotmail.com>
2019-12-11 07:58:51 +0100

    2.16.5, bugfix

--- src/xml_utils.ts
+++ src/xml_utils.ts
@@ -1,6 +1,6 @@
 export function xmlToArray(data: any) {
   if (data === undefined) {
-    return undefined;
+    return [];
   } else if (data.length) { // input data is an Array
     return data;
   } else {
