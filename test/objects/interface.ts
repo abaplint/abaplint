91fd2ffdaa6ded576e0b5a8d4c8fde7ced2fbe8a
larshp <larshp@hotmail.com>
2019-12-22 20:21:11 +0100

    description_empty, check intf, closes #669

--- test/objects/interface.ts
+++ test/objects/interface.ts
@@ -3,8 +3,6 @@ import {Registry} from "../../src/registry";
 import {MemoryFile} from "../../src/files/memory_file";
 import {Interface} from "../../src/objects";
 
-// todo, most(all?) of these tests to be moved to abap/types/
-
 describe("Objects, interface, isGeneratedProxy", () => {
   it("test, positive", () => {
     const abap = "INTERFACE zif_foobar PUBLIC.\n" +
@@ -33,5 +31,31 @@ describe("Objects, interface, isGeneratedProxy", () => {
     reg.parse();
     const intf = reg.getABAPObjects()[0] as Interface;
     expect(intf.isGeneratedProxy()).to.equal(true);
+  });
+});
+
+describe("Objects, interface, getDescription", () => {
+  it("test, positive", () => {
+    const xml = `<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_INTF" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <VSEOINTERF>
+    <CLSNAME>ZIF_ABAPGIT_AUTH</CLSNAME>
+    <LANGU>E</LANGU>
+    <DESCRIPT>Authorizations</DESCRIPT>
+    <EXPOSURE>2</EXPOSURE>
+    <STATE>1</STATE>
+    <UNICODE>X</UNICODE>
+   </VSEOINTERF>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+
+    const reg = new Registry();
+    reg.addFile(new MemoryFile("zif_foobar.intf.xml", xml));
+    reg.parse();
+    const intf = reg.getABAPObjects()[0] as Interface;
+    expect(intf.getDescription()).to.equal("Authorizations");
   });
 });
