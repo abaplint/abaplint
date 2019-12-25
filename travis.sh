570fa908cdc0f37ca030a5c06de17c010318e4d3
Lars Hvam <larshp@hotmail.com>
2019-12-25 12:46:58 +0100

    CI: Github Actions changes (#688)
    * Update coverage.yml
    
    * Update playground.yml
    
    * Update travis.sh
    
    * deploy to playground
    
    * Update main.yml

--- travis.sh
+++ travis.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 cd ..
-git clone https://github.com/abaplint/playground.abaplint.org.git
 git clone https://github.com/abaplint/syntax.abaplint.org.git
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-
-cp abaplint/web/playground/build/* playground.abaplint.org/
-cd playground.abaplint.org
-git status
-git add -A
-git status
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -q https://$GITHUB_API_KEY@github.com/abaplint/playground.abaplint.org.git > /dev/null 2>&1
-
-cd ..
 
 cp -r abaplint/web/syntax/* syntax.abaplint.org/
 cd syntax.abaplint.org
