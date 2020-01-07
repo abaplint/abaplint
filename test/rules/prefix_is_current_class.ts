4815899d1465cc22c069a9ddc53cdf1d6ea9173b
GB <27279305+g-back@users.noreply.github.com>
2020-01-07 14:52:15 +0100

    Add unit tests

---
+++ test/rules/prefix_is_current_class.ts
@@ -0,0 +1,137 @@
+import {testRule} from "./_utils";
+import {PrefixIsCurrentClass, PrefixIsCurrentClassConf} from "../../src/rules";
+
+const defaultConfigTests = [
+
+  {
+    // static reference to own class type
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                TYPES: BEGIN OF ty_foo,
+                         foo TYPE i,
+                       END OF ty_foo.
+                METHODS foobar RETURNING VALUE(rv_string) TYPE zcl_foo=>ty_foo.
+            ENDCLASS.`, cnt: 1,
+  },
+  {
+    // static reference to own class type without class name
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                TYPES: BEGIN OF ty_foo,
+                         foo TYPE i,
+                       END OF ty_foo.
+                METHODS foobar RETURNING VALUE(rv_string) TYPE ty_foo.
+              PROTECTED SECTION.
+                DATA mv_foo TYPE i.
+            ENDCLASS.`, cnt: 0,
+  },
+  {
+    // static reference to own class in string and comment
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                METHODS foobar.
+            ENDCLASS.
+            CLASS zcl_foo IMPLEMENTATION.
+              METHOD foobar.
+                DATA(_string) = |zcl_foo=>foo( )|.
+                WRITE: 'zcl_foo=>foo( )'.
+                WRITE: 'foo'. " zcl_foo=>foo( )
+              ENDMETHOD.
+            ENDCLASS.`, cnt: 0,
+  },
+  {
+    // static reference to own class class-method
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                METHODS foobar.
+                CLASS-METHODS moobar.
+              PROTECTED SECTION.
+                DATA mv_foo TYPE i.
+            ENDCLASS.
+            CLASS zcl_foo IMPLEMENTATION.
+              METHOD foobar.
+                zcl_foo=>moobar( ).
+              ENDMETHOD.
+
+              METHOD moobar.
+                WRITE: '1'.
+              ENDMETHOD.
+            ENDCLASS.`, cnt: 1,
+  },
+  {
+    // me-> reference instance method
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                METHODS foobar.
+                METHODS moobar.
+              PROTECTED SECTION.
+                DATA mv_foo TYPE i.
+            ENDCLASS.
+            CLASS zcl_foo IMPLEMENTATION.
+              METHOD foobar.
+                me->moobar( ).
+              ENDMETHOD.
+
+              METHOD moobar.
+                WRITE: '1'.
+              ENDMETHOD.
+            ENDCLASS.`, cnt: 1,
+  },
+  {
+    // me-> reference instance attribute
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                METHODS foobar.
+              PROTECTED SECTION.
+                DATA mv_foo TYPE i.
+            ENDCLASS.
+            CLASS zcl_foo IMPLEMENTATION.
+              METHOD foobar.
+                me->mv_foo = 1.
+              ENDMETHOD.
+            ENDCLASS.`, cnt: 0,
+  },
+];
+
+testRule(defaultConfigTests, PrefixIsCurrentClass);
+
+const meReferenceAllowedTests = [
+  {
+    // me-> reference instance method
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                METHODS foobar.
+                METHODS moobar.
+              PROTECTED SECTION.
+                DATA mv_foo TYPE i.
+            ENDCLASS.
+            CLASS zcl_foo IMPLEMENTATION.
+              METHOD foobar.
+                me->moobar( ).
+              ENDMETHOD.
+
+              METHOD moobar.
+                WRITE: '1'.
+              ENDMETHOD.
+            ENDCLASS.`, cnt: 0,
+  },
+  {
+    // me-> reference instance attribute
+    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
+              PUBLIC SECTION.
+                METHODS foobar.
+              PROTECTED SECTION.
+                DATA mv_foo TYPE i.
+            ENDCLASS.
+            CLASS zcl_foo IMPLEMENTATION.
+              METHOD foobar.
+                me->mv_foo = 1.
+              ENDMETHOD.
+            ENDCLASS.`, cnt: 0,
+  },
+];
+
+const confMeAllowed = new PrefixIsCurrentClassConf();
+confMeAllowed.omitMeInstanceCalls = false;
+
+testRule(meReferenceAllowedTests, PrefixIsCurrentClass, confMeAllowed);
