67e6f6bdfa4010757c8dfc5f0c3af7aab34bc88d
GB <27279305+g-back@users.noreply.github.com>
2020-01-10 12:28:55 +0100

    add more elements to format options

--- src/abap/expressions/string_template.ts
+++ src/abap/expressions/string_template.ts
@@ -5,12 +5,49 @@ import {Source} from "./source";
 
 export class StringTemplate extends Expression {
   public getRunnable(): IStatementRunnable {
-    const formatting = alt(str("ALPHA = OUT"),
-                           str("TIME = USER"),
-                           str("DATE = USER"),
-                           str("TIME = ENVIRONMENT"),
-                           str("DATE = ENVIRONMENT"),
-                           seq(str("WIDTH ="), new Source()));
+
+    // https://help.sap.com/doc/abapdocu_752_index_htm/7.52/en-us/abapcompute_string_format_options.htm
+    const alphaOptions = alt(str("OUT"),
+                             str("RAW"),
+                             str("IN"));
+
+    const alignOptions = alt(str("LEFT"),
+                             str("RIGHT"),
+                             str("CENTER"));
+
+    const dateTimeOptions = alt(str("RAW"),
+                                str("ISO"),
+                                str("USER"),
+                                str("ENVIRONMENT"));
+
+    const timeStampOptions = alt(str("SPACE"),
+                                 str("ISO"),
+                                 str("USER"),
+                                 str("ENVIRONMENT"));
+
+    const numberOptions = alt(str("RAW"),
+                              str("USER"),
+                              str("ENVIRONMENT"));
+
+    const signOptions = alt(str("LEFT"),
+                            str("LEFTPLUS"),
+                            str("LEFTSPACE"),
+                            str("RIGHT"),
+                            str("RIGHTPLUS"),
+                            str("RIGHTSPACE"));
+
+    const caseOptions = alt(str("RAW"),
+                            str("UPPER"),
+                            str("LOWER"));
+
+    const formatting = alt(seq(str("ALPHA"), str("="), alphaOptions),
+                           seq(str("TIME"), str("="), dateTimeOptions),
+                           seq(str("DATE"), str("="), dateTimeOptions),
+                           seq(str("TIMESTAMP"), str("="), timeStampOptions),
+                           seq(str("NUMBER"), str("="), numberOptions),
+                           seq(str("SIGN"), str("="), signOptions),
+                           seq(str("CASE"), str("="), caseOptions),
+                           seq(str("WIDTH"), str("="), new Source(), opt(seq(str("ALIGN"), str("="), alignOptions))));
 
     const nest = seq(tok(Tokens.StringTemplateBegin),
                      new Source(),
