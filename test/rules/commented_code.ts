b252e79da0dbe98c7a9e29623e93580e78a7bd20
GB <27279305+g-back@users.noreply.github.com>
2020-01-22 11:33:45 +0100

    add abapdoc test cases

--- test/rules/commented_code.ts
+++ test/rules/commented_code.ts
@@ -27,6 +27,24 @@ const tests = [
        "cl_abap_unit_assert=>assert_char_cp(
        "   act = lv_abap
     "      exp = '*MOO*' ).`, cnt: 1},
+  {abap:
+    // abapdoc, allowed
+      `"! todo
+      "!    DATA(lv_abap) = mo_cut->build_meta( ls_generate ).
+      "!
+      "!    cl_abap_unit_assert=>assert_not_initial( lv_abap ).
+         "!cl_abap_unit_assert=>assert_char_cp(
+         "!   act = lv_abap
+      "!      exp = '*MOO*' ).`, cnt: 0},
+  {abap:
+    // mix of abapdoc and standard comments
+        `" todo
+        "    DATA(lv_abap) = mo_cut->build_meta( ls_generate ).
+        "
+        "    cl_abap_unit_assert=>assert_not_initial( lv_abap ).
+           "!cl_abap_unit_assert=>assert_char_cp(
+           "!   act = lv_abap
+        "!      exp = '*MOO*' ).`, cnt: 1},
 ];
 
 testRule(tests, CommentedCode);
