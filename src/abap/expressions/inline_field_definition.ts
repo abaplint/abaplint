a2a546f61c3c75bcc06ad739df8a3028cb300bf2
GB <27279305+g-back@users.noreply.github.com>
2020-01-27 16:17:36 +0100

    modify init and inlinefielddefinition

--- src/abap/expressions/inline_field_definition.ts
+++ src/abap/expressions/inline_field_definition.ts
@@ -1,8 +1,10 @@
-import {Expression, IStatementRunnable, seq, str} from "../combi";
+import {Expression, IStatementRunnable, seq, str, alt} from "../combi";
 import {Field, Source} from ".";
+import {TypeName} from "./type_name";
 
 export class InlineFieldDefinition extends Expression {
   public getRunnable(): IStatementRunnable {
-    return seq(new Field(), str("="), new Source());
+    return alt(seq(new Field(), str("TYPE"), new TypeName()),
+               seq(new Field(), str("="), new Source()));
   }
 }
