1c481b545bf4954f5d6d81f0c7f0fdd2b1f59513
larshp <larshp@hotmail.com>
2020-02-19 14:13:22 +0100

    web syntax: fix linter errors

--- web/syntax/script.js
+++ web/syntax/script.js
@@ -9,7 +9,7 @@ function inputChanged() {
   const value = document.getElementById('filter').value;
   let newUrl = window.location.pathname;
   if (value !== "") {
-    newUrl = newUrl +"?filter=" + document.getElementById('filter').value;
+    newUrl = newUrl + "?filter=" + document.getElementById('filter').value;
   }
   window.history.replaceState(null, document.title, newUrl);
 }
@@ -21,7 +21,7 @@ function setFilter(filter) {
 
 function renderSidenavList(list) {
   let html = "";
-  for(let i of list) {
+  for(const i of list) {
     html = html + "<a href=\"javascript:setFilter('" + i.name + "');\">" + i.name + "</a><br>";
   }
 
@@ -36,7 +36,7 @@ function renderLeft() {
 
 function renderList(filter, list) {
   let ret = "";
-  for(let i of list) {
+  for(const i of list) {
     if (!filter || i.name.toLowerCase().includes(filter.toLowerCase())) {
       ret = ret + "<div style=\"page-break-inside:avoid;\">" +
         "<u>" + i.name + "</u><br>" +
@@ -72,7 +72,7 @@ function renderSyntax(type, name) {
   let found = {};
   let prev = {};
   let next = {};
-  let list = json[type+"s"];
+  const list = json[type + "s"];
   for(let index = 0; index < list.length; index++) {
     if (list[index].name === name) {
       found = list[index];
@@ -93,8 +93,8 @@ function renderSyntax(type, name) {
   }
   html = html + "<a href=\"https://github.com/abaplint/abaplint/blob/master/src/abap/" + found.type + "s/" + found.filename + "\">Source</a><br>";
 
-  let use = found.using.map((e) => { return "<a href=\"#/" + e + "\">" + e + "</a>"; });
-  let by = found.used_by.map((e) => { return "<a href=\"#/" + e + "\">" + e + "</a>"; });
+  const use = found.using.map((e) => { return "<a href=\"#/" + e + "\">" + e + "</a>"; });
+  const by = found.used_by.map((e) => { return "<a href=\"#/" + e + "\">" + e + "</a>"; });
 
   html = html + found.svg + "<br>\n" +
     "<b>Using</b>: " + use.join(", ") + "<br>\n" +
@@ -108,7 +108,7 @@ class Router {
     if(window.location.hash === "") {
       renderMain();
     } else {
-      let split = window.location.hash.split("/");
+      const split = window.location.hash.split("/");
       renderSyntax(split[1], split[2]);
     }
   }
@@ -121,8 +121,6 @@ function onRightClick() {
 }
 
 function renderMain() {
-  console.dir(window.location);
-
   let filter = new URL(window.location).searchParams.get("filter");
   if (filter === null) {
     filter = "";
