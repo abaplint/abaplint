64350ab2f151de25a3f54aa5dfc1905aec001c5a
GB <27279305+g-back@users.noreply.github.com>
2020-03-10 08:56:09 +0100

    add test cases for dash-usage

--- test/rules/identical_form_names.ts
+++ test/rules/identical_form_names.ts
@@ -6,12 +6,9 @@ const tests = [
   {abap: "FORM foobar.\nENDFORM.", cnt: 0},
   {abap: "FORM moo.\nENDFORM.\nFORM boo.\nENDFORM.", cnt: 0},
   {abap: "FORM abc.\nENDFORM.\nFORM ABC.\nENDFORM.", cnt: 1},
-  /*
-  {abap: `FORM foo-bar.
-  ENDFORM.
-  FORM foo-moo.
-  ENDFORM.`, cnt: 0},
-  */
+  {abap: "FORM abc-dash.\nENDFORM.\nFORM ABC-dash.\nENDFORM.", cnt: 1},
+  {abap: "FORM abc.\nENDFORM.\nFORM ABC-dash.\nENDFORM.", cnt: 0},
+  {abap: `FORM foo-bar. \nENDFORM. \n FORM foo-moo. \nENDFORM.`, cnt: 0},
 ];
 
 testRule(tests, IdenticalFormNames);
