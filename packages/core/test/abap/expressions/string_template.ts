a31d91858f6802dbf987e96fa61cd8c3bb1f4c99
larshp <larshp@hotmail.com>
2020-04-26 14:46:17 +0200

    parsing, string template formatting, closes #845

---
+++ packages/core/test/abap/expressions/string_template.ts
@@ -0,0 +1,22 @@
+import {expect} from "chai";
+import * as Combi from "../../../src/abap/2_statements/combi";
+import * as Expressions from "../../../src/abap/2_statements/expressions";
+import {getTokens} from "../_utils";
+import {Config} from "../../../src/config";
+
+const tests = [
+  {c: "|hello|", r: new Expressions.StringTemplate(),      e: true},
+  {c: "|{ condense( iv_fnam ) }({ iv_index ALIGN = RIGHT PAD = '0' WIDTH = len })|", r: new Expressions.StringTemplate(), e: true},
+];
+
+describe("Test expression matchers", () => {
+  tests.forEach((test) => {
+    const not = test.e === true ? "" : "not ";
+
+    it("\"" + test.c + "\" should " + not + "match " + test.r.getName(), () => {
+      const tokens = getTokens(test.c);
+      const match = Combi.Combi.run(test.r.getRunnable(), tokens, Config.getDefault().getVersion());
+      expect(match !== undefined).to.equals(test.e);
+    });
+  });
+});
