5d162f81d3abe872b4c8fdf6d7675fc114ffe8f4
g-back <27279305+g-back@users.noreply.github.com>
2020-05-07 10:06:48 +0200

    class_attribute_names: add constants, add ignoreLocal (#872)
    * refactor, add constant, add local ignore
    
    * remove comments
    
    * add additional test cases
    
    * fix import, add schema changes
    
    * change default pattern to any, modify tests to reflect

--- packages/core/src/rules/naming/class_attribute_names.ts
+++ packages/core/src/rules/naming/class_attribute_names.ts
@@ -1,23 +1,25 @@
 import {Issue} from "../../issue";
-import {IObject} from "../../objects/_iobject";
-import {Class} from "../../objects";
-import {IRegistry} from "../../_iregistry";
 import {IAttributes} from "../../abap/types/_class_attributes";
-import {ClassAttribute} from "../../abap/types/class_attribute";
 import {NamingRuleConfig} from "../_naming_rule_config";
-import {IRule} from "../_irule";
 import {NameValidator} from "../../utils/name_validator";
+import {ABAPRule} from "../_abap_rule";
+import {ABAPFile} from "../../files";
+import {Identifier} from "../../abap/types/_identifier";
 
 export class ClassAttributeNamesConf extends NamingRuleConfig {
   /** Ignore global exception classes */
   public ignoreExceptions: boolean = true;
+  /** Ignore local classes */
+  public ignoreLocal: boolean = true;
   /** The pattern for static variable names */
   public statics: string = "^G._.+$";
   /** The pattern for instance variable names */
   public instance: string = "^M._.+$";
+  /** The pattern for constant variable names */
+  public constants: string = "";
 }
 
-export class ClassAttributeNames implements IRule {
+export class ClassAttributeNames extends ABAPRule {
 
   private conf = new ClassAttributeNamesConf();
 
@@ -33,7 +35,7 @@ export class ClassAttributeNames implements IRule {
   private getDescription(expected: string, actual: string): string {
     return this.conf.patternKind === "required" ?
       "Class attribute name does not match pattern " + expected + ": " + actual :
-      "Class attribute name must not match pattern " + expected + ": " + actual ;
+      "Class attribute name must not match pattern " + expected + ": " + actual;
   }
 
   public getConfig() {
@@ -44,36 +46,28 @@ export class ClassAttributeNames implements IRule {
     this.conf = conf;
   }
 
-  public run(obj: IObject, _reg: IRegistry): Issue[] {
-    let attr: IAttributes | undefined = undefined;
+  public runParsed(file: ABAPFile): Issue[] {
+    let issues: Issue[] = [];
     if (this.conf.patternKind === undefined) {
       this.conf.patternKind = "required";
     }
-// todo, consider local classes(PROG, FUGR, CLAS)
-
-    if (obj instanceof Class) {
-      const definition = obj.getClassDefinition();
-      if (definition === undefined) {
-        return [];
+    let attributes: IAttributes[] = [];
+    for (const classDef of file.getInfo().getClassDefinitions()) {
+      if ((classDef.isLocal() && this.conf.ignoreLocal)
+        || (classDef.isException() && this.conf.ignoreExceptions)) {
+        continue;
       }
-      if (this.conf.ignoreExceptions && definition.isException()) {
-        return [];
-      }
-      attr = definition.getAttributes();
-      if (attr === undefined) {
-        return [];
-      }
-// todo, INTF
-//    } else if (obj instanceof Interface) {
-//      methods = obj();
+      attributes = attributes.concat(classDef.getAttributes());
     }
 
-
-    return this.checkAttributes(attr);
+    for (const attribute of attributes) {
+      issues = issues.concat(this.checkAttributes(attribute));
+    }
+    return issues;
   }
 
   private checkAttributes(attr: IAttributes | undefined): Issue[] {
-    if (!attr) { return []; }
+    if (!attr) {return [];}
     let ret: Issue[] = [];
 
     for (const ins of attr.getInstance()) {
@@ -84,10 +78,14 @@ export class ClassAttributeNames implements IRule {
       ret = ret.concat(this.checkName(sta, this.conf.statics));
     }
 
+    for (const con of attr.getConstants()) {
+      ret = ret.concat(this.checkName(con, this.conf.constants));
+    }
+
     return ret;
   }
 
-  private checkName(attr: ClassAttribute, expected: string): Issue[] {
+  private checkName(attr: Identifier, expected: string): Issue[] {
     const ret: Issue[] = [];
     const regex = new RegExp(expected, "i");
     const name = attr.getName();
