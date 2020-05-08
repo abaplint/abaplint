d15c1dc9db8e56dfa99bee29ceb7628a9037a091
Marcello Urbani <marcello.urbani@gmail.com>
2020-05-08 08:14:01 +0100

    ABAPObject type guard

--- packages/core/src/objects/_abap_object.ts
+++ packages/core/src/objects/_abap_object.ts
@@ -7,6 +7,7 @@ import {IClassImplementation} from "../abap/types/_class_implementation";
 import {IObject} from "./_iobject";
 import {Version} from "../version";
 
+const tag = Symbol("ABAPObject");
 export interface ITextElement {
   key: string;
   text: string;
@@ -14,10 +15,17 @@ export interface ITextElement {
 
 export abstract class ABAPObject extends AbstractObject {
   private parsed: readonly ABAPFile[];
+  private [tag] = true;
 
   public constructor(name: string) {
     super(name);
     this.parsed = [];
+  }
+
+  public static is(x:any):x is ABAPObject // silly overload to remove unused warning
+  // eslint-disable-next-line no-dupe-class-members
+  public static is(x:ABAPObject):x is ABAPObject{
+    return !!x?.[tag];
   }
 
   public parse(version: Version, globalMacros: readonly string[] | undefined): IObject {
