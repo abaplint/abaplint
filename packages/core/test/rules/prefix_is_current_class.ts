8b80ce42a2bacf8150aae799cb93017f48d0cb68
larshp <larshp@hotmail.com>
2020-05-24 12:55:59 +0200

    refactor test for prefix_is_current_class

--- packages/core/test/rules/prefix_is_current_class.ts
+++ packages/core/test/rules/prefix_is_current_class.ts
@@ -1,153 +1,181 @@
-import {testRule} from "./_utils";
+import {expect} from "chai";
 import {PrefixIsCurrentClass, PrefixIsCurrentClassConf} from "../../src/rules";
+import {MemoryFile} from "../../src/files/memory_file";
+import {Registry} from "../../src/registry";
 
-const defaultConfigTests = [
+function run(abap: string, config?: PrefixIsCurrentClassConf): number {
+  const reg = new Registry().addFile(new MemoryFile("zfoo.prog.abap", abap)).parse();
+  const rule = new PrefixIsCurrentClass();
+  if (config) {
+    rule.setConfig(config);
+  }
+  const issues = rule.run(reg.getObjects()[0], reg);
+  return issues.length;
+}
 
-  {
-    // static reference to own class type
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                TYPES: BEGIN OF ty_foo,
-                         foo TYPE i,
-                       END OF ty_foo.
-                METHODS foobar RETURNING VALUE(rv_string) TYPE zcl_foo=>ty_foo.
-            ENDCLASS.`, cnt: 1,
-  },
-  {
-    // static reference to own class type without class name
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                TYPES: BEGIN OF ty_foo,
-                         foo TYPE i,
-                       END OF ty_foo.
-                METHODS foobar RETURNING VALUE(rv_string) TYPE ty_foo.
-              PROTECTED SECTION.
-                DATA mv_foo TYPE i.
-            ENDCLASS.`, cnt: 0,
-  },
-  {
-    // static reference to own class in string and comment
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                METHODS foobar.
-            ENDCLASS.
-            CLASS zcl_foo IMPLEMENTATION.
-              METHOD foobar.
-                DATA(_string) = |zcl_foo=>foo( )|.
-                WRITE: 'zcl_foo=>foo( )'.
-                WRITE: 'foo'. " zcl_foo=>foo( )
-              ENDMETHOD.
-            ENDCLASS.`, cnt: 0,
-  },
-  {
-    // static reference to own class in string template source
-    abap: ` CLASS lcl_moo DEFINITION.
-            PUBLIC SECTION.
-              METHODS abc.
-            PROTECTED SECTION.
-              CLASS-DATA mv_abc TYPE i.
-          ENDCLASS.
-          CLASS lcl_moo IMPLEMENTATION.
-            METHOD abc.
-              WRITE: | { lcl_moo=>mv_abc } |.
-              WRITE: | { mv_abc } |.
-              WRITE: |lcl_moo=>mv_abc{ 2 }lcl_moo=>mv_abc{ 3 }lcl_moo=>mv_abc{ 4 }|.
-            ENDMETHOD.
-          ENDCLASS.`, cnt: 1,
-  },
-  {
-    // static reference to own class class-method
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                METHODS foobar.
-                CLASS-METHODS moobar.
-              PROTECTED SECTION.
-                DATA mv_foo TYPE i.
-            ENDCLASS.
-            CLASS zcl_foo IMPLEMENTATION.
-              METHOD foobar.
-                zcl_foo=>moobar( ).
-              ENDMETHOD.
+describe("prefix is current class, default Config", () => {
 
-              METHOD moobar.
-                WRITE: '1'.
-              ENDMETHOD.
-            ENDCLASS.`, cnt: 1,
-  },
-  {
-    // me-> reference instance method
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                METHODS foobar.
-                METHODS moobar.
-              PROTECTED SECTION.
-                DATA mv_foo TYPE i.
-            ENDCLASS.
-            CLASS zcl_foo IMPLEMENTATION.
-              METHOD foobar.
-                me->moobar( ).
-              ENDMETHOD.
+  it("static reference to own class type", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      TYPES: BEGIN OF ty_foo,
+               foo TYPE i,
+             END OF ty_foo.
+      METHODS foobar RETURNING VALUE(rv_string) TYPE zcl_foo=>ty_foo.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(1);
+  });
 
-              METHOD moobar.
-                WRITE: '1'.
-              ENDMETHOD.
-            ENDCLASS.`, cnt: 1,
-  },
-  {
-    // me-> reference instance attribute
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                METHODS foobar.
-              PROTECTED SECTION.
-                DATA mv_foo TYPE i.
-            ENDCLASS.
-            CLASS zcl_foo IMPLEMENTATION.
-              METHOD foobar.
-                me->mv_foo = 1.
-              ENDMETHOD.
-            ENDCLASS.`, cnt: 0,
-  },
-];
+  it("static reference to own class type without class name", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      TYPES: BEGIN OF ty_foo,
+               foo TYPE i,
+             END OF ty_foo.
+      METHODS foobar RETURNING VALUE(rv_string) TYPE ty_foo.
+    PROTECTED SECTION.
+      DATA mv_foo TYPE i.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(0);
+  });
 
-testRule(defaultConfigTests, PrefixIsCurrentClass);
+  it("static reference to own class in string and comment", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      METHODS foobar.
+  ENDCLASS.
+  CLASS zcl_foo IMPLEMENTATION.
+    METHOD foobar.
+      DATA(_string) = |zcl_foo=>foo( )|.
+      WRITE: 'zcl_foo=>foo( )'.
+      WRITE: 'foo'. " zcl_foo=>foo( )
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(0);
+  });
 
-const meReferenceAllowedTests = [
-  {
-    // me-> reference instance method
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                METHODS foobar.
-                METHODS moobar.
-              PROTECTED SECTION.
-                DATA mv_foo TYPE i.
-            ENDCLASS.
-            CLASS zcl_foo IMPLEMENTATION.
-              METHOD foobar.
-                me->moobar( ).
-              ENDMETHOD.
+  it("static reference to own class in string template source", async () => {
+    const abap = `CLASS lcl_moo DEFINITION.
+    PUBLIC SECTION.
+      METHODS abc.
+    PROTECTED SECTION.
+      CLASS-DATA mv_abc TYPE i.
+  ENDCLASS.
+  CLASS lcl_moo IMPLEMENTATION.
+    METHOD abc.
+      WRITE: | { lcl_moo=>mv_abc } |.
+      WRITE: | { mv_abc } |.
+      WRITE: |lcl_moo=>mv_abc{ 2 }lcl_moo=>mv_abc{ 3 }lcl_moo=>mv_abc{ 4 }|.
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(1);
+  });
 
-              METHOD moobar.
-                WRITE: '1'.
-              ENDMETHOD.
-            ENDCLASS.`, cnt: 0,
-  },
-  {
-    // me-> reference instance attribute
-    abap: ` CLASS zcl_foo DEFINITION PUBLIC.
-              PUBLIC SECTION.
-                METHODS foobar.
-              PROTECTED SECTION.
-                DATA mv_foo TYPE i.
-            ENDCLASS.
-            CLASS zcl_foo IMPLEMENTATION.
-              METHOD foobar.
-                me->mv_foo = 1.
-              ENDMETHOD.
-            ENDCLASS.`, cnt: 0,
-  },
-];
+  it("static reference to own class class-method", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      METHODS foobar.
+      CLASS-METHODS moobar.
+    PROTECTED SECTION.
+      DATA mv_foo TYPE i.
+  ENDCLASS.
+  CLASS zcl_foo IMPLEMENTATION.
+    METHOD foobar.
+      zcl_foo=>moobar( ).
+    ENDMETHOD.
 
-const confMeAllowed = new PrefixIsCurrentClassConf();
-confMeAllowed.omitMeInstanceCalls = false;
+    METHOD moobar.
+      WRITE: '1'.
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(1);
+  });
 
-testRule(meReferenceAllowedTests, PrefixIsCurrentClass, confMeAllowed);
+  it("me-> reference instance method", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      METHODS foobar.
+      METHODS moobar.
+    PROTECTED SECTION.
+      DATA mv_foo TYPE i.
+  ENDCLASS.
+  CLASS zcl_foo IMPLEMENTATION.
+    METHOD foobar.
+      me->moobar( ).
+    ENDMETHOD.
+
+    METHOD moobar.
+      WRITE: '1'.
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(1);
+  });
+
+  it("me-> reference instance attribute", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      METHODS foobar.
+    PROTECTED SECTION.
+      DATA mv_foo TYPE i.
+  ENDCLASS.
+  CLASS zcl_foo IMPLEMENTATION.
+    METHOD foobar.
+      me->mv_foo = 1.
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap);
+    expect(issues).to.equal(0);
+  });
+
+});
+
+
+describe("prefix is current class, meReferenceAllowedTests", () => {
+
+  const config = new PrefixIsCurrentClassConf();
+  config.omitMeInstanceCalls = false;
+
+  it("me-> reference instance method", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      METHODS foobar.
+      METHODS moobar.
+    PROTECTED SECTION.
+      DATA mv_foo TYPE i.
+  ENDCLASS.
+  CLASS zcl_foo IMPLEMENTATION.
+    METHOD foobar.
+      me->moobar( ).
+    ENDMETHOD.
+
+    METHOD moobar.
+      WRITE: '1'.
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap, config);
+    expect(issues).to.equal(0);
+  });
+
+  it("me-> reference instance attribute", async () => {
+    const abap = `CLASS zcl_foo DEFINITION PUBLIC.
+    PUBLIC SECTION.
+      METHODS foobar.
+    PROTECTED SECTION.
+      DATA mv_foo TYPE i.
+  ENDCLASS.
+  CLASS zcl_foo IMPLEMENTATION.
+    METHOD foobar.
+      me->mv_foo = 1.
+    ENDMETHOD.
+  ENDCLASS.`;
+    const issues = run(abap, config);
+    expect(issues).to.equal(0);
+  });
+
+});
