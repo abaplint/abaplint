dc2eb4647ddd09f6e421d638fdcbe215a405d4d4
GB <27279305+g-back@users.noreply.github.com>
2020-06-02 13:09:39 +0200

    Add fix unit tests

--- packages/core/test/rules/preferred_compare_operator.ts
+++ packages/core/test/rules/preferred_compare_operator.ts
@@ -1,5 +1,5 @@
 import {PreferredCompareOperator} from "../../src/rules/preferred_compare_operator";
-import {testRule} from "./_utils";
+import {testRule, testRuleFix} from "./_utils";
 
 const tests = [
   {abap: "parser error", cnt: 0},
@@ -10,3 +10,17 @@ const tests = [
 ];
 
 testRule(tests, PreferredCompareOperator);
+
+
+
+const fixTests = [
+  {input: "IF foo EQ bar. ENDIF.", output: "IF foo = bar. ENDIF."},
+  {input: "IF foo NE bar. ENDIF.", output: "IF foo <> bar. ENDIF."},
+  {input: "IF foo >< bar. ENDIF.", output: "IF foo <> bar. ENDIF."},
+  {input: "IF foo GT bar. ENDIF.", output: "IF foo > bar. ENDIF."},
+  {input: "IF foo GE bar. ENDIF.", output: "IF foo >= bar. ENDIF."},
+  {input: "IF foo LE bar. ENDIF.", output: "IF foo <= bar. ENDIF."},
+  {input: "IF foo LT bar. ENDIF.", output: "IF foo < bar. ENDIF."},
+  {input: "SELECT * FROM foo INTO TABLE bar WHERE moo EQ boo.", output: "SELECT * FROM foo INTO TABLE bar WHERE moo = boo."},
+];
+testRuleFix(fixTests, PreferredCompareOperator);
