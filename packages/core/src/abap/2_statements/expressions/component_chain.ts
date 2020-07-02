62e553c1f6e0f4a8533c1971ea46c4b0b27b1d29
larshp <larshp@hotmail.com>
2020-07-02 15:57:28 +0200

    simplify chains

--- packages/core/src/abap/2_statements/expressions/component_chain.ts
+++ packages/core/src/abap/2_statements/expressions/component_chain.ts
@@ -1,4 +1,4 @@
-import {seq, opt, optPrio, alt, str, plus, star, Expression} from "../combi";
+import {seq, optPrio, alt, str, star, Expression} from "../combi";
 import {FieldLength, TableExpression, ArrowOrDash, ComponentName, FieldOffset} from ".";
 import {IStatementRunnable} from "../statement_runnable";
 
@@ -6,8 +6,7 @@ export class ComponentChain extends Expression {
   public getRunnable(): IStatementRunnable {
 
     const chain = seq(new ComponentName(),
-                      optPrio(plus(new TableExpression())),
-                      star(seq(new ArrowOrDash(), alt(str("*"), new ComponentName()), opt(plus(new TableExpression())))));
+                      star(alt(seq(new ArrowOrDash(), alt(str("*"), new ComponentName())), new TableExpression())));
 
     const ret = seq(chain, optPrio(new FieldOffset()), optPrio(new FieldLength()));
 
