d9b2853f2afccbf17e5dac26474493ee8ee54091
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2020-07-03 01:06:08 +0200

    Update tests

--- packages/core/test/rules/check_transformation_exists.ts
+++ packages/core/test/rules/check_transformation_exists.ts
@@ -12,7 +12,7 @@ function runMulti(files: {filename: string, contents: string}[]): Issue[] {
   reg.parse();
   let issues: Issue[] = [];
   for (const obj of reg.getObjects()) {
-    issues = issues.concat(new CheckTransformationExists().initialize(reg).run(obj));
+    issues = issues.concat(new CheckTransformationExists().run(obj, reg));
   }
   return issues;
 }
@@ -25,7 +25,7 @@ describe("Rules, check_transformation_exists", () => {
 
   it("not found", () => {
     const contents = `REPORT zfoo.
-      CALL TRANSFORMATION id
+      CALL TRANSFORMATION ztest
         SOURCE (lt_stab)
         RESULT XML li_doc.`;
     const issues = runMulti([{filename: "zfoo.prog.abap", contents}]);
@@ -43,12 +43,21 @@ describe("Rules, check_transformation_exists", () => {
 
   it("found, ok", () => {
     const contents = `REPORT zfoo.
-      CALL TRANSFORMATION id
+      CALL TRANSFORMATION ztest
         SOURCE (lt_stab)
         RESULT XML li_doc.`;
     const issues = runMulti([
       {filename: "zfoo.prog.abap", contents},
-      {filename: "id.xslt.xml", contents: "<foo></foo>"}]);
+      {filename: "ztest.xslt.xml", contents: "<foo></foo>"}]);
+    expect(issues.length).to.equals(0);
+  });
+
+  it("id transformation", () => {
+    const contents = `REPORT zfoo.
+      CALL TRANSFORMATION id
+        SOURCE (lt_stab)
+        RESULT XML li_doc.`;
+    const issues = runMulti([{filename: "zfoo.prog.abap", contents}]);
     expect(issues.length).to.equals(0);
   });
 
