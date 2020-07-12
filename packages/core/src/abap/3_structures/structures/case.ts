1b48a9c453f1463e90c812ed5bde0ab266e515b0
larshp <larshp@hotmail.com>
2020-07-12 17:04:59 +0200

    allow macro call inside CASE structure

--- packages/core/src/abap/3_structures/structures/case.ts
+++ packages/core/src/abap/3_structures/structures/case.ts
@@ -1,14 +1,15 @@
 import * as Statements from "../../2_statements/statements";
 import {IStructure} from "./_structure";
-import {star, sta, beginEnd, sub} from "./_combi";
+import {star, sta, beginEnd, sub, alt} from "./_combi";
 import {IStructureRunnable} from "./_structure_runnable";
 import {When} from "./when";
+import {MacroCall} from "../../2_statements/statements/_statement";
 
 export class Case implements IStructure {
 
   public getMatcher(): IStructureRunnable {
     return beginEnd(sta(Statements.Case),
-                    star(sub(new When())),
+                    star(alt(sub(new When()), sta(MacroCall))),
                     sta(Statements.EndCase));
   }
 
