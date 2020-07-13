13035a82865999a7414db65f887553d4fba676f8
larshp <larshp@hotmail.com>
2020-07-13 09:24:48 +0200

    refactoring

--- packages/core/src/abap/types/_class_definition.ts
+++ packages/core/src/abap/types/_class_definition.ts
@@ -1,7 +1,6 @@
 import {IInterfaceDefinition} from "./_interface_definition";
 
 export interface IClassDefinition extends IInterfaceDefinition {
-  isException(): boolean;
   isFinal(): boolean;
   isForTesting(): boolean;
   isAbstract(): boolean;
