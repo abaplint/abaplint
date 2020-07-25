40aa9a240e8a78437331352ebee0e72e73ca4b39
larshp <larshp@hotmail.com>
2020-07-25 12:14:15 +0200

    allow dollar in typename, #1084

--- packages/core/src/abap/2_statements/expressions/type_name.ts
+++ packages/core/src/abap/2_statements/expressions/type_name.ts
@@ -5,7 +5,7 @@ import {IStatementRunnable} from "../statement_runnable";
 export class TypeName extends Expression {
 
   public getRunnable(): IStatementRunnable {
-    const name = reg(/^[\w~\/%]+$/);
+    const name = reg(/^[\w~\/%$]+$/);
     const cla = seq(name, alt(tok(StaticArrow), tok(InstanceArrow)));
     const field = seq(tok(Dash), name);
     return seq(opt(cla), name, starPrio(field));
--- packages/core/test/abap/statements/fieldsymbol.ts
+++ packages/core/test/abap/statements/fieldsymbol.ts
@@ -3,6 +3,7 @@ import * as Statements from "../../../src/abap/2_statements/statements";
 
 const tests = [
   "field-symbol <foo> type c.",
+  "FIELD-SYMBOLS <sdf$> TYPE ty_$.",
   "FIELD-SYMBOLS <foo>.",
 ];
 
