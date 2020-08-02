28a75bbe2689fa2898d46ceb178384a255506cf4
larshp <larshp@hotmail.com>
2020-08-02 07:06:28 +0200

    test

--- .github/regression/run.js
+++ .github/regression/run.js
@@ -1,1 +1,35 @@
-console.dir("output yes, hoelwiow");
+'use strict';
+const fs = require("fs");
+
+// todo, also output runtimes
+
+let files = fs.readdirSync(".", {withFileTypes: true});
+files = files.filter(f => f.isFile());
+files = files.filter(f => f.name.endsWith(".json"));
+
+let map = {};
+for (let f of files) {
+  const name = f.name.split("#")[0];
+  if (map[name] === undefined) {
+    map[name] = {};
+  }
+  const contents = fs.readFileSync(f.name, "utf-8");
+  if (f.name.endsWith("#before.json")) {
+    map[name].before = JSON.parse(contents);
+  } else {
+    map[name].after = JSON.parse(contents);
+  }
+}
+
+let comment = "";
+for (let name in map) {
+  if (map[name].before.length === map[name].after.length) {
+    comment += name + ": match :green_circle:";
+  } else {
+    comment += name + ": fail :red_circle:";
+  }
+}
+
+console.dir(comment);
+
+fs.writeFileSync("comment-body.txt", comment);
