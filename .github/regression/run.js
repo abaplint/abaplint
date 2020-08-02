6800a2589e43edccb9cc603d5201f7a8fa44e45b
Lars Hvam <larshp@hotmail.com>
2020-08-02 08:15:40 +0200

    add regression CI, wip (#1112)
    * add regression CI, wip
    
    * and on pull_request
    
    * test
    
    * all
    
    * only on push
    
    * also master, test
    
    * regression testing
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * p
    
    * whitespace
    
    * test
    
    * try push
    
    * not master
    
    * checkout name
    
    * foo
    
    * foo
    
    * update
    
    * test
    
    * test
    
    * test
    
    * json
    
    * test
    
    * test
    
    * test
    
    * hello
    
    * test
    
    * test
    
    * test
    
    * hmm
    
    * test
    
    * check outputs
    
    * dump
    
    * find pr
    
    * test
    
    * test
    
    * update1
    
    * update test
    
    * update
    
    * update
    
    * update
    
    * test
    
    * foo
    
    * test
    
    * test
    
    * test
    
    * foobar
    
    * test
    
    * test
    
    * okay
    
    * update
    
    * okay
    
    * take the bundle
    
    * update path
    
    * Update regression.yml
    
    * Update regression.yml
    
    * Update regression.yml
    
    * Update regression.yml
    
    * Update regression.yml
    
    * test
    
    * test
    
    * test
    
    * test
    
    * json format
    
    * test
    
    * test
    
    * body
    
    * add todo comment

---
+++ .github/regression/run.js
@@ -0,0 +1,39 @@
+'use strict';
+const fs = require("fs");
+
+// todo, also output runtimes
+
+console.dir(process.env.REPOS);
+
+let files = fs.readdirSync(".", {withFileTypes: true});
+files = files.filter(f => f.isFile());
+files = files.filter(f => f.name.endsWith(".json"));
+console.dir(files);
+
+let map = {};
+for (let f of files) {
+  const name = f.name.split("#")[0];
+  if (map[name] === undefined) {
+    map[name] = {};
+  }
+  const contents = fs.readFileSync(f.name, "utf-8");
+  if (f.name.endsWith("#before.json")) {
+    map[name].before = JSON.parse(contents);
+  } else {
+    map[name].after = JSON.parse(contents);
+  }
+}
+
+let comment = "Regression test results:\n";
+for (let name in map) {
+  // todo, this assumes the content is the same
+  if (map[name].before.length === map[name].after.length) {
+    comment += "- " + name + ": match :green_circle:\n";
+  } else {
+    comment += "- " + name + ": fail :red_circle:\n";
+  }
+}
+
+console.dir(comment);
+
+fs.writeFileSync("comment-body.txt", comment);
