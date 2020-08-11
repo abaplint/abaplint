12b79e7d5d1b27a8a9dbc090830cf639c19c6d3c
larshp <larshp@hotmail.com>
2020-08-11 17:56:08 +0200

    regression run, make table

--- .github/regression/run.js
+++ .github/regression/run.js
@@ -22,23 +22,28 @@ for (let r of repos) {
   map[r].after = JSON.parse(fs.readFileSync("output.json", "utf-8"));
 }
 
+let issues = "";
 let comment = "Regression test results:\n";
+
+comment += "| Repository | Issues |\n";
+comment += "| --- | --- |\n";
 for (let name in map) {
   const link = "[" + name + "](https://github.com/" + name + ")"
   // todo, this assumes the array content is the same
   if (map[name].before.length === map[name].after.length) {
-    comment += "- " + link + ": :green_circle: ";
+    comment += "| " + link + "| :green_circle: ";
   } else if (map[name].before.length > map[name].after.length) {
-    comment += "- " + link + ": :yellow_circle: ";
+    comment += "| " + link + "| :yellow_circle: ";
   } else {
-    comment += "- " + link + ": :red_circle:";
+    comment += "| " + link + "| :red_circle:";
   }
-  comment += " " + map[name].before.length + " -> " + map[name].after.length + "\n";
+  comment += " " + map[name].before.length + " -> " + map[name].after.length + "|\n";
 
   for (const i of map[name].after) {
-    comment += "`" + i.file + "`: " + i.description + ", " + i.start.row + "\n"
+    issues += "`" + i.file + "`: " + i.description + ", " + i.start.row + "\n"
   }
 }
+comment += "\n" + issues;
 comment += "\nUpdated: " + new Date().toISOString() + "\n";
 comment += "\nSHA: " + process.env.GITHUB_SHA + "\n";
 
