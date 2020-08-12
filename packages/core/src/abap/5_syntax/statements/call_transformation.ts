fa714feff32c189639e683428bf52f425b91bef6
larshp <larshp@hotmail.com>
2020-08-12 08:45:32 +0200

    call transformation with inline

--- packages/core/src/abap/5_syntax/statements/call_transformation.ts
+++ packages/core/src/abap/5_syntax/statements/call_transformation.ts
@@ -3,6 +3,8 @@ import {StatementNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
 import {Target} from "../expressions/target";
 import {Source} from "../expressions/source";
+import {XStringType} from "../../types/basic";
+import {InlineData} from "../expressions/inline_data";
 
 export class CallTransformation {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
@@ -12,7 +14,12 @@ export class CallTransformation {
     }
 
     for (const t of node.findDirectExpressions(Expressions.Target)) {
-      new Target().runSyntax(t, scope, filename);
+      const inline = t?.findDirectExpression(Expressions.InlineData);
+      if (inline) {
+        new InlineData().runSyntax(inline, scope, filename, new XStringType());
+      } else {
+        new Target().runSyntax(t, scope, filename);
+      }
     }
 
   }
