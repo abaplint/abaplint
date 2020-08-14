739e392a8bb8fd18d111433200f89f368051a999
Lars Hvam <larshp@hotmail.com>
2020-08-14 14:51:09 +0200

    2.39.4, ignore star, sy-lilli, switch body, unknown_types error messages (#1171)
    * View, ignore star fields, #1135
    
    * add sy-lilli, #1170
    
    * add nmin and nmax builtins
    
    * better error messages for unknown_types, #1135
    
    * allow INCLUDE in class implementation structure
    
    * walk into SwitchBody
    
    * 2.39.4

--- packages/core/src/rules/unknown_types.ts
+++ packages/core/src/rules/unknown_types.ts
@@ -54,8 +54,9 @@ export class UnknownTypes implements IRule {
     let ret: Issue[] = [];
 
     for (const v of node.getData().vars) {
-      if (this.containsUnknown(v.identifier.getType())) {
-        const message = "Type of \"" + v.name + "\" contains unknown types";
+      const found = this.containsUnknown(v.identifier.getType());
+      if (found) {
+        const message = "Type of \"" + v.name + "\" contains unknown: " + found;
         ret.push(Issue.atIdentifier(v.identifier, message, this.getMetadata().key));
       }
     }
@@ -67,19 +68,20 @@ export class UnknownTypes implements IRule {
     return ret;
   }
 
-  private containsUnknown(type: AbstractType): boolean {
+  private containsUnknown(type: AbstractType): string | undefined {
     if (type instanceof BasicTypes.UnknownType) {
-      return true;
+      return type.getError();
     } else if (type instanceof BasicTypes.StructureType) {
       for (const c of type.getComponents()) {
-        if (this.containsUnknown(c.type)) {
-          return true;
+        const found = this.containsUnknown(c.type);
+        if (found) {
+          return found;
         }
       }
     } else if (type instanceof BasicTypes.TableType) {
       return this.containsUnknown(type.getRowType());
     }
-    return false;
+    return undefined;
   }
 
 }
