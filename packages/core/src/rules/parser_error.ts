950f3add2c535758a7dbfd71bc3378ecddbef9c5
larshp <larshp@hotmail.com>
2020-08-14 07:56:41 +0200

    spaces, #1125

--- packages/core/src/rules/parser_error.ts
+++ packages/core/src/rules/parser_error.ts
@@ -1,10 +1,7 @@
 import {Issue} from "../issue";
-import {Position} from "../position";
-import * as Tokens from "../abap/1_lexer/tokens";
 import {Unknown} from "../abap/2_statements/statements/_statement";
 import {ABAPRule} from "./_abap_rule";
 import {ABAPFile} from "../files";
-import {StatementNode} from "../abap/nodes";
 import {BasicRuleConfig} from "./_basic_rule_config";
 import {STATEMENT_MAX_TOKENS} from "../abap/2_statements/statement_parser";
 import {RuleTag} from "./_irule";
@@ -19,7 +16,9 @@ export class ParserError extends ABAPRule {
     return {
       key: "parser_error",
       title: "Parser error",
-      shortDescription: `Checks for syntax unrecognized by abaplint`,
+      shortDescription: `Checks for syntax unrecognized by abaplint.
+
+See recognized syntax at https://syntax.abaplint.org`,
       tags: [RuleTag.Syntax],
     };
   }
@@ -35,49 +34,24 @@ export class ParserError extends ABAPRule {
   public runParsed(file: ABAPFile) {
     const issues: Issue[] = [];
 
-    let start = new Position(0, 0);
     for (const statement of file.getStatements()) {
-// only report one error per row
-      if (statement.get() instanceof Unknown
-            && start.getRow() !== statement.getStart().getRow()) {
+      if (!(statement.get() instanceof Unknown)) {
+        continue;
+      }
 
-        const missing = this.missingSpace(statement);
-        if (missing) {
-          const message = "Missing space between string or character literal and parentheses, Parser error";
-          start = missing;
-          const issue = Issue.atPosition(file, start, message, this.getMetadata().key);
-          issues.push(issue);
-        } else if (statement.getTokens().length > STATEMENT_MAX_TOKENS) {
-          const message = "Statement too long, refactor statement";
-          const issue = Issue.atToken(file, statement.getTokens()[0], message, this.getMetadata().key);
-          issues.push(issue);
-        } else {
-          const tok = statement.getFirstToken();
-          const message = "Statement does not exist in ABAP" + this.reg.getConfig().getVersion() + "(or a parser error), \"" + tok.getStr() + "\"";
-          const issue = Issue.atStatement(file, statement, message, this.getMetadata().key);
-          issues.push(issue);
-        }
-
+      if (statement.getTokens().length > STATEMENT_MAX_TOKENS) {
+        const message = "Statement too long, refactor statement";
+        const issue = Issue.atToken(file, statement.getTokens()[0], message, this.getMetadata().key);
+        issues.push(issue);
+      } else {
+        const tok = statement.getFirstToken();
+        const message = "Statement does not exist in ABAP" + this.reg.getConfig().getVersion() + "(or a parser error), \"" + tok.getStr() + "\"";
+        const issue = Issue.atStatement(file, statement, message, this.getMetadata().key);
+        issues.push(issue);
       }
     }
 
     return issues;
-  }
-
-  private missingSpace(statement: StatementNode): Position | undefined {
-    const tokens = statement.getTokens();
-    for (let i = 0; i < tokens.length - 1; i++) {
-      const current = tokens[ i ];
-      const next = tokens[ i + 1 ];
-      if (current.getRow() === next.getRow() &&
-          current.getCol() + current.getStr().length === next.getCol() &&
-          (current instanceof Tokens.String && next.getStr() === ")"
-          || current.getStr() === "(" && next instanceof Tokens.String)) {
-        return next.getStart();
-      }
-    }
-
-    return undefined;
   }
 
 }
--- packages/core/test/rules/parser_error.ts
+++ packages/core/test/rules/parser_error.ts
@@ -6,12 +6,8 @@ const tests = [
   {abap: "WRITE: / 'abc'.", cnt: 0},
   {abap: "##EXISTS\nENDMETHOD.", cnt: 0},
   {abap: "##needed.", cnt: 0},
-  {abap: "ro_html->add('foo' && 'bar' ).", cnt: 1},
-  {abap: "moo('sdf').", cnt: 1},
-  {abap: "moo( bar).", cnt: 1},
   {abap: "moo( 'sdf' ).", cnt: 0},
   {abap: "moo( bar ).", cnt: 0},
-  {abap: "moo( 'sdf').", cnt: 1}, // see differences between ABAP and abaplint
   {abap: "APPEND NEW lcl_foo( VALUE #( connid = '456') ) TO foos.", cnt: 1},
   {abap: "DEFINE _foo.\nEND-OF-DEFINITION.\n_foo.", cnt: 0},
   {abap: "DEFINE _foo.\nEND-OF-DEFINITION.\n_foo ##CALLED.", cnt: 0},
