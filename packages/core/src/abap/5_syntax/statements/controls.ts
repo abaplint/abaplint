56246dcc3bd1229b71ee1f86a637247be55e0b4b
larshp <larshp@hotmail.com>
2020-08-25 08:02:45 +0200

    add tableview, closes #1220

--- packages/core/src/abap/5_syntax/statements/controls.ts
+++ packages/core/src/abap/5_syntax/statements/controls.ts
@@ -2,7 +2,7 @@ import * as Expressions from "../../2_statements/expressions";
 import {StatementNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
 import {TypedIdentifier} from "../../types/_typed_identifier";
-import {StructureType, CharacterType} from "../../types/basic";
+import {StructureType, CharacterType, IntegerType, TableType} from "../../types/basic";
 
 export class Controls {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
@@ -12,6 +12,32 @@ export class Controls {
 
     if (node.findDirectTokenByText("TABSTRIP") && token) {
       const type = new StructureType([{name: "ACTIVETAB", type: new CharacterType(132)}]);
+      const id = new TypedIdentifier(token, filename, type);
+      scope.addIdentifier(id);
+    }
+
+    if (node.findDirectTokenByText("TABLEVIEW") && token) {
+      const cols = new StructureType([
+        {name: "SCREEN", type: new CharacterType(1)}, // todo
+        {name: "INDEX", type: new IntegerType()},
+        {name: "SELECTED", type: new CharacterType(1)},
+        {name: "VISLENGTH", type: new IntegerType()},
+        {name: "INVISIBLE", type: new CharacterType(1)},
+      ]);
+      const type = new StructureType([
+        {name: "FIXED_COLS", type: new CharacterType(132)},
+        {name: "LINES", type: new IntegerType()},
+        {name: "TOP_LINE", type: new IntegerType()},
+        {name: "CURRENT_LINE", type: new IntegerType()},
+        {name: "LEFT_COL", type: new IntegerType()},
+        {name: "LINE_SEL_MODE", type: new CharacterType(1)},
+        {name: "COL_SEL_MODE", type: new CharacterType(1)},
+        {name: "LINE_SELECTOR", type: new CharacterType(1)},
+        {name: "H_GRID", type: new CharacterType(1)},
+        {name: "V_GRID", type: new CharacterType(1)},
+        {name: "COLS", type: new TableType(cols, false)},
+        {name: "INVISIBLE", type: new CharacterType(1)},
+      ]);
       const id = new TypedIdentifier(token, filename, type);
       scope.addIdentifier(id);
     }
