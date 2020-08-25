ca0ab12b6b0b172717a8ee135eab295823463c31
larshp <larshp@hotmail.com>
2020-08-25 11:42:45 +0200

    tabbed block, closes #1224

--- packages/core/src/abap/5_syntax/statements/selection_screen.ts
+++ packages/core/src/abap/5_syntax/statements/selection_screen.ts
@@ -2,7 +2,7 @@ import * as Expressions from "../../2_statements/expressions";
 import {StatementNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
 import {TypedIdentifier} from "../../types/_typed_identifier";
-import {CharacterType} from "../../types/basic";
+import {CharacterType, StructureType} from "../../types/basic";
 
 export class SelectionScreen {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string) {
@@ -14,6 +14,17 @@ export class SelectionScreen {
 
     const name = field.getFirstToken();
 
-    scope.addIdentifier(new TypedIdentifier(name, filename, new CharacterType(83)));
+    const concat = node.concatTokens().toUpperCase();
+    if (concat.includes("BEGIN OF TABBED BLOCK")) {
+      const type = new StructureType([
+        {name: "PROG", type: new CharacterType(40)},
+        {name: "DYNNR", type: new CharacterType(4)},
+        {name: "ACTIVETAB", type: new CharacterType(132)},
+      ]);
+
+      scope.addIdentifier(new TypedIdentifier(name, filename, type));
+    } else {
+      scope.addIdentifier(new TypedIdentifier(name, filename, new CharacterType(83)));
+    }
   }
 }
