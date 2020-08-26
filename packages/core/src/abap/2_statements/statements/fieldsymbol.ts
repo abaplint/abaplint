e70e7bfb3d47c1671d8e7fe03336818b4c78aa40
larshp <larshp@hotmail.com>
2020-08-26 11:21:01 +0200

    add FIELDSYB STRUCTURE DEFAULT, #1228

--- packages/core/src/abap/2_statements/statements/fieldsymbol.ts
+++ packages/core/src/abap/2_statements/statements/fieldsymbol.ts
@@ -1,14 +1,16 @@
 import {IStatement} from "./_statement";
 import {str, seq, alt, opt} from "../combi";
-import {FieldSymbol as Name, Type, TypeTable} from "../expressions";
+import {FieldSymbol as Name, Type, TypeTable, TypeName, Field} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class FieldSymbol implements IStatement {
 
   public getMatcher(): IStatementRunnable {
+    const stru = seq(str("STRUCTURE"), new TypeName(), str("DEFAULT"), new Field());
+
     return seq(alt(str("FIELD-SYMBOL"), str("FIELD-SYMBOLS")),
                new Name(),
-               opt(alt(new Type(), new TypeTable())));
+               opt(alt(new Type(), new TypeTable(), stru)));
   }
 
 }
--- packages/core/test/abap/statements/fieldsymbol.ts
+++ packages/core/test/abap/statements/fieldsymbol.ts
@@ -6,6 +6,7 @@ const tests = [
   "FIELD-SYMBOLS <sdf$> TYPE ty_$.",
   "field-symbols <fs_*bar> type any.",
   "FIELD-SYMBOLS <foo>.",
+  "FIELD-SYMBOLS <bar> STRUCTURE usr02 DEFAULT usr02.",
 ];
 
 statementType(tests, "FIELD-SYMBOL", Statements.FieldSymbol);
