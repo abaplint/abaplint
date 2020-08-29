9bfe7a990a5de550d72d24d57567749c84321fa5
larshp <larshp@hotmail.com>
2020-08-29 08:44:43 +0200

    fix regression logic

--- .github/regression/run.js
+++ .github/regression/run.js
@@ -48,9 +48,12 @@ for (let name in map) {
   comment += " " + map[name].before.length + " -> " + map[name].after.length + "| " + runtimeInfo + " |\n";
 
   for (const i of map[name].after) {
+    if (issues.lengt > 1000) {
+      continue;
+    }
     let urlFile = i.file.split("/").splice(1).join("/");
     let url = "https://github.com/" + name + "/blob/master/" + urlFile + "#L" + i.start.row;
-    issues += "[`" + i.file + ":`](" + url + "): " + i.description + "\n"
+    issues += "[`" + i.file + ":" + i.start.row + "`](" + url + "): " + i.description + "\n"
   }
 }
 comment += "\n" + issues;
