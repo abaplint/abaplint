ac3e302394f3ab83cb81ba6f2e73b91ac30f0928
larshp <larshp@hotmail.com>
2020-08-31 16:00:58 +0200

    monaco rename provider

--- packages/monaco/src/abap_rename_provider.ts
+++ packages/monaco/src/abap_rename_provider.ts
@@ -1,4 +1,5 @@
 import {IRegistry, LanguageServer} from "@abaplint/core";
+import {TextDocumentEdit} from "vscode-languageserver-types";
 
 export class ABAPRenameProvider implements monaco.languages.RenameProvider {
   private readonly reg: IRegistry;
@@ -10,16 +11,33 @@ export class ABAPRenameProvider implements monaco.languages.RenameProvider {
   public provideRenameEdits(model: monaco.editor.ITextModel, position: monaco.Position, newName: string, token: monaco.CancellationToken):
   monaco.languages.ProviderResult<monaco.languages.WorkspaceEdit & monaco.languages.Rejection> {
 
-// todo, apply the workspacedit to the model and FileSystem, probably wait for vscode-web
-    throw new Error("provideRenameEdits, method not implemented");
+    const rename = new LanguageServer(this.reg).rename({
+      textDocument: {uri: model.uri.toString()},
+      position: {line: position.lineNumber - 1, character: position.column - 1},
+      newName});
+
+    const edit: monaco.languages.WorkspaceEdit = {edits: []};
+    for (const r of rename?.documentChanges ? rename?.documentChanges : []) {
+      if (TextDocumentEdit.is(r)) {
+        for (const e of r.edits) {
+          const textedit: monaco.languages.TextEdit = {range: new monaco.Range(
+            e.range.start.line + 1,
+            e.range.start.character + 1,
+            e.range.end.line + 1,
+            e.range.end.character + 1), text: newName};
+          const wte: monaco.languages.WorkspaceTextEdit = {resource: model.uri, edit: textedit};
+          edit.edits.push(wte);
+        }
+      }
+    }
+
+    return edit;
   }
 
   public resolveRenameLocation(model: monaco.editor.ITextModel, position: monaco.Position, token: monaco.CancellationToken):
   monaco.languages.ProviderResult<monaco.languages.RenameLocation & monaco.languages.Rejection> {
 
-    const ls = new LanguageServer(this.reg);
-
-    const rename = ls.prepareRename({
+    const rename = new LanguageServer(this.reg).prepareRename({
       textDocument: {uri: model.uri.toString()},
       position: {line: position.lineNumber - 1, character: position.column - 1}});
 
