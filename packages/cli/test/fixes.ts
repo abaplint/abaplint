d55d31d04a3e184c2af00fc321d6277a6bef809e
larshp <larshp@hotmail.com>
2020-09-01 15:08:27 +0200

    dont apply overlapping fixes at the same time

--- packages/cli/test/fixes.ts
+++ packages/cli/test/fixes.ts
@@ -33,4 +33,20 @@ describe("Apply fixes", () => {
     const result = input.readFileSync("zfoobar.prog.abap").toString();
     expect(result).to.equal(`method( var1 = value1\n        var2 = value2 ).`);
   });
+
+  it("test 3, overlapping fixes", () => {
+    const file = new MemoryFile("zfoobar.prog.abap", "DATA: foo, bar.");
+    const reg = new Registry().addFile(file).parse();
+    const issues = reg.findIssues();
+
+    const jsonFiles: any = {};
+    jsonFiles[file.getFilename()] = file.getRaw();
+
+    const input = memfs.createFsFromVolume(memfs.Volume.fromJSON(jsonFiles));
+    applyFixes(issues, reg, input);
+
+    const result = input.readFileSync("zfoobar.prog.abap").toString();
+    expect(result).to.equal(``);
+  });
+
 });
