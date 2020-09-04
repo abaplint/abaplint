ec133034ea3448474e57bcca86b74552bfdc39e5
larshp <larshp@hotmail.com>
2020-09-04 10:16:39 +0200

    more macro names, and allow macro call in TYPES

--- packages/core/src/abap/3_structures/structures/types.ts
+++ packages/core/src/abap/3_structures/structures/types.ts
@@ -2,12 +2,13 @@ import * as Statements from "../../2_statements/statements";
 import {IStructure} from "./_structure";
 import {star, sta, alt, sub, beginEnd} from "./_combi";
 import {IStructureRunnable} from "./_structure_runnable";
+import {MacroCall} from "../../2_statements/statements/_statement";
 
 export class Types implements IStructure {
 
   public getMatcher(): IStructureRunnable {
     return beginEnd(sta(Statements.TypeBegin),
-                    star(alt(sta(Statements.Type), sub(new Types()), sta(Statements.IncludeType))),
+                    star(alt(sta(Statements.Type), sub(new Types()), sta(MacroCall), sta(Statements.IncludeType))),
                     sta(Statements.TypeEnd));
   }
 
