57053fc024003a2758b29966e7d6ddae50af7a46
larshp <larshp@hotmail.com>
2020-09-22 11:50:23 +0200

    many_parenthesis

--- packages/core/src/rules/many_parenthesis.ts
+++ packages/core/src/rules/many_parenthesis.ts
@@ -4,7 +4,7 @@ import {ABAPRule} from "./_abap_rule";
 import {ABAPFile} from "../files";
 import {BasicRuleConfig} from "./_basic_rule_config";
 import {IRuleMetadata} from "./_irule";
-import {ExpressionNode} from "../abap/nodes";
+import {ExpressionNode, TokenNode} from "../abap/nodes";
 
 export class ManyParenthesisConf extends BasicRuleConfig {
 }
@@ -19,8 +19,18 @@ export class ManyParenthesis extends ABAPRule {
       title: "Too many parenthesis",
       shortDescription: `Searches for expressions where extra parenthesis can safely be removed`,
       tags: [],
-      badExample: `IF ( destination IS INITIAL ).`,
-      goodExample: `IF destination IS INITIAL.`,
+      badExample: `
+IF ( destination IS INITIAL ).
+ENDIF.
+IF foo = boo AND ( bar = lar AND moo = loo ).
+ENDIF.
+`,
+      goodExample: `
+IF destination IS INITIAL.
+ENDIF.
+IF foo = boo AND bar = lar AND moo = loo.
+ENDIF.
+`,
     };
   }
 
@@ -33,20 +43,26 @@ export class ManyParenthesis extends ABAPRule {
   }
 
   public runParsed(file: ABAPFile) {
-    const issues: Issue[] = [];
+    let issues: Issue[] = [];
 
     const structure = file.getStructure();
     if (structure === undefined) {
       return [];
     }
 
+    for (const cond of structure.findAllExpressions(Expressions.Cond)) {
+      issues = issues.concat(this.analyze(file, cond));
+    }
+
     for (const sub of structure.findAllExpressions(Expressions.CondSub)) {
-      for (const cond of sub.findDirectExpressions(Expressions.Cond)) {
-        if (this.isSimple(cond) === true) {
-          const message = "Too many parenthesis";
-          const issue = Issue.atToken(file, sub.getFirstToken(), message, this.getMetadata().key, this.conf.severity);
-          issues.push(issue);
-        }
+      const cond = sub.findDirectExpressions(Expressions.Cond);
+      if (cond.length !== 1) {
+        continue;
+      }
+      if (cond[0].getChildren().length === 1) {
+        const message = "Too many parenthesis, simple";
+        const issue = Issue.atToken(file, sub.getFirstToken(), message, this.getMetadata().key, this.conf.severity);
+        issues.push(issue);
       }
     }
 
@@ -55,12 +71,56 @@ export class ManyParenthesis extends ABAPRule {
 
 ////////////////////
 
-  private isSimple(cond: ExpressionNode): boolean {
-    const children = cond.getChildren();
-    if (children.length === 1 && children[0].get() instanceof Expressions.Compare) {
-      return true;
+  private analyze(file: ABAPFile, cond: ExpressionNode): Issue[] {
+    const issues: Issue[] = [];
+    let comparator = "";
+
+    for (const c of cond.getChildren()) {
+      let current = "";
+      if (c instanceof TokenNode) {
+        current = c.get().getStr().toUpperCase();
+      } else if (c instanceof ExpressionNode && c.get() instanceof Expressions.CondSub) {
+        if (c.getFirstToken().getStr() === "NOT") {
+          return [];
+        }
+        const i = c.findDirectExpression(Expressions.Cond);
+        if (i === undefined) {
+          return [];
+        }
+        current = this.findComparator(i);
+      }
+      if (comparator === "") {
+        comparator = current;
+      } else if (comparator !== current) {
+        return [];
+      }
     }
-    return false;
+
+    if (comparator !== "" && comparator !== "MIXED") {
+      const message = "Too many parenthesis, complex";
+      const issue = Issue.atToken(file, cond.getFirstToken(), message, this.getMetadata().key, this.conf.severity);
+      issues.push(issue);
+    }
+
+    return issues;
+  }
+
+  private findComparator(cond: ExpressionNode): string {
+    let comparator = "";
+
+    const children = cond.getChildren();
+    for (const c of children) {
+      if (c instanceof TokenNode) {
+        const current = c.get().getStr().toUpperCase();
+        if (comparator === "") {
+          comparator = current;
+        } else if (current !== comparator) {
+          return "MIXED";
+        }
+      }
+    }
+
+    return comparator;
   }
 
 }
