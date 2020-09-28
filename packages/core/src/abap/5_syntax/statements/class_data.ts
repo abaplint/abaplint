f5278b259ea3a2540d48ba62eecbabc8cf6c880c
larshp <larshp@hotmail.com>
2020-09-28 16:55:25 +0200

    add static tag

--- packages/core/src/abap/5_syntax/statements/class_data.ts
+++ packages/core/src/abap/5_syntax/statements/class_data.ts
@@ -1,7 +1,7 @@
 import * as Expressions from "../../2_statements/expressions";
 import {StatementNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
-import {TypedIdentifier} from "../../types/_typed_identifier";
+import {IdentifierMeta, TypedIdentifier} from "../../types/_typed_identifier";
 import {DataDefinition} from "../expressions/data_definition";
 import {UnknownType} from "../../types/basic/unknown_type";
 
@@ -9,7 +9,13 @@ export class ClassData {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): TypedIdentifier | undefined {
     const dd = node.findFirstExpression(Expressions.DataDefinition);
     if (dd) {
-      return new DataDefinition().runSyntax(dd, scope, filename);
+      const found = new DataDefinition().runSyntax(dd, scope, filename);
+      if (found === undefined) {
+        return undefined;
+      }
+      const meta = found.getMeta().slice();
+      meta.push(IdentifierMeta.Static);
+      return new TypedIdentifier(found.getToken(), filename, found.getType(), meta);
     }
 
     const fallback = node.findFirstExpression(Expressions.NamespaceSimpleName);
