792c77661be91df60e3224ccbc3f2d75cc1523a8
larshp <larshp@hotmail.com>
2020-09-29 16:49:21 +0200

    prio

--- packages/core/src/abap/2_statements/expressions/field_chain.ts
+++ packages/core/src/abap/2_statements/expressions/field_chain.ts
@@ -1,4 +1,4 @@
-import {seq, optPrio, alt, str, star, tok, Expression, altPrio} from "../combi";
+import {seq, optPrio, str, star, tok, Expression, altPrio} from "../combi";
 import {AttributeName, ClassName, SourceField, SourceFieldSymbol, TableExpression, ComponentName, FieldOffset, FieldLength} from ".";
 import {InstanceArrow, StaticArrow, Dash, DashW} from "../../1_lexer/tokens";
 import {IStatementRunnable} from "../statement_runnable";
@@ -6,7 +6,7 @@ import {IStatementRunnable} from "../statement_runnable";
 export class FieldChain extends Expression {
   public getRunnable(): IStatementRunnable {
 
-    const attr = seq(tok(InstanceArrow), alt(new AttributeName(), str("*")));
+    const attr = seq(tok(InstanceArrow), altPrio(new AttributeName(), str("*")));
     const comp = seq(tok(Dash), optPrio(new ComponentName()));
 
     const chain = star(altPrio(attr, comp, new TableExpression()));
