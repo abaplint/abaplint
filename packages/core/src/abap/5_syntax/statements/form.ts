56c72309b9740b37e2cfa97ee4cd41dbe95b634f
Lars Hvam <larshp@hotmail.com>
2020-10-06 11:20:32 +0200

    2.55.1, fix some FORM scoping problems (#1423)
    * FORM, fix some scoping problems
    
    * 2.55.1

--- packages/core/src/abap/5_syntax/statements/form.ts
+++ packages/core/src/abap/5_syntax/statements/form.ts
@@ -2,13 +2,17 @@ import {StatementNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
 import {FormDefinition} from "../../types/form_definition";
 import {ScopeType} from "../_scope_type";
+import {FormName} from "../../2_statements/expressions";
 
 export class Form {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
+    const name = node.findDirectExpression(FormName)?.concatTokens();
+    if (name === undefined) {
+      throw new Error("Form, could not find name");
+    }
+    scope.push(ScopeType.Form, name, node.getFirstToken().getStart(), filename);
 
     const form = new FormDefinition(node, filename, scope);
-    scope.push(ScopeType.Form, form.getName(), node.getFirstToken().getStart(), filename);
-
     scope.addList(form.getUsingParameters());
     scope.addList(form.getChangingParameters());
     scope.addList(form.getTablesParameters());
