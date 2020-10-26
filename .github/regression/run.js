045a49a2010ded1dc00b53f44907ad271c52cd31
larshp <larshp@hotmail.com>
2020-10-26 09:51:49 +0100

    sort regression test repo list

--- .github/regression/run.js
+++ .github/regression/run.js
@@ -4,7 +4,7 @@ const childProcess = require("child_process");
 
 // todo, also output analysis runtimes
 
-const repos = JSON.parse(process.env.REPOS);
+const repos = JSON.parse(process.env.REPOS).sort();
 console.dir(repos);
 
 let map = {};
