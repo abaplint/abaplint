eec0cbcbfeb0df0a3b261703fbbf6947f48e6083
larshp <larshp@hotmail.com>
2020-11-12 11:56:55 +0100

    add CORRESPONDING DEEP

--- packages/core/src/abap/2_statements/expressions/corresponding_body.ts
+++ packages/core/src/abap/2_statements/expressions/corresponding_body.ts
@@ -1,4 +1,4 @@
-import {seq, tok, Expression, str, plus, ver, opt, alt} from "../combi";
+import {seq, tok, Expression, str, plus, ver, optPrio, alt} from "../combi";
 import {WParenLeftW, WParenRightW} from "../../1_lexer/tokens";
 import {ComponentName, Source, Field} from ".";
 import {Version} from "../../../version";
@@ -14,11 +14,12 @@ export class CorrespondingBody extends Expression {
     const discarding = ver(Version.v751, str("DISCARDING DUPLICATES"));
 
     return seq(
-      opt(baseParen),
+      optPrio(str("DEEP")),
+      optPrio(baseParen),
       new Source(),
-      opt(discarding),
-      opt(mapping),
-      opt(seq(str("EXCEPT"), alt(plus(new Field()), str("*")))),
+      optPrio(discarding),
+      optPrio(mapping),
+      optPrio(seq(str("EXCEPT"), alt(plus(new Field()), str("*")))),
     );
   }
 }
