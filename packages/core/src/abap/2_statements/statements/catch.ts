12df80210ae82e2d7f76e2d345f46a4a5716fb4e
larshp <larshp@hotmail.com>
2020-12-03 12:58:24 +0100

    CATCH add O references, #1587

--- packages/core/src/abap/2_statements/statements/catch.ts
+++ packages/core/src/abap/2_statements/statements/catch.ts
@@ -1,6 +1,6 @@
 import {IStatement} from "./_statement";
 import {str, opt, seq, plus, optPrio} from "../combi";
-import {Target, Field} from "../expressions";
+import {Target, ClassName} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class Catch implements IStatement {
@@ -8,7 +8,7 @@ export class Catch implements IStatement {
   public getMatcher(): IStatementRunnable {
     return seq(str("CATCH"),
                optPrio(str("BEFORE UNWIND")),
-               plus(new Field()),
+               plus(new ClassName()),
                opt(seq(str("INTO"), new Target())));
   }
 
--- packages/core/src/abap/5_syntax/statements/catch.ts
+++ packages/core/src/abap/5_syntax/statements/catch.ts
@@ -5,20 +5,36 @@ import {TypedIdentifier, IdentifierMeta} from "../../types/_typed_identifier";
 import {UnknownType} from "../../types/basic/unknown_type";
 import {ObjectReferenceType, VoidType} from "../../types/basic";
 import {Target} from "../expressions/target";
+import {IReferenceExtras, ReferenceType} from "../_reference";
 
 export class Catch {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
+
+    for (const c of node.findDirectExpressions(Expressions.ClassName)) {
+      const token = c.getFirstToken();
+      const className = token.getStr();
+      const found = scope.existsObject(className);
+      if (found.found === true && found.id) {
+        scope.addReference(token, found.id, found.type, filename);
+      } else if (scope.getDDIC().inErrorNamespace(className) === false) {
+        const extra: IReferenceExtras = {ooName: className, ooType: "Void"};
+        scope.addReference(token, undefined, ReferenceType.ObjectOrientedVoidReference, filename, extra);
+      } else {
+        throw new Error("CATCH, unknown class " + className);
+      }
+    }
+
     const target = node.findDirectExpression(Expressions.Target);
-    const className = node.findDirectExpression(Expressions.Field)?.getFirstToken().getStr();
+    const firstClassName = node.findDirectExpression(Expressions.ClassName)?.getFirstToken().getStr();
 
     if (target?.findDirectExpression(Expressions.InlineData)) {
       const token = target.findFirstExpression(Expressions.TargetField)?.getFirstToken();
-      const found = scope.existsObject(className);
-      if (token && found.found === true && className && found.id) {
+      const found = scope.existsObject(firstClassName);
+      if (token && found.found === true && firstClassName && found.id) {
         const identifier = new TypedIdentifier(token, filename, new ObjectReferenceType(found.id), [IdentifierMeta.InlineDefinition]);
         scope.addIdentifier(identifier);
-      } else if (token && scope.getDDIC().inErrorNamespace(className) === false) {
-        const identifier = new TypedIdentifier(token, filename, new VoidType(className), [IdentifierMeta.InlineDefinition]);
+      } else if (token && scope.getDDIC().inErrorNamespace(firstClassName) === false) {
+        const identifier = new TypedIdentifier(token, filename, new VoidType(firstClassName), [IdentifierMeta.InlineDefinition]);
         scope.addIdentifier(identifier);
       } else if (token) {
         const message = "Catch, could not determine type for \"" + token.getStr() + "\"";
