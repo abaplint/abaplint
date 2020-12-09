b02ac381e4ec405abaf72624357584a1b1079f85
larshp <larshp@hotmail.com>
2020-12-09 12:24:56 +0100

    goto method implementations

--- packages/core/src/lsp/implementation.ts
+++ packages/core/src/lsp/implementation.ts
@@ -3,6 +3,13 @@ import {IRegistry} from "../_iregistry";
 import {ABAPObject} from "../objects/_abap_object";
 import {LSPUtils} from "./_lsp_utils";
 import {LSPLookup} from "./_lookup";
+import {MethodDefinition} from "../abap/types";
+import {SyntaxLogic} from "../abap/5_syntax/syntax";
+import {ISpaghettiScopeNode} from "../abap/5_syntax/_spaghetti_scope";
+import {Token} from "../abap/1_lexer/tokens/_token";
+import {ReferenceType} from "../abap/5_syntax/_reference";
+
+// note: finding implementations might be slow, ie finding method implementations currently searches the full registry
 
 // go to implementation
 export class Implementation {
@@ -29,12 +36,48 @@ export class Implementation {
       return [];
     }
 
-    const loc = LSPLookup.lookup(found, this.reg, obj)?.implementation;
-    if (loc) {
-      return [loc];
-    } else {
-      return [];
+    const lookup = LSPLookup.lookup(found, this.reg, obj);
+    if (lookup?.implementation) {
+      return [lookup?.implementation];
     }
+
+    if (lookup?.definitionId instanceof MethodDefinition) {
+      return this.findMethodImplementations(lookup.definitionId);
+    }
+
+    return [];
+  }
+
+  private findMethodImplementations(def: MethodDefinition): LServer.Location[] {
+    let ret: LServer.Location[] = [];
+
+    // note that this searches _everything_
+    for (const obj of this.reg.getObjects()) {
+      if (this.reg.isDependency(obj) || !(obj instanceof ABAPObject)) {
+        continue;
+      }
+      const found = this.searchReferences(new SyntaxLogic(this.reg, obj).run().spaghetti.getTop(), def.getToken());
+      ret = ret.concat(found);
+    }
+
+    return ret;
+  }
+
+  private searchReferences(scope: ISpaghettiScopeNode, token: Token): LServer.Location[] {
+    let ret: LServer.Location[] = [];
+
+    for (const r of scope.getData().references) {
+      if (r.referenceType === ReferenceType.MethodImplementationReference
+          && r.resolved?.getStart().equals(token.getStart())) {
+        ret.push(LSPUtils.identiferToLocation(r.position));
+      }
+    }
+
+    for (const c of scope.getChildren()) {
+      ret = ret.concat(this.searchReferences(c, token));
+    }
+
+    return ret;
   }
 
 }
