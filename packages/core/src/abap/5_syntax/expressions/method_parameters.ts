911a8233372a842454904f122f18871c7cc03f5a
Lars Hvam <larshp@hotmail.com>
2020-12-28 15:33:18 +0100

    2.63.7, bugfix, multiple references (#1630)
    * bugfix, multiple references
    
    * 2.63.7
    
    * bugfix

--- packages/core/src/abap/5_syntax/expressions/method_parameters.ts
+++ packages/core/src/abap/5_syntax/expressions/method_parameters.ts
@@ -97,7 +97,6 @@ export class MethodParameters {
         throw new Error("Could not determine target type");
       } else if (item.targetType) {
 // todo, check that targetType and parameterType are compatible
-        new Target().runSyntax(item.target, scope, filename);
       }
     }
   }
@@ -117,7 +116,6 @@ export class MethodParameters {
 
       if (item.targetType) {
 // todo, check that targetType and parameterType are compatible
-        new Target().runSyntax(item.target, scope, filename);
         if (0) {
           console.log(parameterType); // todo
         }
