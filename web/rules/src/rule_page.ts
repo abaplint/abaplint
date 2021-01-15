7c73070f57e8e3166092d9c5235b200a5bbca6d0
larshp <larshp@hotmail.com>
2021-01-15 12:20:20 +0100

    surface pragma and pseudo on rules.abaplint.org

--- web/rules/src/rule_page.ts
+++ web/rules/src/rule_page.ts
@@ -90,6 +90,13 @@ export function buildRule(meta: IRuleMetadata) {
   // eslint-disable-next-line max-len
   html = html + `&nbsp;<a href="${link}"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="2ch"><title>edit</title><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg></a>`;
 
+  if (meta.pragma) {
+    html += "<br>Pragma: " + meta.pragma + "<br>"
+  }
+  if (meta.pseudoComment) {
+    html += "<br>Pseudo comment: " + meta.pragma + "<br>"
+  }
+
   html = html + "<h2>Description</h2>\n" + meta.shortDescription + "<br><br>";
 
   if (meta.extendedInformation !== undefined && meta.extendedInformation !== "") {
