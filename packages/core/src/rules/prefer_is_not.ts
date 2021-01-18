fe6b1d7550b58a54df2f18ff726ad6fe5e8648e9
Jan Ježek <jan.jezek.ml@gmail.com>
2021-01-18 11:09:22 +0100

    prefer_is_not add fix (#1677)
    * prefer_is_not add fix
    
    * fix test
    
    * eslint fixes
    
    * Added toUpperCase to literals, more generic msg
    
    * Commit suggestion
    
    Co-authored-by: Lars Hvam <larshp@hotmail.com>
    
    * Move fix to separate method
    
    * eslint fixes
    
    * Use only one issues.push()
    
    Co-authored-by: Lars Hvam <larshp@hotmail.com>
    
    * Change error message
    
    * Fix quick fix logic
    
    * Added fix for CompareOperator
    
    Co-authored-by: Lars Hvam <larshp@hotmail.com>

--- packages/core/src/rules/prefer_is_not.ts
+++ packages/core/src/rules/prefer_is_not.ts
@@ -2,8 +2,11 @@ import {Issue} from "../issue";
 import {ABAPRule} from "./_abap_rule";
 import * as Expressions from "../abap/2_statements/expressions";
 import {BasicRuleConfig} from "./_basic_rule_config";
+import {EditHelper, IEdit} from "../edit_helper";
 import {IRuleMetadata, RuleTag} from "./_irule";
 import {ABAPFile} from "../abap/abap_file";
+import {Position} from "../position";
+import {ExpressionNode} from "../abap/nodes";
 
 export class PreferIsNotConf extends BasicRuleConfig {
 }
@@ -21,7 +24,7 @@ export class PreferIsNot extends ABAPRule {
 https://github.com/SAP/styleguides/blob/master/clean-abap/CleanABAP.md#prefer-is-not-to-not-is
 
 "if not is_valid( )." examples are skipped`,
-      tags: [RuleTag.Styleguide, RuleTag.SingleFile],
+      tags: [RuleTag.Styleguide, RuleTag.Quickfix, RuleTag.SingleFile],
       goodExample: `IF variable IS NOT INITIAL.
 IF variable NP 'TODO*'.
 IF variable <> 42.`,
@@ -51,12 +54,56 @@ IF NOT variable = 42.`,
         }
 
         const message = "Prefer IS NOT to NOT IS";
-        issues.push(Issue.atToken(file, c.getFirstToken(), message, this.getMetadata().key, this.conf.severity));
+
+        const fix = this.getFix(file, c);
+        issues.push(Issue.atToken(file, c.getFirstToken(), message, this.getMetadata().key, this.conf.severity, fix));
       }
     }
 
     return issues;
   }
 
-}
+  private getFix(file: ABAPFile, c: ExpressionNode): IEdit|undefined {
+    let insertFix: IEdit;
 
+    if (c.getChildren()[2].getFirstToken().getStr().toUpperCase() === "IS") {
+      const tokenPositionBeforeDelete = c.getChildren()[2].getLastToken().getEnd();
+      const tokenPosition = new Position(tokenPositionBeforeDelete.getRow(), tokenPositionBeforeDelete.getCol() + 1);
+      insertFix = EditHelper.insertAt(file, tokenPosition, "NOT ");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr().toUpperCase() === "IN" || c.getChildren()[2].getFirstToken().getStr().toUpperCase() === "BETWEEN") {
+      const tokenPositionBeforeDelete = c.getChildren()[1].getLastToken().getEnd();
+      const tokenPosition = new Position(tokenPositionBeforeDelete.getRow(), tokenPositionBeforeDelete.getCol() + 1);
+      insertFix = EditHelper.insertAt(file, tokenPosition, "NOT ");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr() === "=") {
+      insertFix = EditHelper.replaceToken(file, c.getChildren()[2].getLastToken(), "<>");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr() === "<>") {
+      insertFix = EditHelper.replaceToken(file, c.getChildren()[2].getLastToken(), "=");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr() === "<") {
+      insertFix = EditHelper.replaceToken(file, c.getChildren()[2].getLastToken(), ">");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr() === ">") {
+      insertFix = EditHelper.replaceToken(file, c.getChildren()[2].getLastToken(), "<");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr() === "<=") {
+      insertFix = EditHelper.replaceToken(file, c.getChildren()[2].getLastToken(), ">=");
+    }
+    else if(c.getChildren()[2].getFirstToken().getStr() === ">=") {
+      insertFix = EditHelper.replaceToken(file, c.getChildren()[2].getLastToken(), "<=");
+    }
+    else {
+      return;
+    }
+
+    const endCol = c.getChildren()[0].getFirstToken().getEnd().getCol() + 1;
+    const endPosition = new Position(c.getChildren()[0].getFirstToken().getEnd().getRow(), endCol);
+    const deleteFix = EditHelper.deleteRange(file, c.getChildren()[0].getFirstToken().getStart(), endPosition);
+    const finalFix = EditHelper.merge(insertFix, deleteFix);
+
+    return finalFix;
+  }
+
+}
--- packages/core/test/rules/prefer_is_not.ts
+++ packages/core/test/rules/prefer_is_not.ts
@@ -1,22 +1,39 @@
-import {testRule} from "./_utils";
+import {testRule, testRuleFix} from "./_utils";
 import {PreferIsNot} from "../../src/rules";
 
 const tests = [
-  {abap: "parser error", cnt: 0},
-  {abap: "CREATE OBJECT foobar.", cnt: 0},
+  {abap: "parser error", cnt: 0, fix: false},
+  {abap: "CREATE OBJECT foobar.", cnt: 0, fix: false},
 
-  {abap: "IF variable IS NOT INITIAL. ENDIF.", cnt: 0},
-  {abap: "IF variable NP 'TODO*'. ENDIF.", cnt: 0},
-  {abap: "IF variable <> 42. ENDIF.", cnt: 0},
+  {abap: "IF variable IS NOT INITIAL. ENDIF.", cnt: 0, fix: false},
+  {abap: "IF variable NP 'TODO*'. ENDIF.", cnt: 0, fix: false},
+  {abap: "IF variable <> 42. ENDIF.", cnt: 0, fix: false},
 
-  {abap: "IF NOT variable IS INITIAL. ENDIF.", cnt: 1},
-  {abap: "IF NOT variable CP 'TODO*'. ENDIF.", cnt: 1},
-  {abap: "IF NOT variable = 42. ENDIF.", cnt: 1},
-  {abap: "IF foo = bar AND NOT variable = 42. ENDIF.", cnt: 1},
-  {abap: "WHILE NOT variable IS INITIAL. ENDWHILE.", cnt: 1},
-  {abap: "foo = boolc( NOT variable = 42 ).", cnt: 1},
+  {abap: "IF NOT variable IS INITIAL. ENDIF.", cnt: 1, fix: true},
+  {abap: "IF NOT variable CP 'TODO*'. ENDIF.", cnt: 1, fix: false},
+  {abap: "IF NOT variable = 42. ENDIF.", cnt: 1, fix: true},
+  {abap: "IF foo = bar AND NOT variable = 42. ENDIF.", cnt: 1, fix: true},
+  {abap: "WHILE NOT variable IS INITIAL. ENDWHILE.", cnt: 1, fix: true},
+  {abap: "foo = boolc( NOT variable = 42 ).", cnt: 1, fix: true},
 
-  {abap: `if not is_valid( ). endif.`, cnt: 0},
+  {abap: `if not is_valid( ). endif.`, cnt: 0, fix: false},
 ];
 
 testRule(tests, PreferIsNot);
+
+const fixes = [
+  {input: "foo = boolc( NOT variable = 42 ).", output: "foo = boolc( variable <> 42 )."},
+  {input: "IF foo = bar AND NOT variable = 42. ENDIF.", output: "IF foo = bar AND variable <> 42. ENDIF."},
+  {input: "IF NOT variable = 42. ENDIF.", output: "IF variable <> 42. ENDIF."},
+  {input: "IF NOT variable <> 42. ENDIF.", output: "IF variable = 42. ENDIF."},
+  {input: "IF NOT variable < 42. ENDIF.", output: "IF variable > 42. ENDIF."},
+  {input: "IF NOT variable > 42. ENDIF.", output: "IF variable < 42. ENDIF."},
+  {input: "IF NOT variable <= 42. ENDIF.", output: "IF variable >= 42. ENDIF."},
+  {input: "IF NOT variable >= 42. ENDIF.", output: "IF variable <= 42. ENDIF."},
+  {input: "IF NOT variable IS INITIAL. ENDIF.", output: "IF variable IS NOT INITIAL. ENDIF."},
+  {input: "WHILE NOT variable IS INITIAL. ENDWHILE.", output: "WHILE variable IS NOT INITIAL. ENDWHILE."},
+  {input: "IF NOT variable BETWEEN 42 AND 42. ENDIF.", output: "IF variable NOT BETWEEN 42 AND 42. ENDIF."},
+  {input: "IF NOT variable IN range. ENDIF.", output: "IF variable NOT IN range. ENDIF."},
+];
+
+testRuleFix(fixes, PreferIsNot);
