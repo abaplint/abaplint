7284073ac1cb5bd91aabe06341587f3311efedf4
larshp <larshp@hotmail.com>
2021-01-25 07:47:13 +0100

    rules.abaplint.org, tt pragma and pseudo

--- web/rules/src/rule_page.ts
+++ web/rules/src/rule_page.ts
@@ -91,10 +91,10 @@ export function buildRule(meta: IRuleMetadata) {
   html = html + `&nbsp;<a href="${link}"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="2ch"><title>edit</title><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg></a>`;
 
   if (meta.pragma) {
-    html += "<br>Pragma: " + meta.pragma + "<br>";
+    html += "<br>Pragma: <tt>" + meta.pragma + "</tt><br>";
   }
   if (meta.pseudoComment) {
-    html += "<br>Pseudo comment: " + meta.pseudoComment + "<br>";
+    html += "<br>Pseudo comment: <tt>" + meta.pseudoComment + "</tt><br>";
   }
 
   html = html + "<h2>Description</h2>\n" + meta.shortDescription + "<br><br>";
