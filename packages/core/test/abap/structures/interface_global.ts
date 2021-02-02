77f90dfbcb2e1f4a467b07496284f566def6cf96
larshp <larshp@hotmail.com>
2021-02-02 11:38:14 +0100

    add testcase

--- packages/core/test/abap/structures/interface_global.ts
+++ packages/core/test/abap/structures/interface_global.ts
@@ -5,6 +5,11 @@ const cases = [
   {abap: `TYPE-POOLS abap.
   INTERFACE zif_bar PUBLIC.
   ENDINTERFACE.`},
+  {abap: `INTERFACE zif_bar PUBLIC.
+  TYPES: BEGIN OF bar,
+  foo TYPE i,
+  END OF bar.
+  ENDINTERFACE.`},
 ];
 
 structureType(cases, new InterfaceGlobal());
