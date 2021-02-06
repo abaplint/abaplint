df74461c8c9f439e9cc959054747276ff4e71847
larshp <larshp@hotmail.com>
2021-02-06 15:11:49 +0100

    ENHS xml parsing

--- packages/core/src/objects/enhancement_spot.ts
+++ packages/core/src/objects/enhancement_spot.ts
@@ -1,6 +1,13 @@
 import {AbstractObject} from "./_abstract_object";
+import {xmlToArray} from "../xml_utils";
+
+export interface IBadiDefinition {
+  name: string,
+  interface: string,
+}
 
 export class EnhancementSpot extends AbstractObject {
+  private badis: IBadiDefinition[] | undefined;
 
   public getType(): string {
     return "ENHS";
@@ -13,4 +20,34 @@ export class EnhancementSpot extends AbstractObject {
     };
   }
 
+  public setDirty(): void {
+    this.badis = undefined;
+    super.setDirty();
+  }
+
+  public listBadiDefinitions(): IBadiDefinition[] {
+    if (this.badis === undefined) {
+      this.badis = this.parseXML();
+    }
+    return this.badis;
+  }
+
+/////////////////
+
+  private parseXML(): IBadiDefinition[] {
+    const parsed = super.parseRaw();
+    if (parsed === undefined) {
+      return [];
+    }
+
+    const ret: IBadiDefinition[] = [];
+    for (const b of xmlToArray(parsed.abapGit["asx:abap"]["asx:values"]?.BADI_DATA?.ENH_BADI_DATA)) {
+      ret.push({
+        name: b.BADI_NAME?._text,
+        interface: b.INTERFACE_NAME?._text,
+      });
+    }
+
+    return ret;
+  }
 }
