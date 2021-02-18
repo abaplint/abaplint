822fb1ac80dfd775b3cecabbbc3e9f573afb6028
larshp <larshp@hotmail.com>
2021-02-18 12:55:11 +0100

    more parsing

--- packages/core/src/abap/2_statements/expressions/language.ts
+++ packages/core/src/abap/2_statements/expressions/language.ts
@@ -1,8 +1,8 @@
-import {str, Expression} from "../combi";
+import {seq, alt, Expression} from "../combi";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class Language extends Expression {
   public getRunnable(): IStatementRunnable {
-    return str("LANGUAGE SQLSCRIPT");
+    return seq("LANGUAGE", alt("SQLSCRIPT", "SQL", "GRAPH"));
   }
 }
