7ff578b6eaf7d38bb401ee491853945e43817e18
larshp <larshp@hotmail.com>
2021-02-18 12:58:19 +0100

    add CMOD, closes #1780

---
+++ packages/core/src/objects/customer_enhancement_project.ts
@@ -0,0 +1,16 @@
+import {AbstractObject} from "./_abstract_object";
+
+export class CustomerEnhancementProject extends AbstractObject {
+
+  public getType(): string {
+    return "CMOD";
+  }
+
+  public getAllowedNaming() {
+    return { // todo, verify
+      maxLength: 30,
+      allowNamespace: true,
+    };
+  }
+
+}
