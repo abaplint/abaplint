e8e8d026ab3bfd347a9a924bceed3619ea48d7dd
larshp <larshp@hotmail.com>
2021-03-17 13:23:08 +0100

    formatting

--- web/rules/src/rule_page.ts
+++ web/rules/src/rule_page.ts
@@ -91,11 +91,14 @@ export function buildRule(meta: IRuleMetadata) {
   html = html + `&nbsp;<a href="${link}"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="2ch"><title>edit</title><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg></a>`;
 
   html += `<p><tt>${meta.key}</tt><br>`;
-  if (meta.pragma) {
-    html += "Pragma: <tt>" + meta.pragma + "</tt><br>";
-  }
-  if (meta.pseudoComment) {
-    html += "<br>Pseudo comment: <tt>" + meta.pseudoComment + "</tt>";
+  if (meta.pragma || meta.pseudoComment) {
+    html += "<br>";
+    if (meta.pragma) {
+      html += "Pragma: <tt>" + meta.pragma + "</tt><br>";
+    }
+    if (meta.pseudoComment) {
+      html += "Pseudo comment: <tt>" + meta.pseudoComment + "</tt><br>";
+    }
   }
   html += "</p>";
 
