85328f7ef0a285d458993539461ac8ae9f0c11ab
larshp <larshp@hotmail.com>
2021-03-23 08:45:19 +0100

    refactor domain parsing

--- packages/core/src/objects/domain.ts
+++ packages/core/src/objects/domain.ts
@@ -5,6 +5,11 @@ import {IRegistry} from "../_iregistry";
 import {DDIC} from "../ddic";
 
 export class Domain extends AbstractObject {
+  private parsedXML: {
+    datatype?: string,
+    length?: string,
+    decimals?: string,
+  } | undefined;
   private parsedType: AbstractType | undefined;
 
   public getType(): string {
@@ -24,31 +29,41 @@ export class Domain extends AbstractObject {
   }
 
   public setDirty(): void {
+    this.parsedXML = undefined;
     this.parsedType = undefined;
     super.setDirty();
   }
 
   public parseType(reg: IRegistry): AbstractType {
-    if (this.parsedType === undefined) {
-      this.parsedType = this.parseXML(reg);
+    if (this.parsedType) {
+      return this.parsedType;
+    } else if (this.parsedXML === undefined) {
+      return new Types.UnknownType("Domain " + this.getName() + " parser error", this.getName());
     }
+    const ddic = new DDIC(reg);
+    this.parsedType = ddic.textToType(this.parsedXML.datatype, this.parsedXML.length, this.parsedXML.decimals, this.getName());
     return this.parsedType;
   }
 
-///////////////
-
-  private parseXML(reg: IRegistry): AbstractType {
-    const parsed = super.parseRaw();
-    if (parsed === undefined) {
-      return new Types.UnknownType("Domain " + this.getName() + "parser error", this.getName());
+  public parse() {
+    if (this.parsedXML) {
+      return {updated: false, runtime: 0};
     }
 
-    const ddic = new DDIC(reg);
-    const dd01v = parsed.abapGit["asx:abap"]["asx:values"].DD01V;
-    const datatype = dd01v.DATATYPE?._text;
-    const length = dd01v.LENG?._text;
-    const decimals = dd01v.DECIMALS?._text;
-    return ddic.textToType(datatype, length, decimals, this.getName());
+    const start = Date.now();
+    const parsed = super.parseRaw();
+    if (parsed === undefined) {
+      return {updated: false, runtime: 0};
+    }
+
+    const dd01v = parsed?.abapGit?.["asx:abap"]?.["asx:values"]?.DD01V;
+    this.parsedXML = {
+      datatype: dd01v?.DATATYPE?._text,
+      length: dd01v?.LENG?._text,
+      decimals: dd01v?.DECIMALS?._text,
+    };
+    const end = Date.now();
+    return {updated: true, runtime: end - start};
   }
 
 }
