8ca4608d5154502294c446c7816dfc2f1d88855a
larshp <larshp@hotmail.com>
2021-04-01 13:43:11 +0200

    add notes

--- packages/core/src/abap/flow/statement_flow.ts
+++ packages/core/src/abap/flow/statement_flow.ts
@@ -1,7 +1,19 @@
-import {StructureNode} from "../nodes";
+import {Position} from "../../position";
+import {StatementNode, StructureNode} from "../nodes";
+
+// Levels: top, FORM, METHOD, FUNCTION-MODULE, MODULE, AT, END-OF-*, GET, START-OF-SELECTION, TOP-OF-PAGE
+// Branching: IF, LOOP, DO, WHILE, CASE, TRY, ON, SELECT(loop), CATCH SYSTEM-EXCEPTIONS, AT, CHECK, PROVIDE
+// Exits: RETURN, EXIT, RAISE, MESSAGE, CONTINUE, REJECT, RESUME, STOP
+
+export type StatementFlowPath = {
+  start: Position;
+  end: Position;
+  name: string;
+  statements: StatementNode[];
+};
 
 export class StatementFlow {
-  public build(_stru: StructureNode): string {
-    return "todo";
+  public build(_stru: StructureNode): StatementFlowPath[] {
+    return [];
   }
 }
