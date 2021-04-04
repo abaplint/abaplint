d25ab56ae8620c4a27209166305def2bd05a9288
larshp <larshp@hotmail.com>
2021-04-04 07:24:41 +0200

    wip

--- packages/core/src/abap/flow/statement_flow.ts
+++ packages/core/src/abap/flow/statement_flow.ts
@@ -1,4 +1,5 @@
 import {StatementNode, StructureNode} from "../nodes";
+import * as Structures from "../3_structures/structures";
 
 // Levels: top, FORM, METHOD, FUNCTION-MODULE, MODULE, AT, END-OF-*, GET, START-OF-SELECTION, TOP-OF-PAGE
 // Branching: IF, LOOP, DO, WHILE, CASE, TRY, ON, SELECT(loop), CATCH SYSTEM-EXCEPTIONS, AT, CHECK, PROVIDE
@@ -11,19 +12,33 @@ export type StatementFlowPath = {
 
 export class StatementFlow {
   public build(stru: StructureNode): StatementFlowPath[] {
-
     const current: StatementFlowPath = {
       name: "top",
       statements: [],
     };
 
-    for (const c of stru.getChildren()) {
+    return this.traverse(stru, current, true);
+  }
+
+  private traverse(n: StructureNode, current: StatementFlowPath, add = false): StatementFlowPath[] {
+    const flows: StatementFlowPath[] = [];
+
+    for (const c of n.getChildren()) {
+      const type = c.get();
       if (c instanceof StatementNode) {
-
+        current.statements.push(c);
       } else {
-
+        if (type instanceof Structures.Normal) {
+          flows.push(...this.traverse(c, current));
+        } else {
+          console.dir("todo, " + c.get().constructor.name);
+        }
       }
     }
 
+    if (add) {
+      flows.push(current);
+    }
+    return flows;
   }
 }
--- packages/core/test/abap/flow/statement_flow.ts
+++ packages/core/test/abap/flow/statement_flow.ts
@@ -2,6 +2,7 @@ import {expect} from "chai";
 import {ABAPFile} from "../../../src/abap/abap_file";
 import {StatementFlow} from "../../../src/abap/flow/statement_flow";
 import {MemoryFile} from "../../../src/files/memory_file";
+import * as Statements from "../../../src/abap/2_statements/statements";
 import {ABAPObject} from "../../../src/objects/_abap_object";
 import {Registry} from "../../../src/registry";
 
@@ -21,5 +22,19 @@ describe("statement_flow", () => {
     const abap = `WRITE 'hello'.`;
     const res = await build(abap);
     expect(res).to.not.equal(undefined);
+    expect(res.length).to.equal(1);
+    expect(res[0].statements.length).to.equal(1);
+    expect(res[0].statements[0].get()).to.be.instanceof(Statements.Write);
+  });
+
+  it("two WRITEs", async () => {
+    const abap = `WRITE 'hello'.
+    WRITE 'world'.`;
+    const res = await build(abap);
+    expect(res).to.not.equal(undefined);
+    expect(res.length).to.equal(1);
+    expect(res[0].statements.length).to.equal(2);
+    expect(res[0].statements[0].get()).to.be.instanceof(Statements.Write);
+    expect(res[0].statements[1].get()).to.be.instanceof(Statements.Write);
   });
 });
