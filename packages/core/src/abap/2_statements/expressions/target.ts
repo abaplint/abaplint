ddb2a35f463f597c459e2be5d0a2af5506a232c4
larshp <larshp@hotmail.com>
2021-04-14 17:55:14 +0200

    upd

--- packages/core/src/abap/2_statements/expressions/target.ts
+++ packages/core/src/abap/2_statements/expressions/target.ts
@@ -1,4 +1,4 @@
-import {seq, optPrio, tok, star, altPrio, Expression} from "../combi";
+import {seq, optPrio, tok, starPrio, altPrio, Expression} from "../combi";
 import {TargetField, TargetFieldSymbol, NewObject, InlineData, InlineFS, Arrow, TableExpression, FieldAll, FieldOffset, FieldLength, TableBody, ClassName, Cast, ComponentName} from ".";
 import {InstanceArrow, StaticArrow, Dash} from "../../1_lexer/tokens";
 import {IStatementRunnable} from "../statement_runnable";
@@ -9,7 +9,7 @@ export class Target extends Expression {
     const attr = seq(tok(InstanceArrow), AttributeName);
     const comp = seq(tok(Dash), ComponentName);
 
-    const something = star(altPrio(attr, comp, TableExpression));
+    const something = starPrio(altPrio(attr, comp, TableExpression));
 
     const cast = seq(altPrio(Cast, NewObject), Arrow, FieldAll);
 
