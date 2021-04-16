fc485ca5678ecfcfc735701fb6a5683b08aa02ee
larshp <larshp@hotmail.com>
2021-04-16 08:26:19 +0200

    parser bugfix

--- packages/core/src/abap/2_statements/statements/delete_internal.ts
+++ packages/core/src/abap/2_statements/statements/delete_internal.ts
@@ -1,6 +1,6 @@
 import {IStatement} from "./_statement";
-import {seq, alt, opt, per, plus, altPrio} from "../combi";
-import {Target, Source, Dynamic, ComponentCompare, ComponentCond, SimpleName, Field, FieldSub} from "../expressions";
+import {seq, alt, opt, optPrio, per, plus, altPrio} from "../combi";
+import {Target, Source, Dynamic, ComponentCompare, ComponentCond, SimpleName, Field, FieldSub, FieldOffset, FieldLength} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class DeleteInternal implements IStatement {
@@ -9,10 +9,10 @@ export class DeleteInternal implements IStatement {
 // todo, is READ and DELETE similar? something can be reused?
     const index = seq("INDEX", Source);
 
-    const using = seq("USING KEY", alt(SimpleName, Dynamic));
+    const using = seq("USING KEY", altPrio(SimpleName, Dynamic));
 
-    const fromTo = seq(opt(seq("FROM", Source)),
-                       opt(seq("TO", Source)));
+    const fromTo = seq(optPrio(seq("FROM", Source)),
+                       optPrio(seq("TO", Source)));
 
     const where = seq("WHERE", alt(ComponentCond, Dynamic));
 
@@ -24,10 +24,12 @@ export class DeleteInternal implements IStatement {
                       Target,
                       alt(per(index, using), fromTo, key), opt(where));
 
+    const f = seq(FieldSub, optPrio(FieldOffset), optPrio(FieldLength));
+
     const adjacent = seq("ADJACENT DUPLICATES FROM",
                          Target,
-                         opt(seq("COMPARING", altPrio("ALL FIELDS", plus(alt(FieldSub, Dynamic))))),
-                         opt(seq("USING KEY", Field)));
+                         opt(seq("COMPARING", altPrio("ALL FIELDS", plus(altPrio(f, Dynamic))))),
+                         optPrio(seq("USING KEY", Field)));
 
     return seq("DELETE", alt(table, adjacent));
   }
