1759183454d41e6ff381e57edca0e2eb9380876c
larshp <larshp@hotmail.com>
2021-04-18 07:59:02 +0200

    minor performance

--- packages/core/src/abap/2_statements/expressions/sql_aggregation.ts
+++ packages/core/src/abap/2_statements/expressions/sql_aggregation.ts
@@ -1,4 +1,4 @@
-import {alt, seq, tok, Expression, optPrio} from "../combi";
+import {seq, altPrio, tok, Expression, optPrio} from "../combi";
 import {ParenLeft, ParenLeftW} from "../../1_lexer/tokens";
 import {Field} from ".";
 import {IStatementRunnable} from "../statement_runnable";
@@ -7,15 +7,15 @@ import {Dynamic} from "./dynamic";
 export class SQLAggregation extends Expression {
   public getRunnable(): IStatementRunnable {
 
-    const f = alt(Field, Dynamic);
+    const f = altPrio(Field, Dynamic);
     const fparen = seq("(", Field, ")");
-    const count = seq("COUNT", alt(tok(ParenLeft), tok(ParenLeftW)), optPrio("DISTINCT"), alt("*", Field, fparen), ")");
-    const max = seq("MAX", alt(tok(ParenLeft), tok(ParenLeftW)), f, ")");
-    const min = seq("MIN", alt(tok(ParenLeft), tok(ParenLeftW)), f, ")");
-    const sum = seq("SUM", alt(tok(ParenLeft), tok(ParenLeftW)), f, ")");
-    const avg = seq("AVG", alt(tok(ParenLeft), tok(ParenLeftW)), f, ")");
+    const count = seq("COUNT", altPrio(tok(ParenLeft), tok(ParenLeftW)), optPrio("DISTINCT"), altPrio("*", Field, fparen), ")");
+    const max = seq("MAX", altPrio(tok(ParenLeft), tok(ParenLeftW)), f, ")");
+    const min = seq("MIN", altPrio(tok(ParenLeft), tok(ParenLeftW)), f, ")");
+    const sum = seq("SUM", altPrio(tok(ParenLeft), tok(ParenLeftW)), f, ")");
+    const avg = seq("AVG", altPrio(tok(ParenLeft), tok(ParenLeftW)), f, ")");
 
-    return alt(count, max, min, sum, avg);
+    return altPrio(count, max, min, sum, avg);
 
   }
 }
--- packages/core/src/abap/2_statements/expressions/table_expression.ts
+++ packages/core/src/abap/2_statements/expressions/table_expression.ts
@@ -1,4 +1,4 @@
-import {seq, opt, tok, alt, altPrio, plus, ver, Expression} from "../combi";
+import {seq, opt, tok, alt, altPrio, optPrio, plus, ver, Expression} from "../combi";
 import {BracketLeftW, WBracketRight, WBracketRightW} from "../../1_lexer/tokens";
 import {Dynamic, Source, SimpleName, ComponentChainSimple} from ".";
 import {Version} from "../../../version";
@@ -10,7 +10,7 @@ export class TableExpression extends Expression {
     const key = seq("KEY", SimpleName);
     const index = seq("INDEX", Source);
     const ret = seq(tok(BracketLeftW),
-                    alt(Source, seq(opt(key), opt("COMPONENTS"), alt(fields, index))),
+                    alt(Source, seq(optPrio(key), opt("COMPONENTS"), altPrio(fields, index))),
                     altPrio(tok(WBracketRight), tok(WBracketRightW)));
     return ver(Version.v740sp02, ret);
   }
