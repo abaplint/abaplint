198bb499ddbe2cc6eabcc27d182e5adaf22bb5e5
larshp <larshp@hotmail.com>
2021-04-22 15:19:45 +0200

    add version for ENUM statements

--- packages/core/src/abap/2_statements/statements/type_enum.ts
+++ packages/core/src/abap/2_statements/statements/type_enum.ts
@@ -1,14 +1,15 @@
 import {IStatement} from "./_statement";
-import {seq} from "../combi";
+import {seq, ver} from "../combi";
 import {Value, NamespaceSimpleName} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
+import {Version} from "../../../version";
 
 export class TypeEnum implements IStatement {
 
   public getMatcher(): IStatementRunnable {
 
 // it is also possible to define without Value, this is covered by normal type
-    const ret = seq("TYPES", NamespaceSimpleName, Value);
+    const ret = ver(Version.v751, seq("TYPES", NamespaceSimpleName, Value));
 
     return ret;
   }
--- packages/core/src/abap/2_statements/statements/type_enum_end.ts
+++ packages/core/src/abap/2_statements/statements/type_enum_end.ts
@@ -1,14 +1,15 @@
 import {IStatement} from "./_statement";
-import {opt, seq} from "../combi";
+import {opt, seq, ver} from "../combi";
 import {NamespaceSimpleName} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
+import {Version} from "../../../version";
 
 export class TypeEnumEnd implements IStatement {
 
   public getMatcher(): IStatementRunnable {
     const structure = seq("STRUCTURE", NamespaceSimpleName);
 
-    const ret = seq("TYPES", "END OF", "ENUM", NamespaceSimpleName, opt(structure));
+    const ret = ver(Version.v751, seq("TYPES", "END OF", "ENUM", NamespaceSimpleName, opt(structure)));
 
     return ret;
   }
