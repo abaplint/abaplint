8e365828549cfeba4478eda5d79ef44ed5dcefc2
larshp <larshp@hotmail.com>
2021-04-27 15:43:17 +0200

    FREE MEMORY without ID is obsolete

--- packages/core/src/abap/2_statements/statements/free_memory.ts
+++ packages/core/src/abap/2_statements/statements/free_memory.ts
@@ -1,5 +1,5 @@
 import {IStatement} from "./_statement";
-import {verNot, seq} from "../combi";
+import {verNot, seq, optPrio} from "../combi";
 import {Source} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
@@ -7,7 +7,7 @@ import {IStatementRunnable} from "../statement_runnable";
 export class FreeMemory implements IStatement {
 
   public getMatcher(): IStatementRunnable {
-    const ret = seq("FREE MEMORY ID", Source);
+    const ret = seq("FREE MEMORY", optPrio(seq("ID", Source)));
 
     return verNot(Version.Cloud, ret);
   }
--- packages/core/test/abap/statements/free_memory.ts
+++ packages/core/test/abap/statements/free_memory.ts
@@ -4,6 +4,7 @@ import * as Statements from "../../../src/abap/2_statements/statements";
 const tests = [
   "free memory id ls_structure.",
   "free memory id 'ALV_SUBMIT_TO_SPOOL'.",
+  "free memory.",
 ];
 
 statementType(tests, "FREE MEMORY", Statements.FreeMemory);
