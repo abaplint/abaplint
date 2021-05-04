ad3f41abc879d1976a9555b42d0af05bfc6827d8
larshp <larshp@hotmail.com>
2021-05-04 14:33:13 +0200

    html formatting

--- web/rules/src/rule_page.ts
+++ web/rules/src/rule_page.ts
@@ -88,19 +88,19 @@ export function buildRule(meta: IRuleMetadata) {
   const link = findPath(meta.key);
   // https://github.com/refactoringui/heroicons/
   // eslint-disable-next-line max-len
-  html = html + `&nbsp;<a href="${link}"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="2ch"><title>edit</title><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg></a>`;
+  html = html + `&nbsp;<a href="${link}"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="2ch"><title>edit</title><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg></a>\n`;
 
-  html += `<p><tt>${meta.key}</tt><br>`;
+  html += `<p><tt>${meta.key}</tt><br>\n`;
   if (meta.pragma || meta.pseudoComment) {
     html += "<br>";
     if (meta.pragma) {
-      html += "Pragma: <tt>" + meta.pragma + "</tt><br>";
+      html += "Pragma: <tt>" + meta.pragma + "</tt><br>\n";
     }
     if (meta.pseudoComment) {
-      html += "Pseudo comment: <tt>" + meta.pseudoComment + "</tt><br>";
+      html += "Pseudo comment: <tt>" + meta.pseudoComment + "</tt><br>\n";
     }
   }
-  html += "</p>";
+  html += "</p>\n";
 
   html = html + "<h2>Description</h2>\n<p>" + meta.shortDescription + "</p>";
 
