e3a457106b8e0a410ac1a3e16c47518a9325bba5
Lars Hvam <larshp@hotmail.com>
2021-05-05 14:21:26 +0200

    minor changes (#1943)
    * bugfix, unused var
    
    * bugfix, select_performance
    
    * html formatting
    
    * prefer_inline, add skipped test
    
    * modify_only_own_db_tables, fix false positive
    
    * 2.74.13
    
    * minor changes

--- packages/core/src/abap/2_statements/expressions/method_param_optional.ts
+++ packages/core/src/abap/2_statements/expressions/method_param_optional.ts
@@ -1,10 +1,10 @@
-import {seq, Expression, opt} from "../combi";
+import {seq, Expression, optPrio} from "../combi";
 import {MethodParam} from ".";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class MethodParamOptional extends Expression {
   public getRunnable(): IStatementRunnable {
-    return seq(MethodParam, opt("OPTIONAL"));
+    return seq(MethodParam, optPrio("OPTIONAL"));
   }
 
 }
