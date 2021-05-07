c4fbb41e5346b7fbcb61ba3b81f1bf6a6b2cba2d
larshp <larshp@hotmail.com>
2021-05-07 07:56:55 +0200

    use_line_exists, bugfix for BINARY SEARCH

--- packages/core/test/rules/use_line_exists.ts
+++ packages/core/test/rules/use_line_exists.ts
@@ -37,7 +37,7 @@ READ TABLE mt_log WITH KEY is_important = abap_true TRANSPORTING NO FIELDS.
 rv_boolean = boolc( sy-subrc IS INITIAL ).`, cnt: 1},
   {abap: `
 READ TABLE lt_t100u TRANSPORTING NO FIELDS WITH KEY arbgb = lv_msg_id msgnr = <ls_t100_text>-msgnr BINARY SEARCH.
-CHECK sy-subrc = 0. "if original message doesn't exist no translations added`, cnt: 1},
+CHECK sy-subrc = 0. "if original message doesn't exist no translations added`, cnt: 0},
   {abap: `
 READ TABLE my_table TRANSPORTING NO FIELDS WITH KEY key = 'A'.
 IF ( sy-subrc <> 0 ).
@@ -57,7 +57,10 @@ LOOP AT nested_tree ASSIGNING FIELD-SYMBOL(<item>) FROM sy-tabix.
   ENDIF.
   INSERT <item>-left INTO TABLE result.
 ENDLOOP.`, cnt: 0},
-
+  {abap: `
+  READ TABLE my_table TRANSPORTING NO FIELDS WITH KEY key = 'A' BINARY SEARCH.
+  IF sy-subrc = 0.
+  ENDIF.`, cnt: 0},
 ];
 
 testRule(tests, UseLineExists);
