d8b6399c41bd90bd93e40030a11c63860c428135
larshp <larshp@hotmail.com>
2021-05-25 09:45:41 +0200

    bugfix, closes #1972

--- packages/core/test/abap/statements/transfer.ts
+++ packages/core/test/abap/statements/transfer.ts
@@ -4,6 +4,7 @@ import * as Statements from "../../../src/abap/2_statements/statements";
 const tests = [
   "TRANSFER lv_file  TO lv_default_file_name.",
   "TRANSFER <rawdata> TO p_back LENGTH bytes.",
+  "TRANSFER l_data TO 'foobar.txt'.",
   "TRANSFER iv_text TO iv_path NO END OF LINE.",
 ];
 
