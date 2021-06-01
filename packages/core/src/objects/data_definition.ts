a2def91184850cf6402bd80d902796837e62c707
Lars Hvam <larshp@hotmail.com>
2021-06-01 14:36:30 +0200

    parse view name

--- packages/core/src/objects/data_definition.ts
+++ packages/core/src/objects/data_definition.ts
@@ -1,6 +1,8 @@
 import {AbstractObject} from "./_abstract_object";
+import {IParseResult} from "./_iobject";
 
 export class DataDefinition extends AbstractObject {
+  private sqlViewName: string | undefined = undefined;
 
   public getType(): string {
     return "DDLS";
@@ -14,11 +16,26 @@ export class DataDefinition extends AbstractObject {
   }
 
   public getSQLViewName(): string | undefined {
-    return undefined;
+    return this.sqlViewName;
   }
 
   public getDescription(): string | undefined {
     // todo
     return undefined;
+  }
+
+  public parse(): IParseResult {
+    if (this.isDirty() === false) {
+      return {updated: false, runtime: 0};
+    }
+
+    this.sqlViewName = undefined;
+    const asddls = this.getFiles().find(f => f.getFilename().endsWith(".asddls"));
+    const match = asddls?.getRaw().match(/@AbapCatalog\.sqlViewName: '(\w+)'/);
+    if (match) {
+      this.sqlViewName = match[1];
+    }
+
+    return {updated: true, runtime: 0};
   }
 }
