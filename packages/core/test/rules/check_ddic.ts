c152e003ca2f02c6e548341561a30847135a618b
Lars Hvam <larshp@hotmail.com>
2021-06-26 17:08:31 +0200

    check_ddic, better error message

--- packages/core/test/rules/check_ddic.ts
+++ packages/core/test/rules/check_ddic.ts
@@ -5,7 +5,7 @@ import {MemoryFile} from "../../src/files/memory_file";
 
 describe("Rule: no_unknown_ddic", () => {
 
-  it("ok, resolved", async () => {
+  it("ok, resolved, dtel", async () => {
     const xml = `
 <?xml version="1.0" encoding="utf-8"?>
 <abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
@@ -34,6 +34,28 @@ describe("Rule: no_unknown_ddic", () => {
 
     const issues = new CheckDDIC().initialize(reg).run(reg.getFirstObject()!);
     expect(issues.length).to.equal(1);
+  });
+
+  it("error, DOMNAME unexpectedly empty", async () => {
+    const xml = `
+<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <DD04V>
+    <ROLLNAME>ZDDIC</ROLLNAME>
+    <DOMNAME></DOMNAME>
+    <REFKIND>D</REFKIND>
+   </DD04V>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+    const reg = new Registry().addFile(new MemoryFile("zddic.dtel.xml", xml));
+    await reg.parseAsync();
+
+    const issues = new CheckDDIC().initialize(reg).run(reg.getFirstObject()!);
+    expect(issues.length).to.equal(1);
+    expect(issues[0].getMessage()).to.include("DOMNAME unexpectely empty");
   });
 
 });
