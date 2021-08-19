39fb628f9e6dd4cc1321cae8eb0c1572e6ee5b85
larshp <larshp@hotmail.com>
2021-08-19 11:49:12 +0200

    trigger regression test

--- packages/core/test/abap/syntax/syntax.ts
+++ packages/core/test/abap/syntax/syntax.ts
@@ -5092,7 +5092,6 @@ ENDCLASS.`;
     expect(issues[0]?.getMessage()).to.equal(undefined);
   });
 
-
 // todo, static method cannot access instance attributes
 // todo, can a private method access protected attributes?
 // todo, readonly fields(constants + enums + attributes flagged read-only)
