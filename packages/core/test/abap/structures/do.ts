b503371c6c7af20ddcc7adaf245cff1ec7406c00
larshp <larshp@hotmail.com>
2021-08-27 12:10:46 +0200

    add testcase

--- packages/core/test/abap/structures/do.ts
+++ packages/core/test/abap/structures/do.ts
@@ -5,6 +5,7 @@ const cases = [
   {abap: "DO 2 TIMES. ENDDO."},
   {abap: "DO 2 TIMES. WRITE bar. ENDDO."},
   {abap: "DO 2 TIMES. MOVE-CORRESPONDING foo TO bar. ENDDO."},
+  {abap: "DO 2 TIMES. MODIFY SCREEN. ENDDO."},
   {abap: "DO 2 TIMES. INSERT 'sdf' 'sdf' INTO header. ENDDO."},
 ];
 
