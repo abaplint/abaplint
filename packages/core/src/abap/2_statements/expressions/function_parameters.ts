4dc4711a6e8043b5e165a8a5041923d8605169ea
larshp <larshp@hotmail.com>
2021-09-01 07:27:29 +0200

    #2022

--- packages/core/src/abap/2_statements/expressions/function_parameters.ts
+++ packages/core/src/abap/2_statements/expressions/function_parameters.ts
@@ -1,10 +1,18 @@
-import {seq, optPrio, altPrio, Expression} from "../combi";
-import {ParameterListS, ParameterListT, ParameterListExceptions, Field} from ".";
+import {seq, optPrio, altPrio, Expression, ver, plusPrio} from "../combi";
+import {ParameterListT, ParameterListExceptions, Field} from ".";
 import {IStatementRunnable} from "../statement_runnable";
+import {ParameterName} from "./parameter_name";
+import {Source} from "./source";
+import {Version} from "../../../version";
+import {SimpleSource3} from "./simple_source3";
 
 export class FunctionParameters extends Expression {
   public getRunnable(): IStatementRunnable {
-    const exporting = seq("EXPORTING", ParameterListS);
+
+    const s = altPrio(ver(Version.v740sp02, Source), SimpleSource3);
+    const exp = plusPrio(seq(ParameterName, "=", s));
+
+    const exporting = seq("EXPORTING", exp);
     const importing = seq("IMPORTING", ParameterListT);
     const changing = seq("CHANGING", ParameterListT);
     const tables = seq("TABLES", ParameterListT);
