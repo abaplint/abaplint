99d02dd195778d10d562af42c9ccaa4323a69ba6
larshp <larshp@hotmail.com>
2021-09-08 18:54:17 +0200

    refactor

--- packages/core/src/lsp/semantic.ts
+++ packages/core/src/lsp/semantic.ts
@@ -1,4 +1,7 @@
 import * as SemanticProtocol from "vscode-languageserver-protocol/lib/common/protocol.semanticTokens";
+import {VirtualPosition} from "..";
+import {Comment, Punctuation, String, StringTemplate, StringTemplateBegin, StringTemplateEnd, StringTemplateMiddle} from "../abap/1_lexer/tokens";
+import {TokenNodeRegex} from "../abap/nodes";
 import {Position} from "../position";
 import {IRegistry} from "../_iregistry";
 import {ITextDocumentRange} from "./_interfaces";
@@ -14,16 +17,26 @@ interface Token {
 
 export class SemanticHighlighting {
   private readonly reg: IRegistry;
+  private static readonly tokenTypes: string[] = [];
+  private static tokenTypeMap: {[name: string]: number};
 
   public constructor(reg: IRegistry) {
     this.reg = reg;
+    if (SemanticHighlighting.tokenTypes.length === 0) {
+      SemanticHighlighting.tokenTypeMap = {};
+
+      for (const t in SemanticProtocol.SemanticTokenTypes) {
+        SemanticHighlighting.tokenTypeMap[t] = SemanticHighlighting.tokenTypes.length;
+        SemanticHighlighting.tokenTypes.push(t);
+      }
+    }
   }
 
   public semanticTokensLegend(): SemanticProtocol.SemanticTokensLegend {
     // https://code.visualstudio.com/api/language-extensions/semantic-highlight-guide#semantic-token-scope-map
     // https://microsoft.github.io/language-server-protocol/specifications/specification-3-17/#semanticTokenTypes
     return {
-      tokenTypes: [SemanticProtocol.SemanticTokenTypes.keyword, SemanticProtocol.SemanticTokenTypes.variable],
+      tokenTypes: SemanticHighlighting.tokenTypes,
       tokenModifiers: [],
     };
   }
@@ -43,13 +56,30 @@ export class SemanticHighlighting {
         break;
       } else if (s.getLastToken().getEnd().isBefore(rangeStartPosition)) {
         continue;
+      } else if (s.getFirstToken().getStart() instanceof VirtualPosition) {
+        continue;
       }
-      for (const t of s.getTokens()) {
+      for (const t of s.getTokenNodes()) {
+        console.dir(t);
+        let tokenType = SemanticProtocol.SemanticTokenTypes.keyword;
+        if (t.get() instanceof String
+            || t.get() instanceof StringTemplate
+            || t.get() instanceof StringTemplateBegin
+            || t.get() instanceof StringTemplateEnd
+            || t.get() instanceof StringTemplateMiddle) {
+          tokenType = SemanticProtocol.SemanticTokenTypes.string;
+        } else if (t.get() instanceof Comment) {
+          tokenType = SemanticProtocol.SemanticTokenTypes.comment;
+        } else if (t instanceof TokenNodeRegex || t.get() instanceof Punctuation) {
+          tokenType = SemanticProtocol.SemanticTokenTypes.method;
+        }
+        const token = t.getFirstToken();
+
         tokens.push({
-          line: t.getStart().getRow() - 1,
-          startChar: t.getStart().getCol() - 1,
-          length: t.getStr().length,
-          tokenType: SemanticProtocol.SemanticTokenTypes.keyword,
+          line: token.getStart().getRow() - 1,
+          startChar: token.getStart().getCol() - 1,
+          length: token.getStr().length,
+          tokenType: tokenType,
           tokenModifiers: [],
         });
       }
@@ -74,7 +104,7 @@ export class SemanticHighlighting {
         ret.push(t.startChar); // todo, delta?
       }
       ret.push(t.length);
-      ret.push(0); // everything as the first token type
+      ret.push(SemanticHighlighting.tokenTypeMap[t.tokenType]);
       ret.push(0); // no modifier logic implemented yet
 
       prevLine = t.line;
