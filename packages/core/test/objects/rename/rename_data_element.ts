36a113a0b5f2dafc70b24a912439181b0202b2f9
Lars Hvam <larshp@hotmail.com>
2021-09-28 17:48:27 +0200

    add testcase for DTEL renaming

---
+++ packages/core/test/objects/rename/rename_data_element.ts
@@ -0,0 +1,47 @@
+import {MemoryFile} from "../../../src/files/memory_file";
+import {expect} from "chai";
+import {Renamer} from "../../../src/objects/rename/renamer";
+import {Registry} from "../../../src/registry";
+
+describe("Rename Data Element", () => {
+
+  it.only("DTEL, no references, just the object", () => {
+    const xml = `<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <DD04V>
+    <ROLLNAME>ZBAR</ROLLNAME>
+    <DDLANGUAGE>E</DDLANGUAGE>
+    <HEADLEN>55</HEADLEN>
+    <SCRLEN1>10</SCRLEN1>
+    <SCRLEN2>20</SCRLEN2>
+    <SCRLEN3>40</SCRLEN3>
+    <DDTEXT>testing</DDTEXT>
+    <REPTEXT>testing</REPTEXT>
+    <SCRTEXT_S>testing</SCRTEXT_S>
+    <SCRTEXT_M>testing</SCRTEXT_M>
+    <SCRTEXT_L>testing</SCRTEXT_L>
+    <DTELMASTER>E</DTELMASTER>
+    <DATATYPE>CHAR</DATATYPE>
+    <LENG>000001</LENG>
+    <OUTPUTLEN>000001</OUTPUTLEN>
+   </DD04V>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+
+    const reg = new Registry().addFiles([
+      new MemoryFile("zbar.dtel.xml", xml),
+    ]).parse();
+
+    new Renamer(reg).rename("DTEL", "zbar", "foo");
+
+    expect(reg.getObjectCount()).to.equal(1);
+    for (const f of reg.getFiles()) {
+      expect(f.getFilename()).to.equal("foo.dtel.xml");
+      expect(f.getRaw().includes("<ROLLNAME>FOO</ROLLNAME>")).to.equal(true);
+    }
+  });
+
+});
