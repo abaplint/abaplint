525c701b9cc9b26229ee4e397cacde20f2a7e6fd
Lars Hvam <larshp@hotmail.com>
2021-10-07 17:42:05 +0200

    refactor

--- packages/core/src/ddic_references.ts
+++ packages/core/src/ddic_references.ts
@@ -2,16 +2,13 @@ import {IObject} from "./objects/_iobject";
 import {IDDICReferences, IObjectAndToken} from "./_iddic_references";
 
 export class DDICReferences implements IDDICReferences {
-  private readonly index: { [name: string]: { [type: string]: IObjectAndToken[] } } = {};
+  private readonly nameTypeIndex: { [name: string]: { [type: string]: IObjectAndToken[] } } = {};
 
   public setUsing(obj: IObject, using: IObjectAndToken[]): void {
-    const newName = obj.getName().toUpperCase();
-    const newType = obj.getType();
-
-    if (this.index[newName] === undefined) {
-      this.index[newName] = {};
+    this.clear(obj);
+    for (const u of using) {
+      this.addUsing(obj, u);
     }
-    this.index[newName][newType] = using;
   }
 
   public addUsing(obj: IObject, using: IObjectAndToken | undefined) {
@@ -22,24 +19,28 @@ export class DDICReferences implements IDDICReferences {
     const newName = obj.getName().toUpperCase();
     const newType = obj.getType();
 
-    if (this.index[newName] === undefined) {
-      this.index[newName] = {};
+    if (this.nameTypeIndex[newName] === undefined) {
+      this.nameTypeIndex[newName] = {};
     }
-    if (this.index[newName][newType] === undefined) {
-      this.index[newName][newType] = [];
+    if (this.nameTypeIndex[newName][newType] === undefined) {
+      this.nameTypeIndex[newName][newType] = [];
     }
-    this.index[newName][newType].push(using);
+    this.nameTypeIndex[newName][newType].push(using);
   }
 
   public clear(obj: IObject) {
-    this.setUsing(obj, []);
+    const newName = obj.getName().toUpperCase();
+    const newType = obj.getType();
+    if (this.nameTypeIndex[newName]?.[newType]) {
+      this.nameTypeIndex[newName][newType] = [];
+    }
   }
 
   public listUsing(obj: IObject): readonly IObjectAndToken[] {
     const newName = obj.getName().toUpperCase();
     const newType = obj.getType();
 
-    const found = this.index[newName]?.[newType];
+    const found = this.nameTypeIndex[newName]?.[newType];
     if (found !== undefined) {
       return found;
     } else {
@@ -54,9 +55,9 @@ export class DDICReferences implements IDDICReferences {
     const searchName = obj.getName().toUpperCase();
     const searchType = obj.getType();
 
-    for (const name in this.index) {
-      for (const type in this.index[name]) {
-        for (const f of this.index[name][type]) {
+    for (const name in this.nameTypeIndex) {
+      for (const type in this.nameTypeIndex[name]) {
+        for (const f of this.nameTypeIndex[name][type]) {
           if (f.object && f.object.getType() === searchType && f.object.getName() === searchName) {
             ret.push({type, name, token: f.token, filename: f.filename});
           }
