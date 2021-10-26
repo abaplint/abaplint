2614a930aad2d0f0dc46bfb735e52c052be2b4e2
Lars Hvam <larshp@hotmail.com>
2021-10-26 17:25:15 +0200

    dangerous_statement: find dynamic sql

--- packages/core/test/rules/dangerous_statement.ts
+++ packages/core/test/rules/dangerous_statement.ts
@@ -8,6 +8,13 @@ const tests = [
   {abap: "parser error", cnt: 0},
   {abap: "SYSTEM-CALL foo.", cnt: 1},
   {abap: "INSERT REPORT lv_prog FROM lt_tab VERSION 'X'.", cnt: 1},
+
+  {abap: "UPDATE /dmo/flight SET (dynamicUpdate) WHERE carrier_id = @carrierId AND connection_id = @connectionId.", cnt: 1},
+  {abap: "UPDATE /dmo/flight SET foo = 2 WHERE carrier_id = @carrierId AND connection_id = @connectionId.", cnt: 0},
+  {abap: "SELECT * FROM /dmo/flight WHERE (sql) INTO table @DATA(results).", cnt: 1},
+  {abap: "SELECT * FROM /dmo/flight WHERE foo = bar INTO table @DATA(results).", cnt: 0},
+  {abap: "SELECT * FROM (dbTable) INTO TABLE @<results> UP TO 100 ROWS.", cnt: 1},
+  {abap: "SELECT * FROM tab INTO TABLE @<results> UP TO 100 ROWS.", cnt: 0},
 ];
 
 testRule(tests, DangerousStatement);
