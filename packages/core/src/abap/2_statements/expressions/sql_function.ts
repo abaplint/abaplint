1f0893edcdb094dc128ff5b6cf1d8be79726a6bd
Lars Hvam <larshp@hotmail.com>
2021-11-16 06:34:15 +0100

    2.80.10, bugfix (#2235)
    * wip
    
    * bugfix, #855
    
    * upd
    
    * 2.80.10

--- packages/core/src/abap/2_statements/expressions/sql_function.ts
+++ packages/core/src/abap/2_statements/expressions/sql_function.ts
@@ -8,11 +8,16 @@ import {SQLFieldName} from "./sql_field_name";
 
 export class SQLFunction extends Expression {
   public getRunnable(): IStatementRunnable {
+    const param = altPrio(SQLFieldName, SQLAliasField);
 
     const uuid = ver(Version.v754, seq("uuid", tok(ParenLeftW), tok(WParenRightW)));
-    const abs = ver(Version.v751, seq("abs", tok(ParenLeftW), altPrio(SQLFieldName, SQLAliasField), tok(WParenRightW)));
-    const cast = ver(Version.v750, seq("cast", tok(ParenLeftW), altPrio(SQLFieldName, SQLAliasField), "AS CHAR", tok(ParenLeftW), Integer, tok(WParenRightW), tok(WParenRightW)));
+    const abs = ver(Version.v740sp05, seq("abs", tok(ParenLeftW), param, tok(WParenRightW)));
+    const ceil = ver(Version.v740sp05, seq("ceil", tok(ParenLeftW), param, tok(WParenRightW)));
+    const floor = ver(Version.v740sp05, seq("floor", tok(ParenLeftW), param, tok(WParenRightW)));
+    const div = ver(Version.v740sp05, seq("div", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
+    const mod = ver(Version.v740sp05, seq("mod", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
+    const cast = ver(Version.v750, seq("cast", tok(ParenLeftW), param, "AS CHAR", tok(ParenLeftW), Integer, tok(WParenRightW), tok(WParenRightW)));
 
-    return altPrio(uuid, abs, cast);
+    return altPrio(uuid, abs, ceil, floor, cast, div, mod);
   }
 }
