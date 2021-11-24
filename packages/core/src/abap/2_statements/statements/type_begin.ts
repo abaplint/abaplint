7ad012c1b13f31b09d63bdc12fc703b210a22539
Lars Hvam <larshp@hotmail.com>
2021-11-24 06:39:45 +0100

    2.81.4, bugfix, Unexpected TYPEEND (#2251)
    * enable test case
    
    * bugfix, #2254
    
    * update node types
    
    * 2.81.4

--- packages/core/src/abap/2_statements/statements/type_begin.ts
+++ packages/core/src/abap/2_statements/statements/type_begin.ts
@@ -1,13 +1,14 @@
 import {IStatement} from "./_statement";
-import {seq} from "../combi";
+import {optPrio, seq, verNot} from "../combi";
 import {NamespaceSimpleName} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
+import {Version} from "../../../version";
 
 export class TypeBegin implements IStatement {
 
   public getMatcher(): IStatementRunnable {
 
-    const ret = seq("TYPES", "BEGIN OF", NamespaceSimpleName);
+    const ret = seq("TYPES", "BEGIN OF", NamespaceSimpleName, optPrio(verNot(Version.Cloud, "%_FINAL")));
 
     return ret;
   }
