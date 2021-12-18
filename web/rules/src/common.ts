4d252f980f27e73058e5057f60150732be5bd9b0
Lars Hvam <larshp@hotmail.com>
2021-12-18 09:48:48 +0100

    surface singlefile tag, closes #2035

--- web/rules/src/common.ts
+++ web/rules/src/common.ts
@@ -2,7 +2,6 @@
 import {IRuleMetadata, RuleTag} from "../../../packages/core/build/src/rules/_irule";
 
 
-
 export function preamble() {
   return `<!DOCTYPE html>
 <html>
@@ -84,6 +83,13 @@ function styleguide() {
   return `&nbsp;${styleguideIcon}`;
 }
 
+export const singleFileIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" height="2ch">
+<title>single file</title>
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" /></svg>`;
+function singleFile() {
+  return `&nbsp;${singleFileIcon}`;
+}
+
 export function renderIcons(tags: IRuleMetadata["tags"]): string {
   let html = "";
   if (tags?.includes(RuleTag.Quickfix)) {
@@ -112,6 +118,9 @@ export function renderIcons(tags: IRuleMetadata["tags"]): string {
   }
   if (tags?.includes(RuleTag.Security)) {
     html = html + security();
+  }
+  if (tags?.includes(RuleTag.SingleFile)) {
+    html = html + singleFile();
   }
   return html;
 }
