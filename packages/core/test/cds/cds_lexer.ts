7f98b7846a8647e87d99d0d68edab3748c5f2d35
Lars Hvam <larshp@hotmail.com>
2021-12-29 09:31:32 +0100

    2.83.10, CDS Lexer bugfix (#2314)
    * cds lexer testcases
    
    * cds lexer bugfix
    
    * field key
    
    * upd
    
    * 2.83.10

---
+++ packages/core/test/cds/cds_lexer.ts
@@ -0,0 +1,58 @@
+import {MemoryFile} from "../../src";
+import {expect} from "chai";
+import {CDSLexer} from "../../src/cds/cds_lexer";
+
+describe("CDS Lexer", () => {
+
+  it("basic 1", () => {
+    const cds = `define view zhvamfoocust as select from zhvam_cust {
+  key foo
+}`;
+    const file = new MemoryFile("foobar.ddls.asddls", cds);
+    const result = CDSLexer.run(file);
+    expect(result.length).to.equal(11);
+    expect(result[7].getStr()).to.equal("{");
+    expect(result[7].getRow()).to.equal(1);
+  });
+
+  it("basic 2", () => {
+    const cds = `define view zhvamfoocust as select from zhvam_cust
+{
+  key foo
+}`;
+    const file = new MemoryFile("foobar.ddls.asddls", cds);
+    const result = CDSLexer.run(file);
+    expect(result.length).to.equal(11);
+    expect(result[7].getStr()).to.equal("{");
+    expect(result[7].getRow()).to.equal(2);
+  });
+
+  it("basic 3", () => {
+    const cds = `@VDM.viewType: #BASIC
+
+define view zhvamfoocust as select from zhvam_cust
+{
+  key foo
+}`;
+    const file = new MemoryFile("foobar.ddls.asddls", cds);
+    const result = CDSLexer.run(file);
+    expect(result.length).to.equal(16);
+    expect(result[12].getStr()).to.equal("{");
+    expect(result[12].getRow()).to.equal(4);
+  });
+
+  it("basic 4", () => {
+    const cds = `@VDM.viewType: #BASIC // comment
+
+define view zhvamfoocust as select from zhvam_cust
+{
+  key foo
+}`;
+    const file = new MemoryFile("foobar.ddls.asddls", cds);
+    const result = CDSLexer.run(file);
+    expect(result.length).to.equal(16);
+    expect(result[12].getStr()).to.equal("{");
+    expect(result[12].getRow()).to.equal(4);
+  });
+
+});
