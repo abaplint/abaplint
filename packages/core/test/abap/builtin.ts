e89d82e009c736961496d5d7ac178df646282d72
Lars Hvam <larshp@hotmail.com>
2022-02-08 14:33:54 +0100

    closes #2394

--- packages/core/test/abap/builtin.ts
+++ packages/core/test/abap/builtin.ts
@@ -5,6 +5,6 @@ describe("builtin", () => {
   it("to upper, default parameter", async () => {
     const method = new BuiltIn().searchBuiltin("TO_UPPER");
     expect(method).to.not.equals(undefined);
-    expect(method!.getParameters().getDefaultImporting()).to.equal("val");
+    expect(method!.getParameters().getDefaultImporting()).to.equal("VAL");
   });
 });
