e48dd366d186a1f5218a3d3db8e4c88013ac1656
Lars Hvam <larshp@hotmail.com>
2022-06-01 17:53:52 +0200

    more EM

--- packages/core/src/abap/2_statements/statements/modify_entities.ts
+++ packages/core/src/abap/2_statements/statements/modify_entities.ts
@@ -1,5 +1,5 @@
 import {IStatement} from "./_statement";
-import {alt, plus, seq, ver} from "../combi";
+import {alt, opt, optPrio, per, plus, seq, ver} from "../combi";
 import {SimpleName, Source, Target} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
 import {Version} from "../../../version";
@@ -7,15 +7,22 @@ import {Version} from "../../../version";
 export class ModifyEntities implements IStatement {
 
   public getMatcher(): IStatementRunnable {
+    const fieldsWith = seq("FIELDS (", plus(SimpleName), ") WITH", Source);
     const operation = alt(
       seq("UPDATE SET FIELDS WITH", Source),
-      seq("CREATE FIELDS (", plus(SimpleName), ") WITH", Source));
+      seq("UPDATE", fieldsWith),
+      seq("DELETE FROM", Source),
+      seq("EXECUTE", SimpleName, "FROM", Source),
+      seq("CREATE", optPrio("AUTO FILL CID"), fieldsWith));
 
     const s = seq("MODIFY ENTITIES OF", SimpleName,
+                  opt("IN LOCAL MODE"),
                   "ENTITY", SimpleName,
                   operation,
-                  "FAILED", Target,
-                  "REPORTED", Target);
+                  optPrio(seq("MAPPED", Target)),
+                  per(seq("FAILED", Target),
+                      seq("RESULT", Target),
+                      seq("REPORTED", Target)));
     return ver(Version.v754, s);
   }
 
