f15627bb5ea36e589b31dcd5887ab7ed3023ea63
Lars Hvam <larshp@hotmail.com>
2022-07-25 13:31:23 +0200

    refactor, sqlIn

--- packages/core/src/abap/2_statements/expressions/sql_compare.ts
+++ packages/core/src/abap/2_statements/expressions/sql_compare.ts
@@ -1,20 +1,11 @@
-import {ver, seq, tok, alt, starPrio, optPrio, altPrio, Expression, plusPrio} from "../combi";
-import {SQLSource, SQLFieldName, Dynamic, Select, SQLCompareOperator, SQLFunction} from ".";
-import {ParenRight, ParenRightW, WParenLeft, WParenLeftW, WParenRight, WParenRightW} from "../../1_lexer/tokens";
+import {ver, seq, optPrio, altPrio, Expression, plusPrio} from "../combi";
+import {SQLSource, SQLFieldName, Dynamic, Select, SQLIn, SQLCompareOperator, SQLFunction} from ".";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class SQLCompare extends Expression {
   public getRunnable(): IStatementRunnable {
-    const val = new SQLSource();
-
-    const listOld = seq(tok(WParenLeft), val, starPrio(seq(",", val)), altPrio(tok(ParenRight), tok(ParenRightW), tok(WParenRightW)));
-    const listNew = seq(tok(WParenLeftW), val, starPrio(seq(",", val)), altPrio(tok(WParenRight), tok(WParenRightW)));
-    const list = alt(listOld, ver(Version.v740sp02, listNew)); // version is a guess, https://github.com/abaplint/abaplint/issues/2530
-
     const subSelect = seq("(", Select, ")");
-
-    const inn = seq("IN", altPrio(SQLSource, list, subSelect));
 
     const between = seq("BETWEEN", SQLSource, "AND", SQLSource);
 
@@ -31,7 +22,7 @@ export class SQLCompare extends Expression {
 
     const rett = seq(altPrio(SQLFunction, seq(SQLFieldName, optPrio(arith))),
                      altPrio(seq(SQLCompareOperator, altPrio(sub, source)),
-                             seq(optPrio("NOT"), altPrio(inn, like, between)),
+                             seq(optPrio("NOT"), altPrio(SQLIn, like, between)),
                              nul));
 
     const exists = seq("EXISTS", subSelect);
