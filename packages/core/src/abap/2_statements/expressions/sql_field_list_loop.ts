bfca45deff47b7d4288b028bbc3e0dee595049ff
Lars Hvam <larshp@hotmail.com>
2022-08-05 14:20:53 +0200

    refactor

--- packages/core/src/abap/2_statements/expressions/sql_field_list_loop.ts
+++ packages/core/src/abap/2_statements/expressions/sql_field_list_loop.ts
@@ -1,8 +1,7 @@
 import {seq, ver, Expression, optPrio, opt, alt, star} from "../combi";
-import {SQLFieldName, Dynamic, SQLAggregation, SQLCase, SQLAsName} from ".";
+import {SQLFieldName, Dynamic, SQLField, SQLAsName} from ".";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
-import {SQLFunction} from "./sql_function";
 import {SQLPath} from "./sql_path";
 
 // loop must include one field from the database table
@@ -10,7 +9,7 @@ export class SQLFieldListLoop extends Expression {
   public getRunnable(): IStatementRunnable {
     const comma = opt(ver(Version.v740sp05, ","));
     const as = seq("AS", SQLAsName);
-    const someField = seq(alt(SQLFieldName, SQLPath, SQLAggregation, SQLFunction, SQLCase), optPrio(as), comma);
+    const someField = seq(SQLField, comma);
     const fieldList = seq(star(someField), alt(SQLFieldName, SQLPath), optPrio(as), comma, star(someField));
 
     const fields = alt("*", Dynamic, fieldList);
