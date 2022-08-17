1fb1687e0ad75bf0207a04aa118ac7c9d0e8df7e
Lars Hvam <larshp@hotmail.com>
2022-08-17 15:49:08 +0200

    fix expression

--- packages/core/src/abap/2_statements/expressions/sql_client.ts
+++ packages/core/src/abap/2_statements/expressions/sql_client.ts
@@ -1,12 +1,12 @@
 import {Version} from "../../../version";
-import {alt, seq, Expression, ver} from "../combi";
+import {alt, seq, Expression, ver, verNot} from "../combi";
 import {IStatementRunnable} from "../statement_runnable";
 import {SQLSourceSimple} from "./sql_source_simple";
 
 export class SQLClient extends Expression {
   public getRunnable(): IStatementRunnable {
-// todo, client specified and connection not possible in Cloud
-    const client = alt("CLIENT SPECIFIED",
+
+    const client = alt(verNot(Version.Cloud, "CLIENT SPECIFIED"),
                        seq("USING", alt(ver(Version.v740sp05, seq("CLIENT", SQLSourceSimple)),
                                         ver(Version.v754, seq("CLIENTS IN", alt(SQLSourceSimple, "T000"))),
                                         ver(Version.v754, "ALL CLIENTS"))));
