2404fb6e4cb6cc5582ca6241c076cb1b42c7ea27
Lars Hvam <larshp@hotmail.com>
2022-08-24 19:40:40 +0200

    SELECT INTO NEW

--- packages/core/src/abap/2_statements/expressions/sql_target.ts
+++ packages/core/src/abap/2_statements/expressions/sql_target.ts
@@ -1,4 +1,4 @@
-import {altPrio, seq, ver, tok, Expression} from "../combi";
+import {altPrio, seq, ver, tok, opt, Expression} from "../combi";
 import {Version} from "../../../version";
 import {WAt, At} from "../../1_lexer/tokens";
 import {Target} from ".";
@@ -6,7 +6,8 @@ import {IStatementRunnable} from "../statement_runnable";
 
 export class SQLTarget extends Expression {
   public getRunnable(): IStatementRunnable {
-    const at = ver(Version.v740sp05, seq(altPrio(tok(WAt), tok(At)), Target));
+    const n = ver(Version.v754, "NEW");
+    const at = ver(Version.v740sp05, seq(opt(n), altPrio(tok(WAt), tok(At)), Target));
 
     return altPrio(at, Target);
   }
