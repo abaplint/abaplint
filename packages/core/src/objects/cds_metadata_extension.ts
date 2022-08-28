551ec9da96001ccece8582a128e9d7b748e779c5
Lars Hvam <larshp@hotmail.com>
2022-08-28 16:28:08 +0200

    parse DDLX

--- packages/core/src/objects/cds_metadata_extension.ts
+++ packages/core/src/objects/cds_metadata_extension.ts
@@ -1,6 +1,16 @@
+import {ExpressionNode} from "../abap/nodes";
+import {CDSParser} from "../cds/cds_parser";
 import {AbstractObject} from "./_abstract_object";
+import {IParseResult} from "./_iobject";
+
+export type ParsedMetadataExtension = {
+  tree: ExpressionNode | undefined;
+};
+
 
 export class CDSMetadataExtension extends AbstractObject {
+  private parserError: boolean | undefined = undefined;
+  private parsedData: ParsedMetadataExtension | undefined = undefined;
 
   public getType(): string {
     return "DDLX";
@@ -11,6 +21,30 @@ export class CDSMetadataExtension extends AbstractObject {
       maxLength: 40,
       allowNamespace: true,
     };
+  }
+
+  public hasParserError() {
+    return this.parserError;
+  }
+
+  public parse(): IParseResult {
+    if (this.isDirty() === false) {
+      return {updated: false, runtime: 0};
+    }
+
+    const start = Date.now();
+
+    this.parsedData = {
+      tree: undefined,
+    };
+
+    this.parsedData.tree = new CDSParser().parse(this.findSourceFile());
+    if (this.parsedData.tree === undefined) {
+      this.parserError = true;
+    }
+
+    this.dirty = false;
+    return {updated: true, runtime: Date.now() - start};
   }
 
   public getDescription(): string | undefined {
