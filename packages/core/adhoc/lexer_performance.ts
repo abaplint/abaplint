187a6d3ad3be19165c99727a915ba99208c0b640
Lars Hvam <larshp@hotmail.com>
2022-08-29 18:19:19 +0200

    minor, move files

---
+++ packages/core/adhoc/lexer_performance.ts
@@ -0,0 +1,17 @@
+import {MemoryFile} from "../src";
+import {Lexer} from "../src/abap/1_lexer/lexer";
+import * as fs from "fs";
+
+console.log("========================");
+const file = new MemoryFile("abapgit.abap", fs.readFileSync("./lexer_performance.abap", "utf-8"));
+
+let total = 0;
+for (let i = 0; i < 10; i++) {
+  const before = Date.now();
+  const result = Lexer.run(file);
+  const runtime = Date.now() - before;
+  console.log("Runtime: " + runtime + "ms");
+  total += runtime;
+  console.log("Tokens: " + result.tokens.length);
+}
+console.log("Total: " + total);
