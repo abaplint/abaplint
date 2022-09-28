4a81c845d8596d0c594281847d7e8f6ed298e957
Lars Hvam <larshp@hotmail.com>
2022-09-28 17:48:21 +0200

    bugfix, GET REFERENCE inline

--- packages/core/src/abap/5_syntax/statements/get_reference.ts
+++ packages/core/src/abap/5_syntax/statements/get_reference.ts
@@ -4,17 +4,20 @@ import {CurrentScope} from "../_current_scope";
 import {Source} from "../expressions/source";
 import {Target} from "../expressions/target";
 import {StatementSyntax} from "../_statement_syntax";
+import {InlineData} from "../expressions/inline_data";
+import {DataReference} from "../../types/basic";
 
 export class GetReference implements StatementSyntax {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
+    const s = node.findDirectExpression(Expressions.Source);
+    const type = new Source().runSyntax(s, scope, filename);
 
-    for (const s of node.findDirectExpressions(Expressions.Source)) {
-      new Source().runSyntax(s, scope, filename);
+    const target = node.findDirectExpression(Expressions.Target);
+    const inline = target?.findDirectExpression(Expressions.InlineData);
+    if (inline) {
+      new InlineData().runSyntax(inline, scope, filename, type ? new DataReference(type) : undefined);
+    } else if (target) {
+      new Target().runSyntax(target, scope, filename);
     }
-
-    for (const t of node.findDirectExpressions(Expressions.Target)) {
-      new Target().runSyntax(t, scope, filename);
-    }
-
   }
 }
