c37fa60580d7b2fe05700fd189a9d09089b3590a
Lars Hvam <larshp@hotmail.com>
2022-10-18 06:38:13 +0200

    closes #2730

--- packages/core/src/rules/unnecessary_pragma.ts
+++ packages/core/src/rules/unnecessary_pragma.ts
@@ -127,6 +127,7 @@ ENDIF.`,
         && !(statement.get() instanceof Statements.Data)
         && !(statement.get() instanceof Statements.DataEnd)
         && !(statement.get() instanceof Statements.Type)
+        && !(statement.get() instanceof Statements.Form)
         && !(statement.get() instanceof Statements.TypeEnd)
         && !(statement.get() instanceof Statements.Constant)
         && !(statement.get() instanceof Statements.ConstantEnd)
