6cf9a8516a02ef56165b99e962179a13c71ebe23
Lars Hvam <larshp@hotmail.com>
2022-10-24 07:50:32 +0200

    bugfix, unused var

--- packages/core/src/abap/5_syntax/expressions/reduce_next.ts
+++ packages/core/src/abap/5_syntax/expressions/reduce_next.ts
@@ -2,6 +2,7 @@ import {ExpressionNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
 import * as Expressions from "../../2_statements/expressions";
 import {Source} from "./source";
+import {Target} from "./target";
 
 export class ReduceNext {
   public runSyntax(node: ExpressionNode | undefined, scope: CurrentScope, filename: string): void {
@@ -11,6 +12,10 @@ export class ReduceNext {
 
     for (const s of node.findDirectExpressions(Expressions.Source)) {
       new Source().runSyntax(s, scope, filename);
+    }
+
+    for (const s of node.findDirectExpressions(Expressions.SimpleTarget)) {
+      new Target().runSyntax(s, scope, filename);
     }
   }
 }
