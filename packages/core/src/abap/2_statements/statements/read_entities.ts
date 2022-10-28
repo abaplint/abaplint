9b2f5e4d8edc1b9f445087f23e3274693406ff35
Lars Hvam <larshp@hotmail.com>
2022-10-28 14:52:57 +0200

    bugfix

--- packages/core/src/abap/2_statements/statements/read_entities.ts
+++ packages/core/src/abap/2_statements/statements/read_entities.ts
@@ -1,5 +1,5 @@
 import {IStatement} from "./_statement";
-import {seq, ver, tok, plus, optPrio, opt} from "../combi";
+import {seq, ver, tok, plus, alt, optPrio, opt} from "../combi";
 import {AssociationName, NamespaceSimpleName, SimpleName, Source, Target} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
 import {Version} from "../../../version";
@@ -8,12 +8,15 @@ import {WParenLeftW, WParenRightW} from "../../1_lexer/tokens";
 export class ReadEntities implements IStatement {
 
   public getMatcher(): IStatementRunnable {
+    const from = seq("FROM", Source);
+
+    const fields = seq("FIELDS", tok(WParenLeftW), plus(SimpleName), tok(WParenRightW), "WITH", Source);
+
     const s = seq("READ ENTITIES OF", NamespaceSimpleName,
                   "IN LOCAL MODE",
                   "ENTITY", SimpleName,
                   opt(seq("BY", AssociationName)),
-                  "FIELDS", tok(WParenLeftW), plus(SimpleName), tok(WParenRightW),
-                  "WITH", Source,
+                  alt(fields, from),
                   "RESULT", Target,
                   optPrio(seq("LINK", Target)),
                   optPrio(seq("FAILED", Target)),
