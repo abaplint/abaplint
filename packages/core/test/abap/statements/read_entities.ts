d82246d136e98d898761b70c14b89b05f290224a
Lars Hvam <larshp@hotmail.com>
2022-10-28 15:39:03 +0200

    bugfixes

--- packages/core/test/abap/statements/read_entities.ts
+++ packages/core/test/abap/statements/read_entities.ts
@@ -45,6 +45,13 @@ const tests = [
     WITH CORRESPONDING #( tab )
     RESULT DATA(lt_dat)
     FAILED DATA(lt_failed).`,
+
+  `READ ENTITIES OF /foo/bar
+    ENTITY ent
+    ALL FIELDS WITH VALUE #( ( sdf = '123' ) )
+    RESULT DATA(lt_tab)
+    FAILED DATA(lt_failed)
+    REPORTED DATA(lt_reported).`,
 ];
 
 statementType(tests, "READ ENTITIES", Statements.ReadEntities);
