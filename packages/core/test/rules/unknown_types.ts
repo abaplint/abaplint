50cac7907e1b4b4685d1a1046af571e61a92fb78
Lars Hvam <larshp@hotmail.com>
2022-11-04 15:39:03 +0100

    test, structured

--- packages/core/test/rules/unknown_types.ts
+++ packages/core/test/rules/unknown_types.ts
@@ -1939,4 +1939,24 @@ ENDCLASS.`;
     expect(issues.length).to.equal(0);
   });
 
+  it("type via reference, var, structured", () => {
+    const abap = `
+INTERFACE lif.
+  TYPES blah TYPE i.
+ENDINTERFACE.
+
+CLASS lcl DEFINITION.
+  PUBLIC SECTION.
+    DATA r_dt TYPE REF TO lif.
+    TYPES: BEGIN OF ty_foo,
+             field TYPE r_dt->blah,
+           END OF ty_foo.
+ENDCLASS.
+CLASS lcl IMPLEMENTATION.
+ENDCLASS.`;
+    let issues = runMulti([{filename: "zfoobar.prog.abap", contents: abap}], fullErrorNamespace());
+    issues = issues.filter(i => i.getKey() === key);
+    expect(issues.length).to.equal(0);
+  });
+
 });
