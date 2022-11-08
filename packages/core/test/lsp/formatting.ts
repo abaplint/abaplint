d869fdaf391f653ee210b558b819b49c622acd93
Lars Hvam <larshp@hotmail.com>
2022-11-08 09:58:09 +0100

    LSP, fix formatting

--- packages/core/test/lsp/formatting.ts
+++ packages/core/test/lsp/formatting.ts
@@ -17,4 +17,18 @@ describe("LSP, formatting", () => {
     expect(edits).to.not.equal(undefined);
   });
 
+  it("update and format", () => {
+    const name = "zfoobar.prog.abap";
+    const file = new MemoryFile(name, "DATA before TYPE i.");
+    const registry = new Registry().addFile(file).parse();
+    registry.updateFile(new MemoryFile(name, "data after TYPE i."));
+
+    const edits = new LanguageServer(registry).documentFormatting({
+      textDocument: {uri: name},
+    });
+
+    expect(edits).to.not.equal(undefined);
+    expect(edits[0].newText).to.include("after");
+  });
+
 });
