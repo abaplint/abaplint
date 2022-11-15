bae5c4f95d83b7376e56b88fe4ab436b0b43ebae
Lars Hvam <larshp@hotmail.com>
2022-11-15 20:41:50 +0100

    refactor

--- packages/core/src/cds/expressions/cds_element.ts
+++ packages/core/src/cds/expressions/cds_element.ts
@@ -1,5 +1,5 @@
 import {CDSAggregate, CDSAnnotation, CDSArithmetics, CDSCase, CDSFunction, CDSName, CDSParameters, CDSString} from ".";
-import {alt, Expression, opt, optPrio, regex, seq, star, starPrio} from "../../abap/2_statements/combi";
+import {altPrio, Expression, opt, optPrio, regex, seq, star, starPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSAs} from "./cds_as";
 import {CDSCast} from "./cds_cast";
@@ -8,14 +8,14 @@ export class CDSElement extends Expression {
   public getRunnable(): IStatementRunnable {
     return seq(starPrio(CDSAnnotation),
                optPrio("KEY"),
-               alt(CDSCast,
-                   CDSAggregate,
-                   CDSString,
-                   CDSFunction,
-                   CDSArithmetics,
-                   CDSCase,
-                   seq(CDSName, opt(CDSParameters), star(seq(".", CDSName, opt(CDSParameters)))),
-                   regex(/^\d+$/)),
+               altPrio(CDSCast,
+                       CDSAggregate,
+                       CDSString,
+                       CDSFunction,
+                       CDSArithmetics,
+                       CDSCase,
+                       seq(CDSName, opt(CDSParameters), star(seq(".", CDSName, opt(CDSParameters)))),
+                       regex(/^\d+$/)),
                opt(CDSAs));
   }
 }
