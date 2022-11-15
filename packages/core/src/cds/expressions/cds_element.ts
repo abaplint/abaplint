f668d57f5ddcfac539edd1b317866f2ca259726e
Lars Hvam <larshp@hotmail.com>
2022-11-15 21:15:48 +0100

    2.93.85: optimize CDS parsing (#2784)

--- packages/core/src/cds/expressions/cds_element.ts
+++ packages/core/src/cds/expressions/cds_element.ts
@@ -1,21 +1,21 @@
 import {CDSAggregate, CDSAnnotation, CDSArithmetics, CDSCase, CDSFunction, CDSName, CDSParameters, CDSString} from ".";
-import {alt, Expression, opt, optPrio, regex, seq, star} from "../../abap/2_statements/combi";
+import {altPrio, Expression, opt, optPrio, regex, seq, star, starPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSAs} from "./cds_as";
 import {CDSCast} from "./cds_cast";
 
 export class CDSElement extends Expression {
   public getRunnable(): IStatementRunnable {
-    return seq(star(CDSAnnotation),
+    return seq(starPrio(CDSAnnotation),
                optPrio("KEY"),
-               alt(seq(CDSName, opt(CDSParameters), star(seq(".", CDSName, opt(CDSParameters)))),
-                   CDSCast,
-                   CDSAggregate,
-                   CDSString,
-                   CDSFunction,
-                   regex(/^\d+$/),
-                   CDSArithmetics,
-                   CDSCase),
+               altPrio(CDSCast,
+                       CDSAggregate,
+                       CDSString,
+                       CDSFunction,
+                       CDSArithmetics,
+                       CDSCase,
+                       seq(CDSName, opt(CDSParameters), star(seq(".", CDSName, opt(CDSParameters)))),
+                       regex(/^\d+$/)),
                opt(CDSAs));
   }
 }
