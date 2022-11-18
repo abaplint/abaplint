3c0fd8525c11e1ad087fbf95aa9866c5d8c0c938
Lars Hvam <larshp@hotmail.com>
2022-11-18 08:00:26 +0100

    refactor

--- packages/core/src/cds/expressions/cds_condition.ts
+++ packages/core/src/cds/expressions/cds_condition.ts
@@ -1,5 +1,5 @@
 import {CDSFunction, CDSName, CDSString} from ".";
-import {alt, Expression, opt, seq, star} from "../../abap/2_statements/combi";
+import {alt, altPrio, Expression, opt, optPrio, seq, star} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSInteger} from "./cds_integer";
 
@@ -7,12 +7,9 @@ export class CDSCondition extends Expression {
   public getRunnable(): IStatementRunnable {
     const name = seq(CDSName, opt(seq(".", alt(CDSName, CDSString))));
     const left = alt(name, CDSFunction);
-    const eq = seq(left, alt("=", seq("!", "="), seq("<", ">"), "<", ">", seq(">", "="), seq("<", "="), "LIKE", "NOT LIKE"), alt(left, CDSInteger, CDSFunction, CDSString));
-    const isInitial = seq(left, "IS INITIAL");
-    const isNotInitial = seq(left, "IS NOT INITIAL");
-    const isNull = seq(left, "IS NULL");
-    const isNotNull = seq(left, "IS NOT NULL");
-    const condition = alt(eq, isNull, isNotNull, isInitial, isNotInitial);
+    const compare = seq(left, alt("=", seq("!", "="), seq("<", ">"), "<", ">", seq(">", "="), seq("<", "="), "LIKE", "NOT LIKE"), alt(left, CDSInteger, CDSFunction, CDSString));
+    const is = seq(left, "IS", optPrio("NOT"), altPrio("INITIAL", "NULL"));
+    const condition = alt(compare, is);
     const paren = seq("(", CDSCondition, ")");
     return seq(alt(condition, paren), star(seq(alt("AND", "OR"), alt(condition, paren))));
   }
