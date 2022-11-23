ed667918356c8b0b2def486417ddd86383a37fb8
Lars Hvam <larshp@hotmail.com>
2022-11-23 15:24:02 +0100

    foo

--- packages/morph/src/statements/return.ts
+++ packages/morph/src/statements/return.ts
@@ -1,10 +1,18 @@
-import {ReturnStatement} from "ts-morph";
-import {handleExpressions} from "../expressions";
+import {BinaryExpression, ReturnStatement} from "ts-morph";
+import {handleExpression, handleExpressions} from "../expressions";
 
 export class MorphReturn {
   public run(s: ReturnStatement) {
-    let ret = "return = ";
-    ret += handleExpressions(s.forEachChildAsArray());
-    return ret + ".\nRETURN.\n";
+    let ret = "";
+    const expr = s.getExpression();
+    if (expr) {
+      ret += "return = ";
+      if (expr instanceof BinaryExpression) {
+        ret += "xsdbool( " + handleExpression(expr) + " ).\n";
+      } else {
+        ret += handleExpression(expr) + ".\n";
+      }
+    }
+    return ret + "RETURN.\n";
   }
 }
