05d331d8d835904a7a1a968f8a2c2ca2c923d5e6
Lars Hvam <larshp@hotmail.com>
2022-11-28 08:29:43 +0100

    wip

---
+++ packages/core/test/rules/unnecessary_return.ts
@@ -0,0 +1,23 @@
+import {expect} from "chai";
+import {MemoryFile, Registry} from "../../src";
+import {UnnecessaryReturn} from "../../src/rules";
+
+async function findIssues(abap: string) {
+  const reg = new Registry().addFile(new MemoryFile("zfoo.prog.abap", abap));
+  await reg.parseAsync();
+  const rule = new UnnecessaryReturn();
+  return rule.initialize(reg).run(reg.getFirstObject()!);
+}
+
+describe("Rule: unnecessary_return", () => {
+
+  it.only("test FORM", async () => {
+    const abap = `
+FORM foo.
+  RETURN.
+ENDFORM.`;
+    const issues = await findIssues(abap);
+    expect(issues.length).to.equal(1);
+  });
+
+});
