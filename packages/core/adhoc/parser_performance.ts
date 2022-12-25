20019a4443d1b0f3343d36925b2d0594a526657a
Lars Hvam <larshp@hotmail.com>
2022-12-25 18:47:43 +0100

    wip

---
+++ packages/core/adhoc/parser_performance.ts
@@ -0,0 +1,19 @@
+import {MemoryFile, Version} from "../src";
+import {Lexer} from "../src/abap/1_lexer/lexer";
+import * as fs from "fs";
+import {StatementParser} from "../src/abap/2_statements/statement_parser";
+
+console.log("========================");
+const file = new MemoryFile("abapgit.abap", fs.readFileSync("./lexer_performance.abap", "utf-8"));
+
+const lexerResult = new Lexer().run(file);
+
+let total = 0;
+for (let i = 0; i < 4; i++) {
+  const before = Date.now();
+  new StatementParser(Version.v702).run([lexerResult], []);
+  const runtime = Date.now() - before;
+  console.log("Runtime: " + runtime + "ms");
+  total += runtime;
+}
+console.log("Total: " + total + "ms");
