762786174a274c5426e8d31327599d3bfa4b63ca
Lars Hvam <larshp@hotmail.com>
2023-01-11 17:44:49 +0100

    bugfix, preferred parameter optional, #2841

--- packages/core/test/abap/syntax/syntax.ts
+++ packages/core/test/abap/syntax/syntax.ts
@@ -7057,6 +7057,29 @@ lv_source = lv_source && lv_res.`;
     expect(issues[0]?.getMessage()).to.equal(undefined);
   });
 
+  it("PREFERRED PARAMETER is optional", () => {
+    const abap = `
+CLASS lcl_tele_mapping DEFINITION.
+  PUBLIC SECTION.
+    CLASS-METHODS replaceit
+      IMPORTING
+        iv_new    TYPE i OPTIONAL
+        iv_string TYPE string
+          PREFERRED PARAMETER iv_string.
+ENDCLASS.
+
+CLASS lcl_tele_mapping IMPLEMENTATION.
+  METHOD replaceit.
+  ENDMETHOD.
+ENDCLASS.
+
+START-OF-SELECTION.
+  lcl_tele_mapping=>replaceit( iv_new = 2 ).
+  lcl_tele_mapping=>replaceit( 'A' ).`;
+    const issues = runProgram(abap);
+    expect(issues[0]?.getMessage()).to.equal(undefined);
+  });
+
 // todo, static method cannot access instance attributes
 // todo, can a private method access protected attributes?
 // todo, readonly fields(constants + enums + attributes flagged read-only)
