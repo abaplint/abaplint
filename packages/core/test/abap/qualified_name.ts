90e5c6d76402c5b8b1f3ab6bcdc31e79b56ccfc9
Lars Hvam <larshp@hotmail.com>
2023-01-20 08:28:04 +0100

    wip

---
+++ packages/core/test/abap/qualified_name.ts
@@ -0,0 +1,83 @@
+import {expect} from "chai";
+import {IFile} from "../../src/files/_ifile";
+import {MemoryFile} from "../../src/files/memory_file";
+import {Registry} from "../../src";
+
+describe("qualified name", () => {
+  it.only("strange", async () => {
+    const files: IFile[] = [];
+
+    const amoo_dtel = `<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <DD04V>
+    <ROLLNAME>AMOO</ROLLNAME>
+    <DDLANGUAGE>E</DDLANGUAGE>
+    <DOMNAME>FLAG</DOMNAME>
+    <HEADLEN>10</HEADLEN>
+    <SCRLEN1>10</SCRLEN1>
+    <SCRLEN2>10</SCRLEN2>
+    <SCRLEN3>20</SCRLEN3>
+    <DDTEXT>MOO</DDTEXT>
+    <REPTEXT>MOO</REPTEXT>
+    <SCRTEXT_S>MOO</SCRTEXT_S>
+    <SCRTEXT_M>MOO</SCRTEXT_M>
+    <SCRTEXT_L>MOO</SCRTEXT_L>
+    <DTELMASTER>D</DTELMASTER>
+    <REFKIND>D</REFKIND>
+   </DD04V>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+    files.push(new MemoryFile("amoo.dtel.xml", amoo_dtel));
+
+    const flag_doma = `<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_DOMA" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <DD01V>
+    <DOMNAME>FLAG</DOMNAME>
+    <DDLANGUAGE>E</DDLANGUAGE>
+    <DATATYPE>CHAR</DATATYPE>
+    <LENG>000001</LENG>
+    <OUTPUTLEN>000001</OUTPUTLEN>
+    <DDTEXT>FOO</DDTEXT>
+   </DD01V>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+    files.push(new MemoryFile("flag.doma.xml", flag_doma));
+
+    const char1_dtel = `<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <DD04V>
+    <ROLLNAME>CHAR1</ROLLNAME>
+    <DDLANGUAGE>E</DDLANGUAGE>
+    <DOMNAME>FLAG</DOMNAME>
+    <DDTEXT>CHAR1</DDTEXT>
+    <DTELMASTER>D</DTELMASTER>
+    <REFKIND>D</REFKIND>
+   </DD04V>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+    files.push(new MemoryFile("char1.dtel.xml", char1_dtel));
+
+    const clas = `CLASS zcl_foobar DEFINITION PUBLIC FINAL CREATE PUBLIC.
+  PUBLIC SECTION.
+    DATA mv_compress TYPE char1.
+ENDCLASS.
+
+CLASS zcl_foobar IMPLEMENTATION.
+
+ENDCLASS.`;
+    files.push(new MemoryFile("zcl_foobar.clas.abap", clas));
+
+    const reg = new Registry().addFiles(files).parse();
+    expect(reg.getObjectCount()).to.equal(4);
+  });
+
+});
