393d4ad4b752304790f1a6d85fe4ee713a73a890
Lars Hvam <larshp@hotmail.com>
2023-02-07 18:25:58 +0100

    2.95.14: bugfix, WRITE AT (#2869)

--- packages/core/src/abap/2_statements/expressions/write_offset_length.ts
+++ packages/core/src/abap/2_statements/expressions/write_offset_length.ts
@@ -1,17 +1,17 @@
 import {regex as reg, Expression, opt, tok, alt, seq} from "../combi";
 import {IStatementRunnable} from "../statement_runnable";
 import {ParenLeft, ParenRightW, WParenLeft, ParenRight} from "../../1_lexer/tokens";
-import {FieldChain} from "./field_chain";
+import {SimpleFieldChain2} from "./simple_field_chain2";
 
 export class WriteOffsetLength extends Expression {
   public getRunnable(): IStatementRunnable {
 
-    const post = seq(alt(FieldChain, reg(/^[\d]+$/), reg(/^\*$/)), alt(tok(ParenRightW), tok(ParenRight)));
+    const post = seq(alt(SimpleFieldChain2, reg(/^[\d]+$/), reg(/^\*$/)), alt(tok(ParenRightW), tok(ParenRight)));
     const wlength = seq(tok(WParenLeft), post);
     const length = seq(tok(ParenLeft), post);
 
     const complex = alt(wlength,
-                        seq(alt(FieldChain, reg(/^\/?[\w\d]+$/), "/"), opt(length)));
+                        seq(alt(SimpleFieldChain2, reg(/^\/?[\w\d]+$/), "/"), opt(length)));
 
     const at = seq(opt("AT"), complex);
 
--- packages/core/src/abap/5_syntax/statements/write.ts
+++ packages/core/src/abap/5_syntax/statements/write.ts
@@ -6,6 +6,8 @@ import {Target} from "../expressions/target";
 import {StatementSyntax} from "../_statement_syntax";
 import {Dynamic} from "../expressions/dynamic";
 import {TypeUtils} from "../_type_utils";
+import {FieldChain} from "../expressions/field_chain";
+import {ReferenceType} from "../_reference";
 
 export class Write implements StatementSyntax {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
@@ -20,6 +22,10 @@ export class Write implements StatementSyntax {
           && new TypeUtils(scope).isHexLike(type) === false) {
         throw new Error("Source not character like");
       }
+    }
+
+    for (const s of node.findAllExpressions(Expressions.SimpleFieldChain2)) {
+      new FieldChain().runSyntax(s, scope, filename, ReferenceType.DataReadReference);
     }
 
     for (const s of node.findAllExpressions(Expressions.Dynamic)) {
