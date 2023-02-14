d8e8d63eaef2071c0ac0321266624abc5a5ae295
Lars Hvam <larshp@hotmail.com>
2023-02-14 18:17:16 +0100

    2.95.16: TTYP parsing (#2872)

--- packages/core/src/objects/table_type.ts
+++ packages/core/src/objects/table_type.ts
@@ -4,15 +4,19 @@ import * as Types from "../abap/types/basic";
 import {IRegistry} from "../_iregistry";
 import {DDIC} from "../ddic";
 import {IObjectAndToken} from "../_iddic_references";
-import {AnyType, DataReference, GenericObjectReferenceType} from "../abap/types/basic";
+import {AnyType, DataReference, GenericObjectReferenceType, ITableOptions, TableAccessType} from "../abap/types/basic";
+import {xmlToArray} from "../xml_utils";
 
 export class TableType extends AbstractObject {
   private parsedXML: {
     rowtype?: string,
-    rowkind?: string
-    datatype?: string
-    leng?: string
-    decimals?: string} | undefined = undefined;
+    rowkind?: string,
+    datatype?: string,
+    leng?: string,
+    decimals?: string,
+    dd42v: {keyname: string, keyfield: string}[];
+    dd43v: {keyname: string, accessmode: string, kind: string, unique: boolean}[];
+  } | undefined = undefined;
 
   public getType(): string {
     return "TTYP";
@@ -35,6 +39,41 @@ export class TableType extends AbstractObject {
     super.setDirty();
   }
 
+  private buildTableOptions(): ITableOptions {
+    const tableOptions: ITableOptions = {
+      withHeader: false,
+      secondary: [],
+    };
+
+    for (const k of this.parsedXML?.dd43v || []) {
+      const fields: string[] = [];
+      for (const f of this.parsedXML?.dd42v || []) {
+        if (f.keyname === k.keyname) {
+          fields.push(f.keyfield);
+        }
+      }
+      let accessType: TableAccessType = TableAccessType.standard;
+      switch (k.accessmode) {
+        case "S":
+          accessType = TableAccessType.sorted;
+          break;
+        case "H":
+          accessType = TableAccessType.hashed;
+          break;
+        default:
+          break;
+      }
+      tableOptions.secondary?.push({
+        name: k.keyname,
+        type: accessType,
+        keyFields: fields,
+        isUnique: k.unique,
+      });
+    }
+
+    return tableOptions;
+  }
+
   public parseType(reg: IRegistry): AbstractType {
     this.parseXML();
 
@@ -42,33 +81,35 @@ export class TableType extends AbstractObject {
 
     const references: IObjectAndToken[] = [];
     let type: AbstractType;
+    const tableOptions = this.buildTableOptions();
+
     if (this.parsedXML === undefined) {
       type = new Types.UnknownType("Table Type, parser error", this.getName());
     } else if (this.parsedXML.rowkind === "S") {
       const lookup = ddic.lookupTableOrView(this.parsedXML.rowtype);
-      type = new Types.TableType(lookup.type, {withHeader: false}, this.getName());
+      type = new Types.TableType(lookup.type, tableOptions, this.getName());
       if (lookup.object) {
         references.push({object: lookup.object});
       }
     } else if (this.parsedXML.rowkind === "E") {
       const lookup = ddic.lookupDataElement(this.parsedXML.rowtype);
-      type = new Types.TableType(lookup.type, {withHeader: false}, this.getName());
+      type = new Types.TableType(lookup.type, tableOptions, this.getName());
       if (lookup.object) {
         references.push({object: lookup.object});
       }
     } else if (this.parsedXML.rowkind === "L") {
       const lookup = ddic.lookupTableType(this.parsedXML.rowtype);
-      type = new Types.TableType(lookup.type, {withHeader: false}, this.getName());
+      type = new Types.TableType(lookup.type, tableOptions, this.getName());
       if (lookup.object) {
         references.push({object: lookup.object});
       }
     } else if (this.parsedXML.rowkind === "R" && this.parsedXML.rowtype === "OBJECT") {
-      type = new Types.TableType(new GenericObjectReferenceType(), {withHeader: false}, this.getName());
+      type = new Types.TableType(new GenericObjectReferenceType(), tableOptions, this.getName());
     } else if (this.parsedXML.rowkind === "R" && this.parsedXML.rowtype === "DATA") {
-      type = new Types.TableType(new DataReference(new AnyType()), {withHeader: false}, this.getName());
+      type = new Types.TableType(new DataReference(new AnyType()), tableOptions, this.getName());
     } else if (this.parsedXML.rowkind === "R" && this.parsedXML.rowtype !== undefined) {
       const lookup = ddic.lookupObject(this.parsedXML.rowtype);
-      type = new Types.TableType(lookup.type, {withHeader: false}, this.getName());
+      type = new Types.TableType(lookup.type, tableOptions, this.getName());
       if (lookup.object) {
         references.push({object: lookup.object});
       }
@@ -77,7 +118,7 @@ export class TableType extends AbstractObject {
         type = new Types.UnknownType("Table Type, empty DATATYPE" + this.getName(), this.getName());
       } else {
         const row = ddic.textToType(this.parsedXML.datatype, this.parsedXML.leng, this.parsedXML.decimals, this.getName());
-        type = new Types.TableType(row, {withHeader: false}, this.getName());
+        type = new Types.TableType(row, tableOptions, this.getName());
       }
     } else {
       type = new Types.UnknownType("Table Type, unknown kind \"" + this.parsedXML.rowkind + "\"" + this.getName(), this.getName());
@@ -94,19 +135,39 @@ export class TableType extends AbstractObject {
       return;
     }
 
-    this.parsedXML = {};
+    this.parsedXML = {
+      dd42v: [],
+      dd43v: [],
+    };
 
     const parsed = super.parseRaw2();
     if (parsed === undefined || parsed.abapGit === undefined) {
       return;
     }
 
-    const dd40v = parsed.abapGit["asx:abap"]["asx:values"].DD40V;
+    const values = parsed.abapGit["asx:abap"]["asx:values"];
+
+    const dd40v = values.DD40V;
     this.parsedXML.rowtype = dd40v.ROWTYPE ? dd40v.ROWTYPE : "";
     this.parsedXML.rowkind = dd40v.ROWKIND ? dd40v.ROWKIND : "";
     this.parsedXML.datatype = dd40v.DATATYPE;
     this.parsedXML.leng = dd40v.LENG;
     this.parsedXML.decimals = dd40v.DECIMALS;
+
+    for (const x of xmlToArray(values.DD42V?.DD42V)) {
+      this.parsedXML.dd42v.push({
+        keyname: x.SECKEYNAME || "",
+        keyfield: x.KEYFIELD || "",
+      });
+    }
+    for (const x of xmlToArray(values.DD43V?.DD43V)) {
+      this.parsedXML.dd43v.push({
+        keyname: x.SECKEYNAME || "",
+        accessmode: x.ACCESSMODE || "",
+        kind: x.KIND || "",
+        unique: x.SECKEYUNIQUE === "X",
+      });
+    }
   }
 
 }
