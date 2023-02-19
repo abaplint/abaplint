18e3ff98b6bdca6f2d44c754b35b877096b2b7f0
Lars Hvam <larshp@hotmail.com>
2023-02-19 14:12:08 +0100

    parse SMIM

--- packages/core/src/objects/mime_object.ts
+++ packages/core/src/objects/mime_object.ts
@@ -1,9 +1,24 @@
+import {IFile} from "../files/_ifile";
 import {AbstractObject} from "./_abstract_object";
 
 export class MIMEObject extends AbstractObject {
+  private parsedXML: {
+    URL?: string,
+    CLASS?: string,
+  } | undefined;
 
   public getType(): string {
     return "SMIM";
+  }
+
+  public getURL(): string | undefined {
+    this.parse();
+    return this.parsedXML?.URL;
+  }
+
+  public getClass(): string | undefined {
+    this.parse();
+    return this.parsedXML?.CLASS;
   }
 
   public getAllowedNaming() {
@@ -13,8 +28,45 @@ export class MIMEObject extends AbstractObject {
     };
   }
 
-  public getDescription(): string | undefined {
-    // todo
+  public getDataFile(): IFile | undefined {
+    const main = this.getXMLFile();
+    for (const f of this.getFiles()) {
+      if (f.getFilename() !== main?.getFilename()) {
+        return f;
+      }
+    }
     return undefined;
+  }
+
+  public setDirty(): void {
+    this.parsedXML = undefined;
+    super.setDirty();
+  }
+
+  public getDescription(): string | undefined {
+// this object type does not have a description
+    return undefined;
+  }
+
+  public parse() {
+    if (this.parsedXML) {
+      return {updated: false, runtime: 0};
+    }
+
+    const start = Date.now();
+    this.parsedXML = {};
+    const parsed = super.parseRaw2();
+
+    if (parsed === undefined
+        || parsed.abapGit === undefined
+        || parsed.abapGit["asx:abap"]["asx:values"] === undefined) {
+      return {updated: false, runtime: 0};
+    }
+
+    this.parsedXML.URL = parsed.abapGit["asx:abap"]["asx:values"].URL;
+    this.parsedXML.CLASS = parsed.abapGit["asx:abap"]["asx:values"].CLASS;
+
+    const end = Date.now();
+    return {updated: true, runtime: end - start};
   }
 }
