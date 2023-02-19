2e5079ba119c0d97fb1d7416247dd1a96167850e
Lars Hvam <larshp@hotmail.com>
2023-02-19 14:01:42 +0100

    parse W3MI description

--- packages/core/src/objects/web_mime.ts
+++ packages/core/src/objects/web_mime.ts
@@ -1,7 +1,11 @@
+import {IFile} from "../files/_ifile";
 import {AbstractObject} from "./_abstract_object";
 import {IAllowedNaming} from "./_iobject";
 
 export class WebMIME extends AbstractObject {
+  private parsedXML: {
+    description?: string,
+  } | undefined;
 
   public getType(): string {
     return "W3MI";
@@ -16,7 +20,42 @@ export class WebMIME extends AbstractObject {
   }
 
   public getDescription(): string | undefined {
-    // todo
+    this.parse();
+    return this.parsedXML?.description;
+  }
+
+  public setDirty(): void {
+    this.parsedXML = undefined;
+    super.setDirty();
+  }
+
+  public getDataFile(): IFile | undefined {
+    for (const f of this.getFiles()) {
+      if (f.getFilename().includes(".data.")) {
+        return f;
+      }
+    }
     return undefined;
+  }
+
+  public parse() {
+    if (this.parsedXML) {
+      return {updated: false, runtime: 0};
+    }
+
+    const start = Date.now();
+    this.parsedXML = {};
+    const parsed = super.parseRaw2();
+
+    if (parsed === undefined
+        || parsed.abapGit === undefined
+        || parsed.abapGit["asx:abap"]["asx:values"] === undefined) {
+      return {updated: false, runtime: 0};
+    }
+
+    this.parsedXML.description = parsed.abapGit["asx:abap"]["asx:values"].TEXT;
+
+    const end = Date.now();
+    return {updated: true, runtime: end - start};
   }
 }
