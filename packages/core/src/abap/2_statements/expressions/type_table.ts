bc68e083bfb059db959877168fac1b94b7c8497f
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2023-03-08 09:22:23 +0100

    Format

--- packages/core/src/abap/2_statements/expressions/type_table.ts
+++ packages/core/src/abap/2_statements/expressions/type_table.ts
@@ -28,8 +28,7 @@ export class TypeTable extends Expression {
 
     // a maximum of 15 secondary table keys can be defined
     // "WITH" is not allowed as a field name in keys
-    const typetable = alt(generic,
-                      seq(normal1,
+    const typetable = alt(generic,seq(normal1,
                           alt(opt(per(header, initial, plusPrio(TypeTableKey))),
                               seq(plus(TypeTableKey), optPrio(initial)))));
 
