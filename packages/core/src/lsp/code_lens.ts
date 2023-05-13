bb8b1ed02b1751eb9ed9efa9ec4b63a56562099b
Lars Hvam <larshp@hotmail.com>
2023-05-13 12:50:49 +0200

    fix

--- packages/core/src/lsp/code_lens.ts
+++ packages/core/src/lsp/code_lens.ts
@@ -1,6 +1,9 @@
 import * as LServer from "vscode-languageserver-types";
 import {IRegistry} from "../_iregistry";
 import {LSPUtils} from "./_lsp_utils";
+import {SyntaxLogic} from "../abap/5_syntax/syntax";
+import {ABAPObject} from "../objects/_abap_object";
+import {MessageClass} from "../objects";
 
 export class CodeLens {
   private readonly reg: IRegistry;
@@ -14,8 +17,25 @@ export class CodeLens {
     if (file === undefined) {
       return [];
     }
-// todo
-    return [];
+
+    const obj = this.reg.findObjectForFile(file);
+    if (obj === undefined || !(obj instanceof ABAPObject)) {
+      return [];
+    }
+    new SyntaxLogic(this.reg, obj).run();
+
+    const ret: LServer.CodeLens[] = [];
+    const list = this.reg.getMSAGReferences().listByFilename(file.getFilename());
+    for (const l of list) {
+      const msag = this.reg.getObject("MSAG", l.messageClass) as MessageClass | undefined;
+      if (msag === undefined) {
+        continue;
+      }
+      const text = msag.getByNumber(l.number)?.getMessage();
+      ret.push(LServer.CodeLens.create(LSPUtils.tokenToRange(l.token), text));
+    }
+
+    return ret;
   }
 
 }
---
+++ packages/core/test/lsp/code_lens.ts
@@ -0,0 +1,47 @@
+import {expect} from "chai";
+import {Registry} from "../../src/registry";
+import {MemoryFile} from "../../src/files/memory_file";
+import {CodeLens} from "../../src/lsp/code_lens";
+
+const filename: string = "codelens.prog.abap";
+
+const xml =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+"<abapGit version=\"v1.0.0\" serializer=\"LCL_OBJECT_MSAG\" serializer_version=\"v1.0.0\">\n" +
+" <asx:abap xmlns:asx=\"http://www.sap.com/abapxml\" version=\"1.0\">\n" +
+"  <asx:values>\n" +
+"   <T100A>\n" +
+"    <ARBGB>ZAG_UNIT_TEST</ARBGB>\n" +
+"    <MASTERLANG>E</MASTERLANG>\n" +
+"    <STEXT>test</STEXT>\n" +
+"   </T100A>\n" +
+"   <T100>\n" +
+"    <T100>\n" +
+"     <SPRSL>E</SPRSL>\n" +
+"     <ARBGB>ZAG_UNIT_TEST</ARBGB>\n" +
+"     <MSGNR>000</MSGNR>\n" +
+"     <TEXT>hello world &amp;</TEXT>\n" +
+"    </T100>\n" +
+"   </T100>\n" +
+"  </asx:values>\n" +
+" </asx:abap>\n" +
+"</abapGit>";
+
+describe("LSP, Code Lens", () => {
+
+  it("parser error", () => {
+    const file = new MemoryFile(filename, "sdfsdfds");
+    const reg = new Registry().addFile(file).parse();
+    const found = new CodeLens(reg).list({uri: filename});
+    expect(found.length).to.equal(0);
+  });
+
+  it("find it", () => {
+    const file = new MemoryFile(filename, "MESSAGE e000(zag_unit_test).");
+    const msag = new MemoryFile("zag_unit_test.msag.xml", xml);
+    const reg = new Registry().addFiles([file, msag]).parse();
+    const found = new CodeLens(reg).list({uri: filename});
+    expect(found.length).to.equal(1);
+  });
+
+});
