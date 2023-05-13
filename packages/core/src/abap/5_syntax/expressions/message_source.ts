d6fc7ca0757a1c983e8d4e462eeda16fe57e597c
Lars Hvam <larshp@hotmail.com>
2023-05-13 10:47:48 +0200

    update

--- packages/core/src/abap/5_syntax/expressions/message_source.ts
+++ packages/core/src/abap/5_syntax/expressions/message_source.ts
@@ -8,5 +8,22 @@ export class MessageSource {
     for (const f of node.findDirectExpressions(Expressions.Source)) {
       new Source().runSyntax(f, scope, filename);
     }
+
+    if (node.getFirstToken().getStr().toUpperCase() === "ID") {
+      const id = node.findExpressionAfterToken("ID")?.concatTokens();
+      const number = node.findExpressionAfterToken("NUMBER")?.concatTokens();
+      if (id?.startsWith("'") && number?.startsWith("'")) {
+        const messageNumber = parseInt(number.substring(1, number.length - 2), 10);
+        const messageClass = id.substring(1, id.length - 2);
+        scope.getMSAGReferences().addUsing(filename, node.getFirstToken(), messageClass, messageNumber);
+      }
+    } else {
+      const typeAndNumber = node.findDirectExpression(Expressions.MessageTypeAndNumber)?.concatTokens();
+      const messageNumber = parseInt(typeAndNumber?.substring(1) || "", 10);
+      const messageClass = node.findDirectExpression(Expressions.MessageClass)?.concatTokens().toUpperCase();
+      if (messageNumber && messageClass) {
+        scope.getMSAGReferences().addUsing(filename, node.getFirstToken(), messageClass, messageNumber);
+      }
+    }
   }
 }
