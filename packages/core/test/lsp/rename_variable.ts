e5a6520d5228a8347b932aeb9da2e75f454e6b50
Lars Hvam <larshp@hotmail.com>
2023-05-31 16:52:54 +0200

    rename method

--- packages/core/test/lsp/rename_variable.ts
+++ packages/core/test/lsp/rename_variable.ts
@@ -91,4 +91,38 @@ ENDCLASS.`;
     expect(reg.getFirstObject()?.getFiles()[0]?.getRaw()).to.equal(expected);
   });
 
+  it("rename method", async () => {
+    const abap = `CLASS lcl DEFINITION.
+  PUBLIC SECTION.
+    CLASS-METHODS foo.
+ENDCLASS.
+
+CLASS lcl IMPLEMENTATION.
+  METHOD foo.
+  ENDMETHOD.
+ENDCLASS.`;
+    const file = new MemoryFile("foobar.prog.abap", abap);
+    const reg = new Registry().addFile(file);
+    await reg.parseAsync();
+
+    const result = new Rename(reg).rename({
+      textDocument: {uri: file.getFilename()},
+      position: LServer.Position.create(2, 20),
+      newName: "renamed"});
+    expect(result).to.not.equal(undefined);
+    new ApplyWorkSpaceEdit(reg).apply(result!);
+    await reg.parseAsync();
+
+    const expected = `CLASS lcl DEFINITION.
+  PUBLIC SECTION.
+    CLASS-METHODS renamed.
+ENDCLASS.
+
+CLASS lcl IMPLEMENTATION.
+  METHOD renamed.
+  ENDMETHOD.
+ENDCLASS.`;
+    expect(reg.getFirstObject()?.getFiles()[0]?.getRaw()).to.equal(expected);
+  });
+
 });
