c6ac934c8a098579d2c0063c8f34dd59fceb7c62
Lars Hvam <larshp@hotmail.com>
2023-06-05 14:40:59 +0200

    wip

--- packages/core/src/lsp/inlay_hints.ts
+++ packages/core/src/lsp/inlay_hints.ts
@@ -3,6 +3,10 @@ import {IRegistry} from "../_iregistry";
 import {LSPUtils} from "./_lsp_utils";
 import {SyntaxLogic} from "../abap/5_syntax/syntax";
 import {ABAPObject} from "../objects/_abap_object";
+import {ISpaghettiScopeNode} from "../abap/5_syntax/_spaghetti_scope";
+import {ReferenceType, IReference} from "../abap/5_syntax/_reference";
+import {TypedIdentifier} from "../abap/types/_typed_identifier";
+import {ClassDefinition} from "../abap/types";
 
 export type InlayHintsSettings = {
   inferredTypes: boolean,
@@ -25,20 +29,50 @@ export class InlayHints {
     if (obj === undefined || !(obj instanceof ABAPObject)) {
       return [];
     }
-    new SyntaxLogic(this.reg, obj).run();
+    const top = new SyntaxLogic(this.reg, obj).run().spaghetti.getTop();
 
     const ret: LServer.InlayHint[] = [];
 
     if (settings.inferredTypes === true) {
-// todo
-      ret.push({
-        label: "INLAY HINT",
-        tooltip: "Inferred type",
-        kind: LServer.InlayHintKind.Type,
-        paddingLeft: true,
-        paddingRight: true,
-        position: {line: 216, character: 20},
-      });
+      const implicit = this.findImplicitReferences(top);
+      for (const i of implicit) {
+
+        let label: string | undefined = undefined;
+        if (i.resolved instanceof TypedIdentifier) {
+          label = i.resolved.getType().toABAP();
+        } else if (i.resolved instanceof ClassDefinition) {
+          label = "TYPE REF TO " + i.resolved.getName();
+        }
+
+        if (label === undefined) {
+          continue;
+        }
+
+        ret.push({
+          label: label,
+          tooltip: "Inferred type",
+          kind: LServer.InlayHintKind.Type,
+          paddingLeft: true,
+          paddingRight: true,
+          position: LSPUtils.positionToLS(i.position.getEnd()),
+        });
+      }
+    }
+
+    return ret;
+  }
+
+  private findImplicitReferences(node: ISpaghettiScopeNode): IReference[] {
+    const ret: IReference[] = [];
+
+    for (const r of node.getData().references) {
+      if (r.referenceType === ReferenceType.InferredType) {
+        ret.push(r);
+      }
+    }
+
+    for (const c of node.getChildren()) {
+      ret.push(...this.findImplicitReferences(c));
     }
 
     return ret;
