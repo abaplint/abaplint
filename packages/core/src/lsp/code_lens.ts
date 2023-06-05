a54f5afe176da99aacf722dfeb7e42a119980409
Lars Hvam <larshp@hotmail.com>
2023-06-05 13:43:18 +0200

    wip

--- packages/core/src/lsp/code_lens.ts
+++ packages/core/src/lsp/code_lens.ts
@@ -5,6 +5,10 @@ import {SyntaxLogic} from "../abap/5_syntax/syntax";
 import {ABAPObject} from "../objects/_abap_object";
 import {MessageClass} from "../objects";
 
+export type CodeLensSettings = {
+  messageText: boolean,
+};
+
 export class CodeLens {
   private readonly reg: IRegistry;
 
@@ -12,7 +16,7 @@ export class CodeLens {
     this.reg = reg;
   }
 
-  public list(textDocument: LServer.TextDocumentIdentifier): LServer.CodeLens[] {
+  public list(textDocument: LServer.TextDocumentIdentifier, settings: CodeLensSettings = {messageText: true}): LServer.CodeLens[] {
     const file = LSPUtils.getABAPFile(this.reg, textDocument.uri);
     if (file === undefined) {
       return [];
@@ -25,14 +29,23 @@ export class CodeLens {
     new SyntaxLogic(this.reg, obj).run();
 
     const ret: LServer.CodeLens[] = [];
-    const list = this.reg.getMSAGReferences().listByFilename(file.getFilename());
-    for (const l of list) {
-      const msag = this.reg.getObject("MSAG", l.messageClass) as MessageClass | undefined;
-      if (msag === undefined) {
-        continue;
+
+    if (settings.messageText === true) {
+      const list = this.reg.getMSAGReferences().listByFilename(file.getFilename());
+      for (const l of list) {
+        const msag = this.reg.getObject("MSAG", l.messageClass) as MessageClass | undefined;
+        if (msag === undefined) {
+          continue;
+        }
+        const text = msag.getByNumber(l.number)?.getMessage();
+        if (text === undefined) {
+          continue;
+        }
+        ret.push({
+          range: LSPUtils.tokenToRange(l.token),
+          command: LServer.Command.create(text, ""),
+        });
       }
-      const text = msag.getByNumber(l.number)?.getMessage();
-      ret.push(LServer.CodeLens.create(LSPUtils.tokenToRange(l.token), text));
     }
 
     return ret;
