4f33e0214daed3bfcf942883a16c1c6a7739ed53
Lars Hvam <larshp@hotmail.com>
2023-07-27 10:01:09 +0200

    stricter FRIENDS

--- packages/core/src/abap/5_syntax/statements/class_local_friends.ts
+++ packages/core/src/abap/5_syntax/statements/class_local_friends.ts
@@ -7,15 +7,33 @@ import {StatementSyntax} from "../_statement_syntax";
 export class ClassLocalFriends implements StatementSyntax {
   public runSyntax(node: StatementNode, scope: CurrentScope, filename: string): void {
 
-    const found = node.findDirectExpression(Expressions.ClassName);
+    const classNames = node.findAllExpressions(Expressions.ClassName);
+
+    const found = classNames[0];
     if (found) {
       const token = found.getFirstToken();
       const name = token.getStr();
+
+      if (scope.getParentObj().getType() === "CLAS"
+          && name.toUpperCase() !== scope.getParentObj().getName().toUpperCase()) {
+        throw new Error(`Befriending must be ` + scope.getParentObj().getName().toUpperCase());
+      }
+
       const def = scope.findClassDefinition(name);
       if (def) {
         scope.addReference(token, def, ReferenceType.ObjectOrientedReference, filename);
       } else {
         throw new Error(`Class ${name.toUpperCase()} not found`);
+      }
+
+    }
+
+    for (let i = 1; i < classNames.length; i++) {
+      const className = classNames[i].concatTokens();
+      // make sure to check also DEFINITION DEFERRED
+      const found = scope.existsObject(className);
+      if (found.found === false) {
+        throw new Error(`Class ${className.toUpperCase()} not found`);
       }
     }
 
