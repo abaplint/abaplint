7cfbcb0b5ce30a9564fa442970bf1106b82a63db
Lars Hvam <larshp@hotmail.com>
2023-08-25 19:02:59 +0200

    bugfix MESH

--- packages/core/src/abap/2_statements/statements/type_mesh.ts
+++ packages/core/src/abap/2_statements/statements/type_mesh.ts
@@ -13,7 +13,7 @@ export class TypeMesh implements IStatement {
 
     const association = seq("ASSOCIATION", NamespaceSimpleName, "TO", NamespaceSimpleName, plus(on));
 
-    const ret = ver(Version.v751, seq("TYPES", NamespaceSimpleName, "TYPE", TypeName, plus(association), opt(using)));
+    const ret = ver(Version.v751, seq("TYPES", NamespaceSimpleName, "TYPE", opt("REF TO"), TypeName, plus(association), opt(using)));
 
     return ret;
   }
