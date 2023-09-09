72cff50f69687c1ac854e9e80465a4ce6dde25ef
Lars Hvam <larshp@hotmail.com>
2023-09-09 11:08:30 +0200

    bugfix, renaming removing namespace

--- packages/core/test/objects/rename/rename_data_element.ts
+++ packages/core/test/objects/rename/rename_data_element.ts
@@ -44,6 +44,45 @@ describe("Rename Data Element", () => {
     }
   });
 
+  it("DTEL, no references, just the object, namespaced", () => {
+    const xml = `<?xml version="1.0" encoding="utf-8"?>
+<abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
+ <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+  <asx:values>
+   <DD04V>
+    <ROLLNAME>/IWBEP/MED_ANNO_NAMESPACE</ROLLNAME>
+    <DDLANGUAGE>E</DDLANGUAGE>
+    <HEADLEN>55</HEADLEN>
+    <SCRLEN1>10</SCRLEN1>
+    <SCRLEN2>20</SCRLEN2>
+    <SCRLEN3>40</SCRLEN3>
+    <DDTEXT>testing</DDTEXT>
+    <REPTEXT>testing</REPTEXT>
+    <SCRTEXT_S>testing</SCRTEXT_S>
+    <SCRTEXT_M>testing</SCRTEXT_M>
+    <SCRTEXT_L>testing</SCRTEXT_L>
+    <DTELMASTER>E</DTELMASTER>
+    <DATATYPE>CHAR</DATATYPE>
+    <LENG>000001</LENG>
+    <OUTPUTLEN>000001</OUTPUTLEN>
+   </DD04V>
+  </asx:values>
+ </asx:abap>
+</abapGit>`;
+
+    const reg = new Registry().addFiles([
+      new MemoryFile("#iwbep#med_anno_namespace.dtel.xml", xml),
+    ]).parse();
+
+    new Renamer(reg).rename("DTEL", "/iwbep/med_anno_namespace", "zmed_anno_namespace");
+
+    expect(reg.getObjectCount()).to.equal(1);
+    for (const f of reg.getFiles()) {
+      expect(f.getFilename()).to.equal("zmed_anno_namespace.dtel.xml");
+      expect(f.getRaw().includes("<ROLLNAME>ZMED_ANNO_NAMESPACE</ROLLNAME>")).to.equal(true);
+    }
+  });
+
   it("DTEL, referenced from PROG", () => {
     const xml = `<?xml version="1.0" encoding="utf-8"?>
 <abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
