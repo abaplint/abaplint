6c3e2b40d684a84c26608ae75fb35f98e6cf55a6
Lars Hvam <larshp@hotmail.com>
2023-09-13 10:11:10 +0200

    add connecction

--- packages/core/src/abap/2_statements/statements/open_cursor.ts
+++ packages/core/src/abap/2_statements/statements/open_cursor.ts
@@ -1,6 +1,6 @@
 import {IStatement} from "./_statement";
 import {verNot, seq, optPrio} from "../combi";
-import {Select, SQLTarget, SQLHints} from "../expressions";
+import {Select, SQLTarget, SQLHints, DatabaseConnection} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 
@@ -12,6 +12,7 @@ export class OpenCursor implements IStatement {
                     SQLTarget,
                     "FOR",
                     Select,
+                    optPrio(DatabaseConnection),
                     optPrio(SQLHints));
 
     return verNot(Version.Cloud, ret);
