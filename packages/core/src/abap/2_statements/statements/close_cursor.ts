c270d2ea610d8d016704e4e13cca61caf9d8bb8f
Lars Hvam <larshp@hotmail.com>
2023-09-13 08:39:39 +0200

    CURSOR bugfixes

--- packages/core/src/abap/2_statements/statements/close_cursor.ts
+++ packages/core/src/abap/2_statements/statements/close_cursor.ts
@@ -1,13 +1,13 @@
 import {IStatement} from "./_statement";
 import {verNot, seq} from "../combi";
-import {SQLSource} from "../expressions";
+import {SQLSourceSimple} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class CloseCursor implements IStatement {
 
   public getMatcher(): IStatementRunnable {
-    const ret = seq("CLOSE CURSOR", SQLSource);
+    const ret = seq("CLOSE CURSOR", SQLSourceSimple);
     return verNot(Version.Cloud, ret);
   }
 
--- packages/core/src/abap/2_statements/statements/fetch_next_cursor.ts
+++ packages/core/src/abap/2_statements/statements/fetch_next_cursor.ts
@@ -1,6 +1,6 @@
 import {IStatement} from "./_statement";
-import {verNot, seq, alt, opt} from "../combi";
-import {SQLSource, SQLIntoTable} from "../expressions";
+import {verNot, seq, alt, optPrio} from "../combi";
+import {SQLSourceSimple, SQLIntoTable} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 import {SQLIntoStructure} from "../expressions/sql_into_structure";
@@ -8,12 +8,12 @@ import {SQLIntoStructure} from "../expressions/sql_into_structure";
 export class FetchNextCursor implements IStatement {
 
   public getMatcher(): IStatementRunnable {
-    const size = seq("PACKAGE SIZE", SQLSource);
+    const size = seq("PACKAGE SIZE", SQLSourceSimple);
 
     const ret = seq("FETCH NEXT CURSOR",
-                    SQLSource,
+                    SQLSourceSimple,
                     alt(SQLIntoStructure, SQLIntoTable),
-                    opt(size));
+                    optPrio(size));
 
     return verNot(Version.Cloud, ret);
   }
