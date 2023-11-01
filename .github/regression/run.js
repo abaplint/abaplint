75999cae17c6faacbf71ac36cd3c80f293c97db2
Lars Hvam <larshp@hotmail.com>
2023-11-01 10:54:58 +0100

    run all

--- .github/regression/run.js
+++ .github/regression/run.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const childProcess = require("child_process");
 
 const repos = [
-  /*
   "ABAP-Logger/ABAP-Logger",
   "abap-observability-tools/abap-metrics-provider",
   "abap-openapi/abap-openapi",
@@ -37,9 +36,7 @@ const repos = [
   "Marc-Bernard-Tools/MBT-Logical-Object-Lister",
   "Marc-Bernard-Tools/MBT-Transport-Request",
   "open-abap/open-abap-bal",
-  */
   "open-abap/open-abap-core",
-  /*
   "open-abap/open-table-maintenance",
   "SAP/abap-file-formats-tools",
   "SAP/abap-file-formats",
@@ -48,7 +45,6 @@ const repos = [
   "sbcgua/mockup_loader",
   "sbcgua/text2tab",
   "Sumu-Ning/AES",
-  */
 ];
 
 console.dir(repos);
