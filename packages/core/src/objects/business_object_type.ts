d05fa671f76658f12d8ad4b13fbba635fbaa536c
Lars Hvam <larshp@hotmail.com>
2023-11-08 10:53:30 +0100

    add SOBJ

---
+++ packages/core/src/objects/business_object_type.ts
@@ -0,0 +1,20 @@
+import {AbstractObject} from "./_abstract_object";
+
+export class BusinessObjectType extends AbstractObject {
+
+  public getType(): string {
+    return "SOBJ";
+  }
+
+  public getAllowedNaming() {
+    return { // todo, verify
+      maxLength: 200,
+      allowNamespace: true,
+    };
+  }
+
+  public getDescription(): string | undefined {
+    // todo
+    return undefined;
+  }
+}
