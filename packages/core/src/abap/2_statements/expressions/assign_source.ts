e307a5ed86d07c2142b1930ba909f8481a888bcf
Lars Hvam <larshp@hotmail.com>
2023-11-28 08:30:31 +0100

    bugfix, ASSIGN

--- packages/core/src/abap/2_statements/expressions/assign_source.ts
+++ packages/core/src/abap/2_statements/expressions/assign_source.ts
@@ -1,14 +1,16 @@
-import {seq, alt, tok, opt, Expression} from "../combi";
+import {seq, alt, tok, opt, Expression, ver} from "../combi";
 import {InstanceArrow, StaticArrow} from "../../1_lexer/tokens";
 import {IStatementRunnable} from "../statement_runnable";
 import {Source} from "./source";
 import {Dynamic} from "./dynamic";
 import {Field} from "./field";
+import {SimpleSource3} from "./simple_source3";
+import {Version} from "../../../version";
 
 export class AssignSource extends Expression {
   public getRunnable(): IStatementRunnable {
     const component = seq("COMPONENT",
-                          Source,
+                          alt(SimpleSource3, ver(Version.v740sp02, Source)),
                           "OF STRUCTURE",
                           Source);
 
