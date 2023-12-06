c6c991415cd332fd267fbffa661a7d187bcf8d97
Lars Hvam <larshp@hotmail.com>
2023-12-06 16:05:25 +0100

    2.104.3: bugfix, syntax (#3176)

--- packages/core/test/abap/statements/append.ts
+++ packages/core/test/abap/statements/append.ts
@@ -37,6 +37,7 @@ const tests = [
   "APPEND LINES OF tab TO index TO target.",
   "APPEND LINES OF tab FROM index TO target.",
   `APPEND VALUE #( %tky = travel-%tky ) TO reported-travel.`,
+  `APPEND VALUE #( hello = ls_item->%tky ) TO rt_result.`,
   `APPEND VALUE #( field_name = 'STATUS') TO gt_list.`,
 ];
 
