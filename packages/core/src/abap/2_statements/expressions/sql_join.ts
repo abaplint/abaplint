8b2f36f4ffe8b5a9dcca69f4ffb9044fe6bda6f9
Lars Hvam <larshp@hotmail.com>
2024-01-10 18:03:56 +0100

    add join type

--- packages/core/src/abap/2_statements/expressions/sql_join.ts
+++ packages/core/src/abap/2_statements/expressions/sql_join.ts
@@ -4,7 +4,7 @@ import {IStatementRunnable} from "../statement_runnable";
 
 export class SQLJoin extends Expression {
   public getRunnable(): IStatementRunnable {
-    const joinType = seq(optPrio(altPrio("INNER", "LEFT OUTER", "LEFT")), "JOIN");
+    const joinType = seq(optPrio(altPrio("INNER", "LEFT OUTER", "LEFT", "RIGHT OUTER", "RIGHT")), "JOIN");
 
     const join = seq(joinType, SQLFromSource, "ON", SQLCond);
 
