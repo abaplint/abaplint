42fd024ec7607b6b1ffd31c511c2adbd8e5e84da
Lars Hvam <larshp@hotmail.com>
2024-01-25 14:23:27 +0100

    fix test seam

--- packages/core/src/abap/2_statements/statements/test_seam.ts
+++ packages/core/src/abap/2_statements/statements/test_seam.ts
@@ -1,12 +1,13 @@
 import {IStatement} from "./_statement";
-import {seq} from "../combi";
+import {seq, ver} from "../combi";
 import {TestSeamName} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
+import {Version} from "../../../version";
 
 export class TestSeam implements IStatement {
 
   public getMatcher(): IStatementRunnable {
-    return seq("TEST-SEAM", TestSeamName);
+    return ver(Version.v750, seq("TEST-SEAM", TestSeamName));
   }
 
 }
