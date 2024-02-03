8ea64c11570dbe75ff4c8e4435d9760b065baae9
Lars Hvam <larshp@hotmail.com>
2024-02-03 10:56:15 +0100

    fix cloud dynamic

--- packages/core/src/abap/5_syntax/expressions/method_source.ts
+++ packages/core/src/abap/5_syntax/expressions/method_source.ts
@@ -38,7 +38,7 @@ export class MethodSource {
         && first.get() instanceof Expressions.Dynamic
         && first instanceof ExpressionNode
         && children[0]?.concatTokens() === "=>") {
-      const name = first.findDirectExpression(Expressions.Constant)?.concatTokens().replace(/'/g, "");
+      const name = first.findDirectExpression(Expressions.Constant)?.concatTokens().replace(/'/g, "").replace(/`/g, "");
       if (name !== undefined && scope.findClassDefinition(name) === undefined) {
         throw new Error(`Class "${name}" not found/released`);
       }
