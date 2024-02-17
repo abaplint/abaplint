63418bce475e5d1955e4e404bc064e6da6ab28bc
Lars Hvam <larshp@hotmail.com>
2024-02-17 10:35:10 +0100

    add testcase, #3228

--- packages/core/test/abap/find_global_definitions.ts
+++ packages/core/test/abap/find_global_definitions.ts
@@ -46,4 +46,35 @@ ENDCLASS.`;
     new FindGlobalDefinitions(reg).run();
   });
 
+  it.only("cyclic class and type pool", async () => {
+    const abap = `
+CLASS cl_abap_typedescr DEFINITION PUBLIC ABSTRACT CREATE PUBLIC .
+  PUBLIC SECTION.
+    METHODS get_property
+      IMPORTING
+        !p_property_kind TYPE abap_typepropkind
+      RETURNING
+        VALUE(p_value)   TYPE REF TO data.
+ENDCLASS.
+
+CLASS cl_abap_typedescr IMPLEMENTATION.
+ENDCLASS.`;
+
+    const typ = `TYPE-POOL abap.
+TYPES:
+  BEGIN OF abap_func_parmbind,
+    kind      TYPE i,
+    name      TYPE abap_parmname,
+  END OF abap_func_parmbind.
+CONSTANTS abap_func_exporting TYPE abap_func_parmbind-kind VALUE 10.`;
+
+    const files = [
+      new MemoryFile("cl_abap_typedescr.clas.abap", abap),
+      new MemoryFile("abap.type.abap", typ),
+    ];
+
+    const reg = await new Registry().addFiles(files).parseAsync();
+    new FindGlobalDefinitions(reg).run();
+  });
+
 });
