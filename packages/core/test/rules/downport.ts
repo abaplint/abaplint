ec2800745c7267a63fa4d9d3f53dc176eeb67184
Lars Hvam <larshp@hotmail.com>
2024-02-26 10:48:12 +0100

    bugfix, downport

--- packages/core/test/rules/downport.ts
+++ packages/core/test/rules/downport.ts
@@ -5719,7 +5719,7 @@ foo = temp1.`;
     testFix(abap, expected);
   });
 
-  it.only("reference, generic typing, ANY downported to REF TO DATA", async () => {
+  it("reference, generic typing, ANY downported to REF TO DATA", async () => {
     const abap = `
 CLASS lcl DEFINITION.
   PUBLIC SECTION.
@@ -5732,7 +5732,17 @@ CLASS lcl IMPLEMENTATION.
   ENDMETHOD.
 ENDCLASS.`;
     const expected = `
-sdfds`;
+CLASS lcl DEFINITION.
+  PUBLIC SECTION.
+    METHODS foo IMPORTING iv_data TYPE any.
+ENDCLASS.
+
+CLASS lcl IMPLEMENTATION.
+  METHOD foo.
+    DATA sdf TYPE REF TO data.
+GET REFERENCE OF iv_data INTO sdf.
+  ENDMETHOD.
+ENDCLASS.`;
     testFix(abap, expected);
   });
 
