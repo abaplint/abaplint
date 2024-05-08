dc13b32edddbf7291a1efa63eee455a64f2c6c98
Lars Hvam <larshp@hotmail.com>
2024-05-08 06:28:20 +0200

    2.108.0: new rule align_pseudo_comments (#3305)

---
+++ packages/core/src/rules/align_pseudo_comments.ts
@@ -0,0 +1,65 @@
+import {ABAPFile} from "../abap/abap_file";
+import {ABAPRule} from "./_abap_rule";
+import {BasicRuleConfig} from "./_basic_rule_config";
+import {IRuleMetadata, RuleTag} from "./_irule";
+import {Issue} from "../issue";
+import {Comment} from "../abap/2_statements/statements/_statement";
+import {Position} from "../position";
+
+export class AlignPseudoCommentsConf extends BasicRuleConfig {
+}
+
+export class AlignPseudoComments extends ABAPRule {
+  private conf = new AlignPseudoCommentsConf();
+
+  public getMetadata(): IRuleMetadata {
+    return {
+      key: "align_pseudo_comments",
+      title: "Align pseudo comments",
+      shortDescription: `Align code inspector pseudo comments in statements`,
+      tags: [RuleTag.SingleFile, RuleTag.Whitespace],
+      badExample: `WRITE 'sdf'. "#EC sdf`,
+      goodExample: `WRITE 'sdf'.                                                "#EC sdf`,
+    };
+  }
+
+  public getConfig() {
+    return this.conf;
+  }
+
+  public setConfig(conf: AlignPseudoCommentsConf) {
+    this.conf = conf;
+  }
+
+  public runParsed(file: ABAPFile) {
+    const issues: Issue[] = [];
+
+    let previousEnd: Position | undefined = undefined;
+
+    for (const statement of file.getStatements()) {
+      if (!(statement.get() instanceof Comment)) {
+        previousEnd = statement.getLastToken().getEnd();
+        continue;
+      }
+      const commentLength = statement.concatTokens().length;
+      const firstCommentToken = statement.getFirstToken();
+      if (firstCommentToken.getStr().startsWith(`"#`) === false) {
+        continue;
+      } else if (previousEnd === undefined) {
+        continue;
+      } else if (commentLength > 10) {
+        const expectedColumn = 72 - commentLength + 1;
+        if (previousEnd.getCol() < expectedColumn && firstCommentToken.getStart().getCol() !== expectedColumn) {
+          const message = "Align pseudo comment to column " + expectedColumn;
+          issues.push(Issue.atStatement(file, statement, message, this.getMetadata().key, this.conf.severity));
+        }
+      } else if (previousEnd.getCol() < 62 && firstCommentToken.getStart().getCol() !== 62) {
+        const message = "Align pseudo comment to column 62";
+        issues.push(Issue.atStatement(file, statement, message, this.getMetadata().key, this.conf.severity));
+      }
+    }
+
+    return issues;
+  }
+
+}
---
+++ packages/core/test/rules/align_pseudo_comments.ts
@@ -0,0 +1,58 @@
+import {expect} from "chai";
+import {MemoryFile} from "../../src/files/memory_file";
+import {Registry} from "../../src/registry";
+import {AlignPseudoComments} from "../../src/rules";
+import {Issue} from "../../src/issue";
+// import {testRuleFixSingle} from "./_utils";
+
+/*
+function testFix(input: string, expected: string, noIssuesAfter = true) {
+  testRuleFixSingle(input, expected, new AlignPseudoComments(), undefined, undefined, noIssuesAfter);
+}
+*/
+
+async function findIssues(abap: string): Promise<readonly Issue[]> {
+  const reg = new Registry().addFile(new MemoryFile("zfoo.prog.abap", abap));
+  await reg.parseAsync();
+  const rule = new AlignPseudoComments();
+  return rule.initialize(reg).run(reg.getFirstObject()!);
+}
+
+describe("Rule: align_pseudo_comments", () => {
+
+  it("parser error, no issues expected", async () => {
+    const issues = await findIssues("hello world.");
+    expect(issues.length).to.equal(0);
+  });
+
+  it("ok1", async () => {
+    const issues = await findIssues(`WRITE 'sdf'.                                                "#EC sdf`);
+    expect(issues.length).to.equal(0);
+  });
+
+  it("ok2", async () => {
+    const issues = await findIssues(`WRITE 'sdf'.                                                "#EC sdfaaa`);
+    expect(issues.length).to.equal(0);
+  });
+
+  it("ok3", async () => {
+    const issues = await findIssues(`WRITE 'sdf'.                                    "#EC sdfsdfsdfsdfsdfsdf`);
+    expect(issues.length).to.equal(0);
+  });
+
+  it("err1", async () => {
+    const issues = await findIssues(`WRITE 'sdf'. "#EC sdf`);
+    expect(issues.length).to.equal(1);
+  });
+
+  it("err2", async () => {
+    const issues = await findIssues(`WRITE 'sdf'.            "#EC sdf`);
+    expect(issues.length).to.equal(1);
+  });
+
+  it("ok, long", async () => {
+    const issues = await findIssues(`WRITE 'ssdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdf'. "#EC sdf`);
+    expect(issues.length).to.equal(0);
+  });
+
+});
