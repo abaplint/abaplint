6abe0134760966bd99f43fb807ec0a0a9476c775
Lars Hvam <larshp@hotmail.com>
2024-05-15 12:53:15 +0200

    start parsing TRAN

--- packages/core/src/objects/transaction.ts
+++ packages/core/src/objects/transaction.ts
@@ -1,9 +1,18 @@
 import {AbstractObject} from "./_abstract_object";
 
 export class Transaction extends AbstractObject {
+  private parsedXML: {
+    description?: string,
+    programName?: string,
+  } | undefined;
 
   public getType(): string {
     return "TRAN";
+  }
+
+  public setDirty(): void {
+    this.parsedXML = undefined;
+    super.setDirty();
   }
 
   public getAllowedNaming() {
@@ -13,8 +22,34 @@ export class Transaction extends AbstractObject {
     };
   }
 
+  public getProgramName(): string | undefined {
+    this.parse();
+    return this.parsedXML?.programName;
+  }
+
   public getDescription(): string | undefined {
-    // todo
-    return undefined;
+    this.parse();
+    return this.parsedXML?.description;
+  }
+
+  public parse() {
+    if (this.parsedXML) {
+      return {updated: false, runtime: 0};
+    }
+
+    const start = Date.now();
+    this.parsedXML = {};
+    const parsed = super.parseRaw2();
+    if (parsed === undefined
+        || parsed.abapGit === undefined
+        || parsed.abapGit["asx:abap"]["asx:values"] === undefined) {
+      return {updated: false, runtime: 0};
+    }
+
+    this.parsedXML.description = parsed.abapGit["asx:abap"]["asx:values"].TSTCT.TTEXT;
+    this.parsedXML.programName = parsed.abapGit["asx:abap"]["asx:values"].TSTC.PGMNA;
+
+    const end = Date.now();
+    return {updated: true, runtime: end - start};
   }
 }
