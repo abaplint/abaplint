6c472c0d5330572e6decf2cf8b81f4e4ab3e64c9
Lars Hvam <larshp@hotmail.com>
2024-05-15 18:35:03 +0200

    2.108.7: basic ENHO parsing (#3316)

--- packages/core/src/objects/enhancement_implementation.ts
+++ packages/core/src/objects/enhancement_implementation.ts
@@ -1,9 +1,18 @@
 import {AbstractObject} from "./_abstract_object";
 
 export class EnhancementImplementation extends AbstractObject {
+  private parsedXML: {
+    className?: string,
+    description?: string,
+  } | undefined;
 
   public getType(): string {
     return "ENHO";
+  }
+
+  public setDirty(): void {
+    this.parsedXML = undefined;
+    super.setDirty();
   }
 
   public getAllowedNaming() {
@@ -13,8 +22,34 @@ export class EnhancementImplementation extends AbstractObject {
     };
   }
 
+  public getClassName(): string | undefined {
+    this.parse();
+    return this.parsedXML?.className;
+  }
+
   public getDescription(): string | undefined {
-    // todo
-    return undefined;
+    this.parse();
+    return this.parsedXML?.description;
+  }
+
+  public parse() {
+    if (this.parsedXML) {
+      return {updated: false, runtime: 0};
+    }
+
+    const start = Date.now();
+    this.parsedXML = {};
+    const parsed = super.parseRaw2();
+    if (parsed === undefined
+        || parsed.abapGit === undefined
+        || parsed.abapGit["asx:abap"]["asx:values"] === undefined) {
+      return {updated: false, runtime: 0};
+    }
+
+    this.parsedXML.className = parsed.abapGit["asx:abap"]["asx:values"].CLASS;
+    this.parsedXML.description = parsed.abapGit["asx:abap"]["asx:values"].SHORTTEXT;
+
+    const end = Date.now();
+    return {updated: true, runtime: end - start};
   }
 }
