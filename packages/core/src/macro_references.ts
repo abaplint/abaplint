f1affe85e3874d09600fbe6581c0a1dce42a0d6d
Lars Hvam <larshp@hotmail.com>
2024-06-07 17:55:00 +0200

    2.109.2: bugfix (#3337)

--- packages/core/src/macro_references.ts
+++ packages/core/src/macro_references.ts
@@ -22,7 +22,7 @@ export class MacroReferences implements IMacroReferences {
   public getDefinitionPosition(filename: string, token: AbstractToken): {start: Position, end: Position} | undefined {
     for (const d of this.definitions[filename] || []) {
       if (d.token.getStart().equals(token.getStart())) {
-        return {start: d.token.getStart(), end: d.token.getEnd()};
+        return {start: d.start, end: d.end};
       }
     }
     return undefined;
