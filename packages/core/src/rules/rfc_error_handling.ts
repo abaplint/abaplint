f73205ffcddc562c44bf6e3a2f0ce55ac6258029
Lars Hvam <larshp@hotmail.com>
2024-06-12 07:29:27 +0200

    cleanup example

--- packages/core/src/rules/rfc_error_handling.ts
+++ packages/core/src/rules/rfc_error_handling.ts
@@ -19,11 +19,9 @@ export class RFCErrorHandling extends ABAPRule {
       tags: [RuleTag.SingleFile],
       shortDescription: `Checks that exceptions 'system_failure' and 'communication_failure' are handled in RFC calls`,
       extendedInformation: `https://help.sap.com/doc/abapdocu_750_index_htm/7.50/en-US/abenrfc_exception.htm`,
-      badExample: `
-CALL FUNCTION 'ZRFC'
+      badExample: `CALL FUNCTION 'ZRFC'
   DESTINATION lv_rfc.`,
-      goodExample: `
-CALL FUNCTION 'ZRFC'
+      goodExample: `CALL FUNCTION 'ZRFC'
   DESTINATION lv_rfc
   EXCEPTIONS
     system_failure        = 1 MESSAGE msg
