16e8a5958334dd8aa3eb17f32aa9b8968f3c15b1
Lars Hvam <larshp@hotmail.com>
2024-07-02 13:02:16 +0200

    2.110.3: bugfixes, syntax (#3344)

--- packages/core/src/abap/2_statements/expressions/sql_function.ts
+++ packages/core/src/abap/2_statements/expressions/sql_function.ts
@@ -1,7 +1,7 @@
 import {Constant} from "./constant";
 import {Version} from "../../../version";
 import {ParenLeftW, WAt, WParenRightW} from "../../1_lexer/tokens";
-import {Expression, ver, seq, tok, alt, altPrio} from "../combi";
+import {Expression, ver, seq, tok, altPrio, optPrio} from "../combi";
 import {IStatementRunnable} from "../statement_runnable";
 import {Integer} from "./integer";
 import {SQLAliasField} from "./sql_alias_field";
@@ -13,9 +13,9 @@ import {SQLAggregation} from "./sql_aggregation";
 export class SQLFunction extends Expression {
   public getRunnable(): IStatementRunnable {
     const paren = seq(tok(ParenLeftW), Source, tok(WParenRightW));
-    const at = ver(Version.v740sp05, seq(tok(WAt), alt(SimpleSource3, paren)));
+    const at = ver(Version.v740sp05, seq(tok(WAt), altPrio(SimpleSource3, paren)));
 
-    const param = alt(SQLFieldName, SQLAliasField, SQLFunction, Constant, SQLAggregation, at);
+    const param = altPrio(SQLFunction, SQLAggregation, SQLFieldName, SQLAliasField, Constant, at);
 
     const castTypes = altPrio(
       seq("CHAR", tok(ParenLeftW), Integer, tok(WParenRightW)),
@@ -24,18 +24,20 @@ export class SQLFunction extends Expression {
       "NUMC",
       "INT8");
 
+    const commaParam = seq(",", param);
+
     const abs = ver(Version.v740sp05, seq("abs", tok(ParenLeftW), param, tok(WParenRightW)));
     const cast = ver(Version.v750, seq("cast", tok(ParenLeftW), param, "AS", castTypes, tok(WParenRightW)));
     const ceil = ver(Version.v740sp05, seq("ceil", tok(ParenLeftW), param, tok(WParenRightW)));
-    const coalesce = ver(Version.v740sp05, seq("coalesce", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
-    const concat = ver(Version.v750, seq("concat", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
-    const div = ver(Version.v740sp05, seq("div", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
+    const coalesce = ver(Version.v740sp05, seq("coalesce", tok(ParenLeftW), param, commaParam, optPrio(commaParam), tok(WParenRightW)));
+    const concat = ver(Version.v750, seq("concat", tok(ParenLeftW), param, commaParam, tok(WParenRightW)));
+    const div = ver(Version.v740sp05, seq("div", tok(ParenLeftW), param, commaParam, tok(WParenRightW)));
     const floor = ver(Version.v740sp05, seq("floor", tok(ParenLeftW), param, tok(WParenRightW)));
     const length = ver(Version.v750, seq("length", tok(ParenLeftW), param, tok(WParenRightW)));
     const lower = ver(Version.v751, seq("lower", tok(ParenLeftW), param, tok(WParenRightW)));
-    const mod = ver(Version.v740sp05, seq("mod", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
-    const replace = ver(Version.v750, seq("replace", tok(ParenLeftW), param, ",", param, ",", param, tok(WParenRightW)));
-    const round = ver(Version.v750, seq("round", tok(ParenLeftW), param, ",", param, tok(WParenRightW)));
+    const mod = ver(Version.v740sp05, seq("mod", tok(ParenLeftW), param, commaParam, tok(WParenRightW)));
+    const replace = ver(Version.v750, seq("replace", tok(ParenLeftW), param, commaParam, commaParam, tok(WParenRightW)));
+    const round = ver(Version.v750, seq("round", tok(ParenLeftW), param, commaParam, tok(WParenRightW)));
     const upper = ver(Version.v751, seq("upper", tok(ParenLeftW), param, tok(WParenRightW)));
     const uuid = ver(Version.v754, seq("uuid", tok(ParenLeftW), tok(WParenRightW)));
 
