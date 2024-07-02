190bfd90a6b7d9e8b6dbdf03975a6603b77628dd
Lars Hvam <larshp@hotmail.com>
2024-07-02 13:57:49 +0200

    case

--- packages/core/src/abap/2_statements/expressions/sql_case.ts
+++ packages/core/src/abap/2_statements/expressions/sql_case.ts
@@ -1,7 +1,7 @@
 import {SQLAggregation, SQLFunction, SQLPath} from ".";
 import {Version} from "../../../version";
 import {WParenLeftW, WParenRightW} from "../../1_lexer/tokens";
-import {Expression, ver, alt, seq, plus, tok, optPrio, opt, altPrio, starPrio} from "../combi";
+import {Expression, ver, seq, plus, tok, optPrio, opt, altPrio, starPrio} from "../combi";
 import {IStatementRunnable} from "../statement_runnable";
 import {Constant} from "./constant";
 import {SQLCond} from "./sql_cond";
@@ -20,7 +20,7 @@ export class SQLCase extends Expression {
     const sub = seq(altPrio("+", "-", "*", "/", "&&"), optPrio(tok(WParenLeftW)), field, optPrio(tok(WParenRightW)));
 
     const sourc = altPrio(SQLCase, SQLAggregation, SQLFunction, SQLSource);
-    const when = seq("WHEN", alt(Constant, SQLCond), "THEN", sourc, starPrio(sub));
+    const when = seq("WHEN", altPrio(SQLCond, Constant), "THEN", sourc, starPrio(sub));
     const els = seq("ELSE", sourc);
 
     return ver(Version.v740sp05, seq("CASE", opt(SQLFieldName), plus(when), optPrio(els), "END"));
