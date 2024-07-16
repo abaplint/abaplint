36fc44b163f9f4fedd816e9a82585547449bba12
Lars Hvam <larshp@hotmail.com>
2024-07-16 15:11:48 +0200

    minor

--- packages/core/src/abap/5_syntax/expressions/default.ts
+++ packages/core/src/abap/5_syntax/expressions/default.ts
@@ -3,14 +3,21 @@ import {ExpressionNode} from "../../nodes";
 import {CurrentScope} from "../_current_scope";
 import {FieldChain} from "./field_chain";
 import {ReferenceType} from "../_reference";
+import {Constant} from "./constant";
 
 export class Default {
   public runSyntax(node: ExpressionNode, scope: CurrentScope, filename: string) {
 
     const chain = node.findDirectExpression(Expressions.FieldChain);
     if (chain) {
-      new FieldChain().runSyntax(chain, scope, filename, ReferenceType.DataReadReference);
+      return new FieldChain().runSyntax(chain, scope, filename, ReferenceType.DataReadReference);
     }
 
+    const constant = node.findDirectExpression(Expressions.Constant);
+    if (constant) {
+      return new Constant().runSyntax(constant);
+    }
+
+    return undefined;
   }
 }
