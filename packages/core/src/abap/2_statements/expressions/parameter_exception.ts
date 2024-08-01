aa3cab556dafadbfa0aacbdabf4770ba50ff43da
Lars Hvam <larshp@hotmail.com>
2024-08-01 17:55:12 +0200

    2.112.9: bugfix, method call (#3382)

--- packages/core/src/abap/2_statements/expressions/parameter_exception.ts
+++ packages/core/src/abap/2_statements/expressions/parameter_exception.ts
@@ -1,5 +1,5 @@
 import {seq, altPrio, Expression, optPrio} from "../combi";
-import {ParameterName, SimpleName, Target} from ".";
+import {Integer, ParameterName, SimpleFieldChain, Target} from ".";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class ParameterException extends Expression {
@@ -7,7 +7,7 @@ export class ParameterException extends Expression {
     const name = altPrio("OTHERS", ParameterName);
     return seq(name,
                "=",
-               SimpleName,
+               altPrio(Integer, SimpleFieldChain),
                optPrio(seq("MESSAGE", Target)));
   }
 }
