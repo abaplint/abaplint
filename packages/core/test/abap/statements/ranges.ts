4be54ced1eb1de1166ac9aa8d7134b6d583cd0d1
Lars Hvam <larshp@hotmail.com>
2024-08-08 11:39:30 +0200

    add testcases

--- packages/core/test/abap/statements/ranges.ts
+++ packages/core/test/abap/statements/ranges.ts
@@ -5,6 +5,7 @@ const tests = [
   "ranges l_prot_only for smodilog-prot_only.",
   "RANGES moo FOR foo-bar OCCURS 50.",
   "RANGES $tadir$ FOR tadir-devclass OCCURS 10.",
+  "RANGES : lr_prctr FOR <foo>-prctr.",
 ];
 
 statementType(tests, "RANGES", Statements.Ranges);
