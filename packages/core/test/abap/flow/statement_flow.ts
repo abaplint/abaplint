32e92168036e63f5dfeb45c698741e0c012fd13c
Lars Hvam <larshp@hotmail.com>
2024-10-17 12:53:30 +0200

    CATCH SYSTEM-EXCEPTIONS

--- packages/core/test/abap/flow/statement_flow.ts
+++ packages/core/test/abap/flow/statement_flow.ts
@@ -551,4 +551,17 @@ ENDMODULE.`;
     expect(res[0].getLabel()).to.equal("MODULE FOO INPUT.");
   });
 
+  it.skip("CATCH SYSTEM-EXCEPTIONS", async () => {
+    const abap = `
+REPORT zfoo.
+
+CATCH SYSTEM-EXCEPTIONS move_cast_error = 1.
+  lo_tabl_descr ?= lo_type_descr.
+ENDCATCH.`;
+
+    const res = await runRaw(abap);
+    expect(res.length).to.equal(1);
+    expect(res[0].getLabel()).to.equal("MODULE FOO INPUT.");
+  });
+
 });
