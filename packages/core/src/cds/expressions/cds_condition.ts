1cc23dc40c5201fb67a951e636d5ea4b450c41a1
Lars Hvam <larshp@hotmail.com>
2024-11-08 14:35:47 +0100

    2.113.43: cds parsing bugfix (#3458)

--- packages/core/src/cds/expressions/cds_condition.ts
+++ packages/core/src/cds/expressions/cds_condition.ts
@@ -1,16 +1,16 @@
 import {CDSFunction, CDSName, CDSString} from ".";
-import {alt, altPrio, Expression, opt, optPrio, seq, star} from "../../abap/2_statements/combi";
+import {alt, altPrio, Expression, optPrio, seq, starPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSInteger} from "./cds_integer";
 
 export class CDSCondition extends Expression {
   public getRunnable(): IStatementRunnable {
-    const name = seq(CDSName, opt(seq(".", alt(CDSName, CDSString))));
+    const name = seq(CDSName, optPrio(seq(".", alt(CDSName, CDSString))));
     const left = alt(name, CDSFunction, CDSString);
     const compare = seq(left, alt("=", seq("!", "="), seq("<", ">"), "<", ">", seq(">", "="), seq("<", "="), "LIKE", "NOT LIKE"), alt(left, CDSInteger, CDSFunction, CDSString));
     const is = seq(left, "IS", optPrio("NOT"), altPrio("INITIAL", "NULL"));
     const condition = seq(optPrio("NOT"), alt(compare, is));
     const paren = seq("(", CDSCondition, ")");
-    return seq(alt(condition, paren), star(seq(alt("AND", "OR"), alt(condition, paren))));
+    return seq(alt(condition, paren), starPrio(seq(altPrio("AND", "OR"), altPrio(condition, paren))));
   }
 }
--- packages/core/src/cds/expressions/cds_select.ts
+++ packages/core/src/cds/expressions/cds_select.ts
@@ -1,18 +1,18 @@
 import {CDSElement, CDSComposition, CDSGroupBy, CDSSource, CDSWhere, CDSHaving} from ".";
-import {Expression, seq, str, plus, star, opt} from "../../abap/2_statements/combi";
+import {Expression, seq, str, opt, optPrio, starPrio, star} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSAssociation} from "./cds_association";
 import {CDSJoin} from "./cds_join";
 
 export class CDSSelect extends Expression {
   public getRunnable(): IStatementRunnable {
-    const fields = opt(seq(star(seq(CDSElement, ",")), CDSElement));
+    const fields = seq(star(seq(CDSElement, ",")), CDSElement);
     const distinct = str("DISTINCT");
 
-    const elements = seq(str("{"), plus(CDSElement), star(seq(",", CDSElement)), str("}"));
+    const elements = seq(str("{"), CDSElement, starPrio(seq(",", CDSElement)), str("}"));
 
     return seq("SELECT",
-               opt(distinct),
+               optPrio(distinct),
                opt(fields),
                "FROM",
                CDSSource,
@@ -21,9 +21,9 @@ export class CDSSelect extends Expression {
                star(CDSAssociation),
                star(CDSComposition),
                opt(elements),
-               opt(CDSWhere),
-               opt(CDSGroupBy),
-               opt(CDSHaving),
-               opt(seq("UNION", opt("ALL"), CDSSelect)));
+               optPrio(CDSWhere),
+               optPrio(CDSGroupBy),
+               optPrio(CDSHaving),
+               optPrio(seq("UNION", optPrio("ALL"), CDSSelect)));
   }
 }
