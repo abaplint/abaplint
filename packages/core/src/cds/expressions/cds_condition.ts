69d366ca38a979ca3cea589224814e32b2e3f2fb
Lars Hvam <larshp@hotmail.com>
2024-11-08 14:11:57 +0100

    Fix

--- packages/core/src/cds/expressions/cds_condition.ts
+++ packages/core/src/cds/expressions/cds_condition.ts
@@ -1,11 +1,11 @@
 import {CDSFunction, CDSName, CDSString} from ".";
-import {alt, altPrio, Expression, opt, optPrio, seq, starPrio} from "../../abap/2_statements/combi";
+import {alt, altPrio, Expression, optPrio, seq, starPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSInteger} from "./cds_integer";
 
 export class CDSCondition extends Expression {
   public getRunnable(): IStatementRunnable {
-    const name = seq(CDSName, opt(seq(".", alt(CDSName, CDSString))));
+    const name = seq(CDSName, optPrio(seq(".", alt(CDSName, CDSString))));
     const left = alt(name, CDSFunction, CDSString);
     const compare = seq(left, alt("=", seq("!", "="), seq("<", ">"), "<", ">", seq(">", "="), seq("<", "="), "LIKE", "NOT LIKE"), alt(left, CDSInteger, CDSFunction, CDSString));
     const is = seq(left, "IS", optPrio("NOT"), altPrio("INITIAL", "NULL"));
