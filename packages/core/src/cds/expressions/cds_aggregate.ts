93a8cf8d331c948afcafeb7a864dbbe2efbce200
Lars Hvam <larshp@hotmail.com>
2024-11-13 09:37:39 +0100

    cds syntax fixes

--- packages/core/src/cds/expressions/cds_aggregate.ts
+++ packages/core/src/cds/expressions/cds_aggregate.ts
@@ -1,11 +1,11 @@
-import {CDSCase, CDSCast, CDSName} from ".";
+import {CDSCase, CDSCast, CDSFunction, CDSName} from ".";
 import {alt, Expression, opt, seq} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSAggregate extends Expression {
   public getRunnable(): IStatementRunnable {
     const name = seq(CDSName, opt(seq(".", CDSName)));
-    const value = alt(name, CDSCast, CDSCase);
+    const value = alt(name, CDSCast, CDSCase, CDSFunction);
     return seq(alt("MAX", "MIN", "SUM", "AVG", "COUNT"), "(", opt("DISTINCT"), value, ")");
   }
 }
