eac8e34f7d9557362c801c7a8e82d149e900e6f2
Lars Hvam <larshp@hotmail.com>
2024-11-13 09:53:53 +0100

    fix arith

--- packages/core/src/cds/expressions/cds_arithmetics.ts
+++ packages/core/src/cds/expressions/cds_arithmetics.ts
@@ -1,5 +1,5 @@
 import {CDSCase, CDSCast, CDSFunction, CDSName, CDSString} from ".";
-import {alt, altPrio, Expression, opt, seq} from "../../abap/2_statements/combi";
+import {alt, altPrio, Expression, opt, plus, seq} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSInteger} from "./cds_integer";
 
@@ -9,8 +9,9 @@ export class CDSArithmetics extends Expression {
     const val = alt(CDSInteger, name, CDSFunction, CDSCase, CDSCast, CDSString);
     const operator = altPrio("+", "-", "*", "/");
 
-    const paren = seq("(", val, operator, val, ")");
-    const noParen = seq(val, operator, val);
+    const operatorValue = seq(operator, val);
+    const paren = seq("(", val, plus(operatorValue), ")");
+    const noParen = seq(val, plus(operatorValue));
     return altPrio(paren, noParen);
   }
 }
