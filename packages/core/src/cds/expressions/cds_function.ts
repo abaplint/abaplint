cf5b583de48dab88ae566928119b47c1de287958
Lars Hvam <larshp@hotmail.com>
2024-11-13 10:09:44 +0100

    refactor

--- packages/core/src/cds/expressions/cds_function.ts
+++ packages/core/src/cds/expressions/cds_function.ts
@@ -1,53 +1,50 @@
-import {CDSArithmetics, CDSCase, CDSCast, CDSName, CDSParameters, CDSString} from ".";
-import {altPrio, Expression, opt, regex, seq, starPrio} from "../../abap/2_statements/combi";
+import {CDSFunctionInput, CDSName} from ".";
+import {altPrio, Expression, seq, starPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSFunction extends Expression {
   public getRunnable(): IStatementRunnable {
-    const qualified = seq(CDSName, opt(CDSParameters), starPrio(seq(".", CDSName, opt(CDSParameters))));
-    const input = altPrio(CDSCast, CDSFunction, CDSArithmetics, CDSCase, CDSString, qualified, regex(/^\d+$/), );
+    const coalesce = seq("COALESCE", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const concat = seq("CONCAT", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const concat_with_space = seq("CONCAT_WITH_SPACE", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const dats_add_days = seq("DATS_ADD_DAYS", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const dats_add_months = seq("DATS_ADD_MONTHS", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const dats_days_between = seq("DATS_DAYS_BETWEEN", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const dats_is_valid = seq("DATS_IS_VALID", "(", CDSFunctionInput, ")");
+    const substring = seq("SUBSTRING", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const coalesce = seq("COALESCE", "(", input, ",", input, ")");
-    const concat = seq("CONCAT", "(", input, ",", input, ")");
-    const concat_with_space = seq("CONCAT_WITH_SPACE", "(", input, ",", input, ",", input, ")");
-    const dats_add_days = seq("DATS_ADD_DAYS", "(", input, ",", input, ",", input, ")");
-    const dats_add_months = seq("DATS_ADD_MONTHS", "(", input, ",", input, ",", input, ")");
-    const dats_days_between = seq("DATS_DAYS_BETWEEN", "(", input, ",", input, ")");
-    const dats_is_valid = seq("DATS_IS_VALID", "(", input, ")");
-    const substring = seq("SUBSTRING", "(", input, ",", input, ",", input, ")");
+    const bintohex = seq("BINTOHEX", "(", CDSFunctionInput, ")");
+    const hextobin = seq("HEXTOBIN", "(", CDSFunctionInput, ")");
 
-    const bintohex = seq("BINTOHEX", "(", input, ")");
-    const hextobin = seq("HEXTOBIN", "(", input, ")");
+    const upper = seq("UPPER", "(", CDSFunctionInput, ")");
+    const lower = seq("LOWER", "(", CDSFunctionInput, ")");
 
-    const upper = seq("UPPER", "(", input, ")");
-    const lower = seq("LOWER", "(", input, ")");
+    const abs = seq("ABS", "(", CDSFunctionInput, ")");
+    const ceil = seq("CEIL", "(", CDSFunctionInput, ")");
+    const floor = seq("FLOOR", "(", CDSFunctionInput, ")");
+    const round = seq("ROUND", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const div = seq("DIV", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const division = seq("DIVISION", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const abs = seq("ABS", "(", input, ")");
-    const ceil = seq("CEIL", "(", input, ")");
-    const floor = seq("FLOOR", "(", input, ")");
-    const round = seq("ROUND", "(", input, ",", input, ")");
-    const div = seq("DIV", "(", input, ",", input, ")");
-    const division = seq("DIVISION", "(", input, ",", input, ",", input, ")");
+    const tstmp_to_dats = seq("TSTMP_TO_DATS", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const tstmp_to_tims = seq("TSTMP_TO_TIMS", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const tstmp_to_dst = seq("TSTMP_TO_DST", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const dats_tims_to_tstmp = seq("DATS_TIMS_TO_TSTMP", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const tstmp_to_dats = seq("TSTMP_TO_DATS", "(", input, ",", input, ",", input, ",", input, ")");
-    const tstmp_to_tims = seq("TSTMP_TO_TIMS", "(", input, ",", input, ",", input, ",", input, ")");
-    const tstmp_to_dst = seq("TSTMP_TO_DST", "(", input, ",", input, ",", input, ",", input, ")");
-    const dats_tims_to_tstmp = seq("DATS_TIMS_TO_TSTMP", "(", input, ",", input, ",", input, ",", input, ",", input, ")");
-
-    const tstmp_is_valid = seq("TSTMP_IS_VALID", "(", input, ")");
+    const tstmp_is_valid = seq("TSTMP_IS_VALID", "(", CDSFunctionInput, ")");
     const tstmp_current_utctimestamp = seq("TSTMP_CURRENT_UTCTIMESTAMP", "(", ")");
-    const tstmp_seconds_between = seq("TSTMP_SECONDS_BETWEEN", "(", input, ",", input, ",", input, ")");
-    const tstmp_add_seconds = seq("TSTMP_ADD_SECONDS", "(", input, ",", input, ",", input, ")");
+    const tstmp_seconds_between = seq("TSTMP_SECONDS_BETWEEN", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const tstmp_add_seconds = seq("TSTMP_ADD_SECONDS", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const abap_system_timezone = seq("ABAP_SYSTEM_TIMEZONE", "(", input, ",", input, ")");
-    const abap_user_timezone = seq("ABAP_USER_TIMEZONE", "(", input, ",", input, ",", input, ")");
+    const abap_system_timezone = seq("ABAP_SYSTEM_TIMEZONE", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const abap_user_timezone = seq("ABAP_USER_TIMEZONE", "(", CDSFunctionInput, ",", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const mod = seq("MOD", "(", input, ",", input, ")");
+    const mod = seq("MOD", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const left = seq("LEFT", "(", input, ",", input, ")");
-    const right = seq("RIGHT", "(", input, ",", input, ")");
+    const left = seq("LEFT", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
+    const right = seq("RIGHT", "(", CDSFunctionInput, ",", CDSFunctionInput, ")");
 
-    const conversionInput = seq(CDSName, "=", ">", input);
+    const conversionInput = seq(CDSName, "=", ">", CDSFunctionInput);
     const conversionInputs = seq(conversionInput, starPrio(seq(",", conversionInput)));
     const unitConversion = seq("UNIT_CONVERSION", "(", conversionInputs, ")");
     const currencyConversion = seq("CURRENCY_CONVERSION", "(", conversionInputs, ")");
