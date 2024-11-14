e2e035f42d75af7c13d6a5baf6a44a61bdc160bb
Lars Hvam <larshp@hotmail.com>
2024-11-14 16:50:40 +0100

    fix

--- packages/core/src/abap/2_statements/expressions/inline_loop_definition.ts
+++ packages/core/src/abap/2_statements/expressions/inline_loop_definition.ts
@@ -1,10 +1,11 @@
 import {Expression, seq, opt, altPrio, optPrio} from "../combi";
-import {TargetField, Source, TargetFieldSymbol} from ".";
+import {TargetField, Source, TargetFieldSymbol, SimpleName} from ".";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class InlineLoopDefinition extends Expression {
   public getRunnable(): IStatementRunnable {
     const index = seq("INDEX INTO", TargetField);
-    return seq(altPrio(TargetFieldSymbol, TargetField), "IN", opt("GROUP"), Source, optPrio(index));
+    const key = seq("USING KEY", SimpleName);
+    return seq(altPrio(TargetFieldSymbol, TargetField), "IN", opt("GROUP"), Source, optPrio(key), optPrio(index));
   }
 }
