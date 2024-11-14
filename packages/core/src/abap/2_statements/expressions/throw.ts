7790ce2cfe155a7abb4573132d29280e0604487d
Lars Hvam <larshp@hotmail.com>
2024-11-14 18:32:24 +0100

    bugfix THROW MESSAGE

--- packages/core/src/abap/2_statements/expressions/throw.ts
+++ packages/core/src/abap/2_statements/expressions/throw.ts
@@ -1,16 +1,19 @@
 import {seq, opt, Expression, tok, alt, altPrio} from "../combi";
-import {ClassName, ParameterListS, Source} from ".";
+import {ClassName, MessageSource, ParameterListS, RaiseWith, Source} from ".";
 import {ParenLeft, ParenLeftW, ParenRightW, WParenRightW} from "../../1_lexer/tokens";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class Throw extends Expression {
   public getRunnable(): IStatementRunnable {
-// todo, MESSAGE
+    const message = seq("MESSAGE",
+                        MessageSource,
+                        opt(RaiseWith));
+
     return seq("THROW",
                opt("RESUMABLE"),
                ClassName,
                altPrio(tok(ParenLeftW), tok(ParenLeft)),
-               opt(alt(Source, ParameterListS)),
+               opt(alt(Source, ParameterListS, message)),
                altPrio(tok(WParenRightW), tok(ParenRightW)));
   }
 }
