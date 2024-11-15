e6f01a547517de52c7dd2a4fd49b39e272d45589
Lars Hvam <larshp@hotmail.com>
2024-11-15 18:11:40 +0100

    fix FORMAT

--- packages/core/test/abap/statements/assign_local_copy.ts
+++ packages/core/test/abap/statements/assign_local_copy.ts
@@ -6,6 +6,7 @@ const tests = [
   "assign local copy of <lt_foo> to <lt_bar>.",
   "ASSIGN LOCAL COPY OF INITIAL (L_VAR) TO <WA>.",
   "ASSIGN LOCAL COPY OF INITIAL data[] TO <lt_data>.",
+  `ASSIGN LOCAL COPY OF MAIN TABLE FIELD (gl_tabf) TO <feld>.`,
 ];
 
 statementType(tests, "ASSIGN LOCAL COPY", Statements.AssignLocalCopy);
