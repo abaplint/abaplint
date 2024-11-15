e2e003d62c7827269be342179d11a6f39da3525d
Lars Hvam <larshp@hotmail.com>
2024-11-15 17:39:27 +0100

    bugfix PRINT

--- packages/core/test/abap/statements/print_control.ts
+++ packages/core/test/abap/statements/print_control.ts
@@ -4,6 +4,7 @@ import * as Statements from "../../../src/abap/2_statements/statements";
 const tests = [
   "print-control index-line lv_line.",
   "print-control function 'ASDF'.",
+  `PRINT-CONTROL LINE 1 POSITION 1 SIZE siz.`,
 ];
 
 statementType(tests, "PRINT-CONTROL", Statements.PrintControl);
