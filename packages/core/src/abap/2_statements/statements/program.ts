e09ca36e2c360caa9eb435af2b68ebde61ca4097
Lars Hvam <larshp@hotmail.com>
2024-11-17 12:49:32 +0100

    fix

--- packages/core/src/abap/2_statements/statements/program.ts
+++ packages/core/src/abap/2_statements/statements/program.ts
@@ -1,6 +1,6 @@
 import {IStatement} from "./_statement";
 import {verNot, str, seq, opt, per} from "../combi";
-import {Source, ReportName, MessageClass} from "../expressions";
+import {Source, ReportName, MessageClass, FieldLength} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 
@@ -10,7 +10,7 @@ export class Program implements IStatement {
     const message = seq("MESSAGE-ID", MessageClass);
     const size = seq("LINE-SIZE", Source);
     const heading = str("NO STANDARD PAGE HEADING");
-    const line = seq("LINE-COUNT", Source);
+    const line = seq("LINE-COUNT", Source, opt(FieldLength));
     const options = per(message, size, heading, line);
 
     const ret = seq("PROGRAM", opt(ReportName), opt(options));
