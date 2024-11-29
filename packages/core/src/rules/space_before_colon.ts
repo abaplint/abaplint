bcb18c8d89c683471b2a10a927f9e06c65ea6af9
Lars Hvam <larshp@hotmail.com>
2024-11-29 14:44:04 +0100

    fix

--- packages/core/src/rules/space_before_colon.ts
+++ packages/core/src/rules/space_before_colon.ts
@@ -5,6 +5,8 @@ import {Issue} from "../issue";
 import {ABAPRule} from "./_abap_rule";
 import {BasicRuleConfig} from "./_basic_rule_config";
 import {IRuleMetadata, RuleTag} from "./_irule";
+import {NativeSQL} from "../abap/2_statements/statements/_statement";
+import {Comment} from "../abap/1_lexer/tokens";
 
 export class SpaceBeforeColonConf extends BasicRuleConfig {
 }
@@ -40,24 +42,33 @@ export class SpaceBeforeColon extends ABAPRule {
   public runParsed(file: ABAPFile) {
     const issues: Issue[] = [];
 
-    let prev = file.getTokens()[0];
-
-    for (const token of file.getTokens()) {
-      if (token.getStr() === ":" && !prev) {
-        const issue = Issue.atToken(file, token, this.getMessage(), this.getMetadata().key, this.conf.severity);
-        issues.push(issue);
-      } else if (token.getStr() === ":"
-          && prev.getRow() === token.getRow()
-          && prev.getCol() + prev.getStr().length < token.getCol()) {
-        const start = new Position(token.getRow(), prev.getEnd().getCol());
-        const end = new Position(token.getRow(), token.getStart().getCol());
-        const fix = EditHelper.deleteRange(file, start, end);
-        const issue = Issue.atRowRange(file, start.getRow(),
-                                       start.getCol(), end.getCol(),
-                                       this.getMessage(), this.getMetadata().key, this.conf.severity, fix);
-        issues.push(issue);
+    for (const statement of file.getStatements()) {
+      const colon = statement.getColon();
+      if (colon === undefined
+          || statement.get() instanceof NativeSQL
+          || statement.get() instanceof Comment) {
+        continue;
       }
-      prev = token;
+
+      let prev = statement.getTokens()[0];
+
+      for (const token of file.getTokens()) {
+        if (token.getStr() === ":" && !prev) {
+          const issue = Issue.atToken(file, token, this.getMessage(), this.getMetadata().key, this.conf.severity);
+          issues.push(issue);
+        } else if (token.getStr() === ":"
+            && prev.getRow() === token.getRow()
+            && prev.getCol() + prev.getStr().length < token.getCol()) {
+          const start = new Position(token.getRow(), prev.getEnd().getCol());
+          const end = new Position(token.getRow(), token.getStart().getCol());
+          const fix = EditHelper.deleteRange(file, start, end);
+          const issue = Issue.atRowRange(file, start.getRow(),
+                                         start.getCol(), end.getCol(),
+                                         this.getMessage(), this.getMetadata().key, this.conf.severity, fix);
+          issues.push(issue);
+        }
+        prev = token;
+      }
     }
 
     return issues;
