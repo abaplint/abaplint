0466990a0b2d98ad8606ad01fd9e673c840ef786
Lars Hvam <larshp@hotmail.com>
2024-12-11 10:18:18 +0100

    bugfix

--- packages/core/src/abap/2_statements/statements/field.ts
+++ packages/core/src/abap/2_statements/statements/field.ts
@@ -1,13 +1,13 @@
 import {IStatement} from "./_statement";
 import {verNot, seq, opt, alt} from "../combi";
-import {FieldSub, FormName} from "../expressions";
+import {FieldChain, FormName} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class Field implements IStatement {
 
   public getMatcher(): IStatementRunnable {
-    const ret = seq("FIELD", FieldSub, "MODULE", FormName, opt(alt("ON INPUT", "ON REQUEST")));
+    const ret = seq("FIELD", FieldChain, "MODULE", FormName, opt(alt("ON INPUT", "ON REQUEST")));
 
     return verNot(Version.Cloud, ret);
   }
