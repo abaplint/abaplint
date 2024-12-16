251b77e9f342f7563cf2101a5cea622d887601e8
Lars Hvam <larshp@hotmail.com>
2024-12-16 10:16:44 +0100

    fix syntax

--- packages/core/src/cds/expressions/cds_case.ts
+++ packages/core/src/cds/expressions/cds_case.ts
@@ -1,13 +1,13 @@
 import {CDSArithmetics, CDSCast, CDSCondition, CDSFunction, CDSName, CDSString} from ".";
-import {alt, altPrio, Expression, opt, optPrio, plus, seq} from "../../abap/2_statements/combi";
+import {alt, altPrio, Expression, opt, optPrio, plusPrio, seq} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSCase extends Expression {
   public getRunnable(): IStatementRunnable {
     const name = seq(CDSName, optPrio(seq(".", CDSName)));
     const value = alt(name, CDSString, CDSFunction, CDSCase, CDSCast, CDSArithmetics);
-    const simple = seq("CASE", alt(name, CDSFunction), plus(seq("WHEN", value, "THEN", value)), "ELSE", value, "END");
-    const complex = seq("CASE", plus(seq("WHEN", CDSCondition, "THEN", value)), opt(seq("ELSE", value)), "END");
+    const simple = seq("CASE", alt(name, CDSFunction), plusPrio(seq("WHEN", value, "THEN", value)), "ELSE", value, "END");
+    const complex = seq("CASE", plusPrio(seq("WHEN", CDSCondition, "THEN", value)), opt(seq("ELSE", value)), "END");
     return altPrio(simple, complex);
   }
 }
