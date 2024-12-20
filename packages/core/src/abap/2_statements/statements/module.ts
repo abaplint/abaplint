0f676efc050d298b413ccb658f48f662fe617360
Lars Hvam <larshp@hotmail.com>
2024-12-20 07:18:28 +0100

    MODULE

--- packages/core/src/abap/2_statements/statements/module.ts
+++ packages/core/src/abap/2_statements/statements/module.ts
@@ -1,15 +1,17 @@
 import {IStatement} from "./_statement";
 import {verNot, seq, alt, opt} from "../combi";
-import {FormName} from "../expressions";
+import {FormName, NamespaceSimpleName} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class Module implements IStatement {
 
   public getMatcher(): IStatementRunnable {
+    const sw = seq("SWITCH", NamespaceSimpleName);
+
     const ret = seq("MODULE",
                     FormName,
-                    opt(alt("INPUT", "OUTPUT", "ON CHAIN-REQUEST")));
+                    opt(alt("INPUT", "OUTPUT", "ON CHAIN-REQUEST", "ON CHAIN-INPUT", sw)));
 
     return verNot(Version.Cloud, ret);
   }
