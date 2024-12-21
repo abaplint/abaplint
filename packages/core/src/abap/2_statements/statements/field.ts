3ca0c44b72020c2833ade60dcf113c7032070556
Lars Hvam <larshp@hotmail.com>
2024-12-21 12:31:50 +0100

    fix

--- packages/core/src/abap/2_statements/statements/field.ts
+++ packages/core/src/abap/2_statements/statements/field.ts
@@ -1,13 +1,16 @@
 import {IStatement} from "./_statement";
-import {verNot, seq, opt, alt} from "../combi";
+import {verNot, seq, opt, alt, tok} from "../combi";
 import {FieldChain, FormName} from "../expressions";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
+import {ParenRightW, WParenLeft} from "../../1_lexer/tokens";
 
 export class Field implements IStatement {
 
   public getMatcher(): IStatementRunnable {
-    const ret = seq("FIELD", FieldChain, opt(seq("MODULE", FormName, opt(alt("ON INPUT", "ON REQUEST")))));
+    const module = seq("MODULE", FormName, opt(alt("ON INPUT", "ON REQUEST")));
+    const values = seq("VALUES", tok(WParenLeft), "BETWEEN '0' AND '3'", tok(ParenRightW));
+    const ret = seq("FIELD", FieldChain, opt(alt(module, values)));
 
     return verNot(Version.Cloud, ret);
   }
