c5e983eacd53a0d303f7e99c34c1c3baa504f53e
Lars Hvam <larshp@hotmail.com>
2025-01-08 20:05:30 +0100

    fix MODIFY

--- packages/core/src/abap/2_statements/statements/modify_internal.ts
+++ packages/core/src/abap/2_statements/statements/modify_internal.ts
@@ -24,7 +24,7 @@ export class ModifyInternal implements IStatement {
 
     const long = seq("MODIFY", opt("TABLE"), target, opt(options), opt(additions));
 
-    const simple = seq("MODIFY TABLE", target, from, opt(using));
+    const simple = seq("MODIFY TABLE", target, per(from, using));
 
     return alt(long, simple);
   }
