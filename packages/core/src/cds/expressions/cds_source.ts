415c77dffc67afb71ff0f3a0b29126a51a3346d0
Lars Hvam <larshp@hotmail.com>
2025-01-13 09:40:34 +0100

    wip

--- packages/core/src/cds/expressions/cds_source.ts
+++ packages/core/src/cds/expressions/cds_source.ts
@@ -1,9 +1,9 @@
 import {CDSAs, CDSName, CDSParametersSelect} from ".";
-import {Expression, opt, seq} from "../../abap/2_statements/combi";
+import {Expression, optPrio, seq} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSSource extends Expression {
   public getRunnable(): IStatementRunnable {
-    return seq(CDSName, opt(CDSParametersSelect), opt(CDSAs));
+    return seq(CDSName, optPrio(CDSParametersSelect), optPrio(CDSAs));
   }
 }
