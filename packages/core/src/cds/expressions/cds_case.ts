e22f55691ac5223c34fbfbf7e1cd7f37054ad4de
Lars Hvam <larshp@hotmail.com>
2025-01-17 16:42:43 +0100

    refactor

--- packages/core/src/cds/expressions/cds_case.ts
+++ packages/core/src/cds/expressions/cds_case.ts
@@ -6,8 +6,8 @@ export class CDSCase extends Expression {
   public getRunnable(): IStatementRunnable {
     const name = seq(CDSName, optPrio(seq(".", CDSName)));
     const value = altPrio(CDSFunction, CDSString, CDSCase, CDSCast, CDSArithmetics, name);
-    const simple = seq("CASE", altPrio(CDSFunction, name), plusPrio(seq("WHEN", value, "THEN", value)), "ELSE", value, "END");
-    const complex = seq("CASE", plusPrio(seq("WHEN", CDSCondition, "THEN", value)), optPrio(seq("ELSE", value)), "END");
-    return altPrio(simple, complex);
+    const simple = seq(altPrio(CDSFunction, name), plusPrio(seq("WHEN", value, "THEN", value)), "ELSE", value, "END");
+    const complex = seq(plusPrio(seq("WHEN", CDSCondition, "THEN", value)), optPrio(seq("ELSE", value)), "END");
+    return seq("CASE", altPrio(complex, simple));
   }
 }
