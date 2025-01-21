98e00a0603c8c6caac6b8f829132a2d8212b5ced
Lars HVam <larshp@hotmail.com>
2025-01-21 13:13:04 +0100

    fix READ TABLE

--- packages/core/src/abap/2_statements/expressions/field_sub.ts
+++ packages/core/src/abap/2_statements/expressions/field_sub.ts
@@ -2,11 +2,13 @@ import {seq, opt, starPrio, tok, regex as reg, Expression} from "../combi";
 import {Dash} from "../../1_lexer/tokens";
 import {IStatementRunnable} from "../statement_runnable";
 import {TableBody} from "./table_body";
+import {FieldLength} from "./field_length";
 
 export class FieldSub extends Expression {
   public getRunnable(): IStatementRunnable {
     const ret = seq(reg(/^\*?!?&?(\/\w+\/)?[a-zA-Z_%$\?][\w%$\$\*]*$/),
                     starPrio(seq(tok(Dash), reg(/^[\w%$\$\*]+$/))),
+                    opt(FieldLength),
                     opt(TableBody));
 
     return ret;
