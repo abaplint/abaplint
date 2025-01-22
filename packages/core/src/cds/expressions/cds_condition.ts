dc4d00ee98a8b48a0cba42a37b5ad6a12ea60acc
Lars HVam <larshp@hotmail.com>
2025-01-22 20:14:47 +0100

    BETWEEN

--- packages/core/src/cds/expressions/cds_condition.ts
+++ packages/core/src/cds/expressions/cds_condition.ts
@@ -8,9 +8,10 @@ export class CDSCondition extends Expression {
     const name = seq(CDSName, optPrio(seq(".", altPrio(CDSString, CDSName))));
     const left = altPrio(CDSString, CDSFunction, name);
     const operators = altPrio("=", seq("!", "="), seq("<", ">"), seq(">", "="), seq("<", "="), "<", ">", "LIKE", "NOT LIKE");
-    const compare = seq(left, operators, altPrio(left, CDSInteger));
-    const is = seq(left, "IS", optPrio("NOT"), altPrio("INITIAL", "NULL"));
-    const condition = seq(optPrio("NOT"), altPrio(compare, is));
+    const compare = seq(operators, altPrio(left, CDSInteger));
+    const is = seq("IS", optPrio("NOT"), altPrio("INITIAL", "NULL"));
+    const between = seq("BETWEEN", left, "AND", left);
+    const condition = seq(optPrio("NOT"), left, altPrio(compare, is, between));
     const paren = seq("(", CDSCondition, ")");
     return seq(altPrio(condition, paren), starPrio(seq(altPrio("AND", "OR"), altPrio(condition, paren))));
   }
