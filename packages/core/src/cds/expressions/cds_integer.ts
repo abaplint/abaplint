5557db0292ff2afd1156c237bbf0d5b952f369fd
Lars HVam <larshp@hotmail.com>
2025-01-27 14:04:05 +0100

    fix negative integer

--- packages/core/src/cds/expressions/cds_integer.ts
+++ packages/core/src/cds/expressions/cds_integer.ts
@@ -1,8 +1,8 @@
-import {Expression, regex} from "../../abap/2_statements/combi";
+import {Expression, opt, regex, seq} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSInteger extends Expression {
   public getRunnable(): IStatementRunnable {
-    return regex(/^\d+$/);
+    return seq(opt("-"), regex(/^\d+$/));
   }
 }
