c7579554ccbc54ac7065f0ea2ad7c8f8ec15c084
Lars Hvam <larshp@hotmail.com>
2025-04-15 10:19:09 +0200

    add testcase

--- packages/core/test/rules/no_chained_assignment.ts
+++ packages/core/test/rules/no_chained_assignment.ts
@@ -1,5 +1,9 @@
 import {NoChainedAssignment} from "../../src/rules";
-import {testRule} from "./_utils";
+import {testRule, testRuleFixSingle} from "./_utils";
+
+function testFix(input: string, expected: string, noIssuesAfter = true) {
+  testRuleFixSingle(input, expected, new NoChainedAssignment(), undefined, undefined, noIssuesAfter);
+}
 
 const tests = [
   {abap: `parser error`, cnt: 0},
@@ -9,3 +13,16 @@ var1 = var3.`, cnt: 0},
 ];
 
 testRule(tests, NoChainedAssignment);
+
+describe.only("Rule: no_chained_assignment", () => {
+
+  it("quick fix 1", async () => {
+    const abap = `
+var1 = var2 = var3.`;
+    const expected = `
+var2 = var3.
+var1 = var3.`;
+    testFix(abap, expected, false);
+  });
+
+});
