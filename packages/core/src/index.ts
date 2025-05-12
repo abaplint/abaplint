dbd9eeed8e3310099d474bc588374dc5be0bc6ea
Lars Hvam <larshp@hotmail.com>
2025-05-12 14:14:12 +0200

    update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -58,8 +58,6 @@ import {CyclomaticComplexityStats} from "./utils/cyclomatic_complexity_stats";
 import {SkipLogic} from "./skip_logic";
 import {Diagnostics} from "./lsp/diagnostics";
 import {LSPEdit} from "./lsp/_edit";
-import {StatementFlow} from "./abap/flow/statement_flow";
-import {FlowGraph, FLOW_EDGE_TYPE} from "./abap/flow/flow_graph";
 
 // do not include this file from anywhere within abaplint
 // https://github.com/abaplint/abaplint/issues/873
@@ -74,7 +72,7 @@ export {MemoryFile, Issue, Config, Version, ReferenceType, CyclomaticComplexityS
   IClassDefinition, IInterfaceDefinition, IMethodLengthResult, VirtualPosition, IObject,
   AbstractType, TypedIdentifier, BasicTypes, ScopeType, INode, AbstractToken as Token, IEdit,
   IDependency, AbstractFile, SpaghettiScopeNode, applyEditSingle, applyEditList,
-  MacroCall, MacroContent, NativeSQL, StatementFlow, FlowGraph, FLOW_EDGE_TYPE,
+  MacroCall, MacroContent, NativeSQL,
   IMethodDefinition, DDLParser, LanguageServerTypes, CDSParser, ExpressionsCDS,
   Tokens, ABAPObject, SyntaxLogic, SpaghettiScope, IdentifierMeta, RulesRunner,
   ABAPFile, CurrentScope, IRegistry, Position, PrettyPrinter, Renamer as Rename,
