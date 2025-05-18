f723e31de604266ade27c16c0a945f8daa0610d8
Lars Hvam <larshp@hotmail.com>
2025-05-18 14:17:40 +0200

    bugfix

--- packages/core/src/abap/2_statements/expressions/table_body.ts
+++ packages/core/src/abap/2_statements/expressions/table_body.ts
@@ -1,10 +1,10 @@
-import {seq, tok, Expression} from "../combi";
-import {BracketLeft, BracketRightW} from "../../1_lexer/tokens";
+import {seq, tok, Expression, altPrio} from "../combi";
+import {BracketLeft, BracketRight, BracketRightW} from "../../1_lexer/tokens";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class TableBody extends Expression {
   public getRunnable(): IStatementRunnable {
-    const ret = seq(tok(BracketLeft), tok(BracketRightW));
+    const ret = seq(tok(BracketLeft), altPrio(tok(BracketRightW), tok(BracketRight)));
     return ret;
   }
 }
