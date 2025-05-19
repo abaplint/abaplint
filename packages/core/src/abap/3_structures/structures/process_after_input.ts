75d47b3b43b41bc88e22e09126b580aba5c06193
Lars Hvam <larshp@hotmail.com>
2025-05-19 16:42:38 +0200

    fix

--- packages/core/src/abap/3_structures/structures/process_after_input.ts
+++ packages/core/src/abap/3_structures/structures/process_after_input.ts
@@ -3,11 +3,17 @@ import * as Statements from "../../2_statements/statements";
 import {alt, seq, sta, star, sub} from "./_combi";
 import {IStructureRunnable} from "./_structure_runnable";
 import {Chain} from "./chain";
+import {Loop} from "./loop";
 
 export class ProcessAfterInput implements IStructure {
 
   public getMatcher(): IStructureRunnable {
-    const pai = star(alt(sta(Statements.Module), sta(Statements.Field), sta(Statements.CallSubscreen), sub(Chain)));
+    const pai = star(alt(
+      sta(Statements.Module),
+      sta(Statements.Field),
+      sta(Statements.CallSubscreen),
+      sub(Chain),
+      sub(Loop)));
 
     return seq(
       sta(Statements.ProcessAfterInput),
