022b678def60755b6444297f3d960b47f1e3f773
Lars Hvam <larshp@hotmail.com>
2025-05-19 07:01:26 +0200

    fix

--- packages/core/src/abap/2_statements/expressions/sql_hints.ts
+++ packages/core/src/abap/2_statements/expressions/sql_hints.ts
@@ -4,7 +4,7 @@ import {IStatementRunnable} from "../statement_runnable";
 
 export class SQLHints extends Expression {
   public getRunnable(): IStatementRunnable {
-    const type = altPrio("ORACLE", "ADABAS", "AS400", "DB2", "HDB", "MSSQLNT", "SYBASE", "DB6");
+    const type = altPrio("ORACLE", "ADABAS", "AS400", "DB2", "HDB", "MSSQLNT", "SYBASE", "DB6", "INFORMIX");
     const ret = seq("%_HINTS", plus(seq(type, alt(Constant, FieldSub))));
     return ret;
   }
