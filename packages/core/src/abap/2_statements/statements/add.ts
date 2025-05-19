f989a28d874bf36d2416a59a44b248aff4da81d6
Lars Hvam <larshp@hotmail.com>
2025-05-19 09:59:29 +0200

    fix

--- packages/core/src/abap/2_statements/statements/add.ts
+++ packages/core/src/abap/2_statements/statements/add.ts
@@ -1,15 +1,15 @@
 import {IStatement} from "./_statement";
-import {seq, altPrio, alt, opt} from "../combi";
-import {Target, Source, FieldSub} from "../expressions";
+import {seq, altPrio, per, opt} from "../combi";
+import {Target, Source} from "../expressions";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class Add implements IStatement {
 
   public getMatcher(): IStatementRunnable {
     const to = seq("TO", Target);
-    const accordingTo = seq("ACCORDING TO", FieldSub);
-    const giving = seq("GIVING", FieldSub, opt(accordingTo));
-    const then = seq("THEN", FieldSub, "UNTIL", FieldSub, alt(giving, to));
+    const accordingTo = seq("ACCORDING TO", Source);
+    const giving = seq("GIVING", Source);
+    const then = seq("THEN", Source, "UNTIL", Source, opt(per(giving, accordingTo)), opt(to));
 
     const ret = seq("ADD", Source, altPrio(to, then));
 
