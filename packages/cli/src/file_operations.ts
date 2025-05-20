e407123f3f244fb5461b28d84ee6fe18568b2a6a
Lars Hvam <larshp@hotmail.com>
2025-05-20 10:28:12 +0200

    test

--- packages/cli/src/file_operations.ts
+++ packages/cli/src/file_operations.ts
@@ -1,9 +1,11 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as zlib from "zlib";
-import * as glob from "glob";
-import {IProgress, IFile, MemoryFile} from "@abaplint/core";
 import {CompressedFile} from "./compressed_file";
+import {IProgress, IFile, MemoryFile} from "@abaplint/core";
+import * as fs from "node:fs";
+import * as glob from "glob";
+import * as os from "node:os";
+import * as path from "node:path";
+import * as pLimit from "p-limit";
+import * as zlib from "node:zlib";
 
 export class FileOperations {
 
@@ -36,10 +38,36 @@ export class FileOperations {
   }
 
   public static async loadFiles(compress: boolean | undefined, input: string[], bar: IProgress): Promise<IFile[]> {
-    const files: IFile[] = [];
+//    const files: IFile[] = [];
 
+    let concurrency = os.cpus().length;
+    if (concurrency > 8) {
+      concurrency = 8;
+    } else if (concurrency < 1) {
+      concurrency = 1;
+    }
+
+    const limit = pLimit(10);
+    console.dir(limit);
+
+    input = input.filter((filename) => {
+      const base = filename.split("/").reverse()[0];
+      if (base.split(".").length <= 2) {
+        return false;
+      }
+      return true;
+    });
     bar.set(input.length, "Reading files");
 
+    const promises = input.map((filename) => {
+      return limit(async () => {
+        bar.tick("Reading files - " + path.basename(filename));
+        return this.readFile(filename, compress);
+      });
+    });
+    const files = await Promise.all(promises);
+
+    /*
     for (const filename of input) {
       bar.tick("Reading files - " + path.basename(filename));
 
@@ -50,6 +78,7 @@ export class FileOperations {
 
       files.push(await this.readFile(filename, compress));
     }
+      */
     return files;
   }
 
