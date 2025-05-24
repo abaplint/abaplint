77870bf28f3cbfcf704cee479c305794e6ffa828
Lars Hvam <larshp@hotmail.com>
2025-05-24 14:49:25 +0200

    fix

--- packages/cli/src/file_operations.ts
+++ packages/cli/src/file_operations.ts
@@ -19,7 +19,7 @@ export class FileOperations {
   }
 
   public static loadFileNames(arg: string, error = true): string[] {
-    const files = glob.sync(arg, {nosort: true, nodir: true});
+    const files = glob.sync(arg, {nodir: true});
     if (files.length === 0 && error) {
       throw "Error: No files found";
     }
