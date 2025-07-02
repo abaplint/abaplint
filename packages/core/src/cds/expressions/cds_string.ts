4e018f195812846666c7c15f572f4f6acbd3a290
Lars Hvam <larshp@hotmail.com>
2025-07-02 08:19:14 +0200

    bugfix

--- packages/core/src/cds/expressions/cds_string.ts
+++ packages/core/src/cds/expressions/cds_string.ts
@@ -4,6 +4,6 @@ import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 export class CDSString extends Expression {
   public getRunnable(): IStatementRunnable {
     // https://stackoverflow.com/a/57754227
-    return regex(/^'[A-Za-zÀ-ž\u0370-\u03FF\u0400-\u04FF: -_]*'$/);
+    return regex(/^'[A-Za-zÀ-ž\u0370-\u03FF\u0400-\u04FF:\| -_]*'$/);
   }
 }
