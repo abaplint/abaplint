2d52fff23dc3950cfe280509f2b7a97bc51542f3
Lars Hvam <larshp@hotmail.com>
2025-07-09 08:28:13 +0200

    refactor

--- packages/core/src/utils/_include_graph.ts
+++ packages/core/src/utils/_include_graph.ts
@@ -2,7 +2,6 @@ import {Issue} from "../issue";
 import {IFile} from "../files/_ifile";
 
 export interface IIncludeGraph {
-  getIssues(): Issue[];
   listMainForInclude(filename: string): string[];
   getIssuesFile(file: IFile): Issue[]
 }
