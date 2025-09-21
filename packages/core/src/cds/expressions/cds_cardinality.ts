8ce1311d3812c35e3f6bd9dbf16ecdad92b9e257
Lars HVam <larshp@hotmail.com>
2025-09-21 09:51:35 +0200

    fix

--- packages/core/src/cds/expressions/cds_cardinality.ts
+++ packages/core/src/cds/expressions/cds_cardinality.ts
@@ -3,7 +3,9 @@ import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSCardinality extends Expression {
   public getRunnable(): IStatementRunnable {
-    const cardinality = seq("[", alt("0", "1", "*"), opt(seq(".", ".", alt("0", "1", "*"))), "]");
-    return cardinality;
+    const numeric = seq("[", alt("0", "1", "*"), opt(seq(".", ".", alt("0", "1", "*"))), "]");
+    const num = alt("ONE", "MANY");
+    const text = seq("OF", num, "TO", num);
+    return alt(numeric, text);
   }
 }
