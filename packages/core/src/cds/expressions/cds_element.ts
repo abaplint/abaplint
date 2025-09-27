98c86c12ca54c5766d5674de58ee73a314616eb3
Lars Hvam <5888506+larshp@users.noreply.github.com>
2025-09-27 10:38:56 +0200

    2.113.216: bugfixes (#3712)

--- packages/core/src/cds/expressions/cds_element.ts
+++ packages/core/src/cds/expressions/cds_element.ts
@@ -1,12 +1,12 @@
 import {CDSAggregate, CDSAnnotation, CDSArithmetics, CDSCase, CDSFunction, CDSInteger, CDSName, CDSPrefixedName, CDSString, CDSType} from ".";
-import {Expression, opt, optPrio, seq, alt, starPrio, altPrio} from "../../abap/2_statements/combi";
+import {Expression, optPrio, seq, alt, starPrio, altPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 import {CDSAs} from "./cds_as";
 import {CDSCast} from "./cds_cast";
 
 export class CDSElement extends Expression {
   public getRunnable(): IStatementRunnable {
-    const redirected = seq(": REDIRECTED TO", opt(alt("PARENT", "COMPOSITION CHILD")), CDSName);
+    const redirected = seq(": REDIRECTED TO", optPrio(altPrio("PARENT", "COMPOSITION CHILD")), CDSName);
     const colonThing = seq(":", alt(CDSName, CDSType, "LOCALIZED"));
 
     return seq(starPrio(CDSAnnotation),
@@ -18,8 +18,8 @@ export class CDSElement extends Expression {
                        CDSCast,
                        CDSCase,
                        seq("(", CDSCase, ")"),
-                       seq(CDSPrefixedName, opt(CDSAs), opt(alt(redirected, colonThing))),
+                       seq(CDSPrefixedName, optPrio(CDSAs), optPrio(altPrio(redirected, colonThing))),
                        CDSInteger),
-               opt(CDSAs));
+               optPrio(CDSAs));
   }
 }
