7c614f7fc121d8aede66f0643e6333c7cef6c2c8
Lars HVam <larshp@hotmail.com>
2025-09-28 09:51:15 +0200

    fix

--- packages/core/src/abap/2_statements/expressions/association_name.ts
+++ packages/core/src/abap/2_statements/expressions/association_name.ts
@@ -3,6 +3,6 @@ import {IStatementRunnable} from "../statement_runnable";
 
 export class AssociationName extends Expression {
   public getRunnable(): IStatementRunnable {
-    return reg(/^\\_[\w]+$/);
+    return reg(/^(\\_[\w]+)+$/);
   }
 }
