c25c7cabea2f7d3b6b0f2d5a8baf272a7c7aa483
Lars Hvam <larshp@hotmail.com>
2025-10-06 06:57:13 +0200

    fix

--- packages/core/src/cds/expressions/cds_function_input.ts
+++ packages/core/src/cds/expressions/cds_function_input.ts
@@ -1,11 +1,11 @@
-import {CDSArithmetics, CDSCase, CDSCast, CDSFunction, CDSInteger, CDSName, CDSParameters, CDSString} from ".";
+import {CDSAggregate, CDSArithmetics, CDSCase, CDSCast, CDSFunction, CDSInteger, CDSName, CDSParameters, CDSString} from ".";
 import {altPrio, Expression, opt, seq, starPrio} from "../../abap/2_statements/combi";
 import {IStatementRunnable} from "../../abap/2_statements/statement_runnable";
 
 export class CDSFunctionInput extends Expression {
   public getRunnable(): IStatementRunnable {
     const qualified = seq(CDSName, opt(CDSParameters), starPrio(seq(".", CDSName, opt(CDSParameters))));
-    const input = altPrio(CDSArithmetics, CDSCast, CDSFunction, CDSCase, CDSString, qualified, CDSInteger);
+    const input = altPrio(CDSArithmetics, CDSAggregate, CDSCast, CDSFunction, CDSCase, CDSString, qualified, CDSInteger);
 
     return input;
   }
