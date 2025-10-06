d0e881a724e6bbf5f11a8167f44137ba01391ecf
Lars Hvam <larshp@hotmail.com>
2025-10-06 17:32:24 +0200

    bugfix

--- packages/core/test/abap/statements/check.ts
+++ packages/core/test/abap/statements/check.ts
@@ -5,6 +5,7 @@ const tests = [
   "CHECK lv_var >< 'ABCD'.",
   "CHECK selpernr.",
   "CHECK foo-bar.",
+  `CHECK <lt>[] IS ASSIGNED AND <lt>[] IS NOT INITIAL.`,
 ];
 
 statementType(tests, "CHECK", Statements.Check);
