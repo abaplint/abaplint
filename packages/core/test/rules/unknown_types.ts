820d21f4c4d45983986d933817e58e4c17bc5553
Lars Hvam <larshp@hotmail.com>
2025-10-25 15:40:54 +0200

    add testcase

--- packages/core/test/rules/unknown_types.ts
+++ packages/core/test/rules/unknown_types.ts
@@ -2432,4 +2432,13 @@ DATA lr_ref TYPE REF TO ty_foo.`;
     expect(issues[0]?.getMessage()).to.equal(undefined);
   });
 
+  it.only("sy datum sub", () => {
+    const abap = `
+DATA foo LIKE sy-datum(4).`;
+    let issues = runMulti([{filename: "zfoobar.prog.abap", contents: abap}]);
+    issues = issues.filter(i => i.getKey() === key);
+    console.dir(issues);
+    expect(issues[0]?.getMessage()).to.equal(undefined);
+  });
+
 });
