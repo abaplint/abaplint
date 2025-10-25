7eba1a4534c372ddf6c0b91bc41bb86a38788696
Lars Hvam <larshp@hotmail.com>
2025-10-25 16:11:46 +0200

    fix

--- packages/core/src/abap/5_syntax/statements/fieldsymbol.ts
+++ packages/core/src/abap/5_syntax/statements/fieldsymbol.ts
@@ -5,19 +5,27 @@ import {BasicTypes} from "../basic_types";
 import {UnknownType} from "../../types/basic/unknown_type";
 import {StatementSyntax} from "../_statement_syntax";
 import {SyntaxInput} from "../_syntax_input";
+import {VoidType} from "../../types/basic";
 
 export class FieldSymbol implements StatementSyntax {
   public runSyntax(node: StatementNode, input: SyntaxInput): void {
     const fsname = node.findFirstExpression(Expressions.FieldSymbol)?.getFirstToken();
+    if (fsname === undefined) {
+      return;
+    }
+
+    if (node.getChildren().length === 5) {
+      // no type specified
+      input.scope.addIdentifier(new TypedIdentifier(fsname, input.filename, VoidType.get("FS-SIMPLE")));
+      return;
+    }
 
     const bfound = new BasicTypes(input).parseType(node);
-    if (bfound && fsname) {
+    if (bfound) {
       input.scope.addIdentifier(new TypedIdentifier(fsname, input.filename, bfound));
       return;
     }
 
-    if (fsname) {
-      input.scope.addIdentifier(new TypedIdentifier(fsname, input.filename, new UnknownType("Fieldsymbol, fallback")));
-    }
+    input.scope.addIdentifier(new TypedIdentifier(fsname, input.filename, new UnknownType("Fieldsymbol, fallback")));
   }
 }
