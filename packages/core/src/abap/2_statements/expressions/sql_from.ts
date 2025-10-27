7de363846f05bdc4b0220d0926d887feade7a673
Lars Hvam <larshp@hotmail.com>
2025-10-27 20:02:08 +0100

    wip

--- packages/core/src/abap/2_statements/expressions/sql_from.ts
+++ packages/core/src/abap/2_statements/expressions/sql_from.ts
@@ -1,15 +1,29 @@
-import {seq, optPrio, starPrio, tok, Expression} from "../combi";
+import {seq, starPrio, optPrio, tok, Expression, altPrio} from "../combi";
 import {WParenLeftW, WParenRightW} from "../../1_lexer/tokens";
 import {SQLJoin, SQLFromSource} from ".";
 import {IStatementRunnable} from "../statement_runnable";
 
 export class SQLFrom extends Expression {
   public getRunnable(): IStatementRunnable {
-    const from = seq("FROM",
-                     starPrio(tok(WParenLeftW)),
-                     SQLFromSource);
+    // Match FROM with 0, 1, 2, or 3 levels of opening parens
+    // and the corresponding closing parens
+    const from0 = seq("FROM", SQLFromSource);
+    const from1 = seq("FROM", tok(WParenLeftW), SQLFromSource);
+    const from2 = seq("FROM", tok(WParenLeftW), tok(WParenLeftW), SQLFromSource);
+    const from3 = seq("FROM", tok(WParenLeftW), tok(WParenLeftW), tok(WParenLeftW), SQLFromSource);
 
-    const source = seq(from, starPrio(seq(optPrio(tok(WParenRightW)), SQLJoin, optPrio(tok(WParenRightW)))));
+    const joins = starPrio(seq(optPrio(tok(WParenRightW)), SQLJoin));
+
+    const close1 = seq(joins, tok(WParenRightW));
+    const close2 = seq(joins, tok(WParenRightW), tok(WParenRightW));
+    const close3 = seq(joins, tok(WParenRightW), tok(WParenRightW), tok(WParenRightW));
+
+    const source = altPrio(
+      seq(from3, close3),
+      seq(from2, close2),
+      seq(from1, close1),
+      seq(from0, joins)
+    );
 
     return source;
   }
--- packages/core/test/abap/expressions/sql_in.ts
+++ packages/core/test/abap/expressions/sql_in.ts
@@ -4,21 +4,19 @@ import {getTokens} from "../_utils";
 import {Config} from "../../../src/config";
 import {SQLIn} from "../../../src/abap/2_statements/expressions";
 
-describe("Test expression, SQLIn", () => {
+describe.only("Test expression, SQLIn", () => {
 
   it("test1", () => {
     const abap = `IN ( SELECT field FROM ztab )`;
     const tokens = getTokens(abap);
     const match = Combi.Combi.run(new SQLIn().getRunnable(), tokens, Config.getDefault().getVersion());
-//    console.dir(match);
     expect(match).to.not.equal(undefined);
   });
 
-  it.only("test2", () => {
+  it("test2", () => {
     const abap = `IN ( SELECT field FROM ztab AS p JOIN t001w AS t ON kunnr = sdfdfs )`;
     const tokens = getTokens(abap);
     const match = Combi.Combi.run(new SQLIn().getRunnable(), tokens, Config.getDefault().getVersion());
-//    console.dir(match);
     expect(match).to.not.equal(undefined);
   });
 
