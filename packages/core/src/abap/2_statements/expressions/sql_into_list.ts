459fc979202ff5288c1da9ec14f861bb74ee23b7
Florian Dupret <67047285+fdupret@users.noreply.github.com>
2026-03-12 15:05:17 +0100

    SQLIntoList: 702-compliant syntax (#3861)
    Co-authored-by: Lars Hvam <5888506+larshp@users.noreply.github.com>

--- packages/core/src/abap/2_statements/expressions/sql_into_list.ts
+++ packages/core/src/abap/2_statements/expressions/sql_into_list.ts
@@ -1,12 +1,13 @@
-import {seq, Expression, tok, starPrio, altPrio} from "../combi";
+import {seq, Expression, tok, starPrio, altPrio, ver} from "../combi";
 import {SQLTarget} from ".";
 import {IStatementRunnable} from "../statement_runnable";
 import {WParenLeftW} from "../../1_lexer/tokens/wparen_leftw";
 import {WParenLeft} from "../../1_lexer/tokens/wparen_left";
+import {Version} from "../../../version";
 
 export class SQLIntoList extends Expression {
   public getRunnable(): IStatementRunnable {
-    const intoList = seq(altPrio(tok(WParenLeft), tok(WParenLeftW)),
+    const intoList = seq(altPrio(tok(WParenLeft), ver(Version.v740sp02, tok(WParenLeftW))),
                          starPrio(seq(SQLTarget, ",")),
                          SQLTarget,
                          ")");
