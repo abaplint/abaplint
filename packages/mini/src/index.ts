b41d88e565f8dfedbace55816a613462796d104e
Lars HVam <larshp@hotmail.com>
2026-04-01 19:36:40 +0200

    cleanup

--- packages/mini/src/index.ts
+++ packages/mini/src/index.ts
@@ -1,17 +1,12 @@
 import {MemoryFile} from "../../core/src/files/memory_file";
-/*
 import {Lexer} from "../../core/src/abap/1_lexer/lexer";
-*/
 
 export function main(filename: string, code: string) {
-  // const file =
-  new MemoryFile(filename, code);
+  const file = new MemoryFile(filename, code);
   console.log("Running lexer...");
-  /*
   const lexer = new Lexer();
   const result = lexer.run(file);
-  return JSON.stringify("hello world");
-  */
+  return result.file.getFilename();
 }
 
 console.log(main("test.abap", "WRITE 'Hello World'."));
