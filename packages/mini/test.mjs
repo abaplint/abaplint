05539c498195baaf50b561e8853e9bb56eb715a7
Lars HVam <larshp@hotmail.com>
2026-04-01 18:17:39 +0200

    wip

--- packages/mini/test.mjs
+++ packages/mini/test.mjs
@@ -4,11 +4,35 @@ import {initializeABAP} from "./output/init.mjs";
 
 await initializeABAP();
 
+
 /*
 const bundle = fs.readFileSync("bundle.js", "utf-8");
 const result = await zcl_mjs.eval({iv_source: new abap.types.String().set(bundle)});
 */
-const foo = `const str = "hello"; str.something();`;
-const result = await zcl_mjs.eval({iv_source: new abap.types.String().set(foo)});
 
+/*
+const foo = `console.log(foobar);`;
+const result = await zcl_mjs.eval({iv_source: new abap.types.String().set(foo)});
+console.dir(result);
+*/
+
+const foo = `var AbstractFile = class {
+  constructor(filename) {
+    this.filename = filename;
+  }
+  getFilename() {
+    return this.filename;
+  }
+};
+
+var MemoryFile = class extends AbstractFile {
+  constructor(filename, raw) {
+    super(filename);
+    this.raw = raw;
+  }
+};
+
+new MemoryFile("dsf", "sdfs");
+console.log("Done");`;
+const result = await zcl_mjs.eval({iv_source: new abap.types.String().set(foo)});
 console.dir(result);
