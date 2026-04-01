fbebe3ab1bfad4bb217ef1a241c67c10e7d5c9f7
Lars Hvam <larshp@hotmail.com>
2026-04-01 14:59:13 +0200

    wip

--- packages/mini/src/index.ts
+++ packages/mini/src/index.ts
@@ -3,6 +3,7 @@ import {Lexer} from "../../core/src/abap/1_lexer/lexer";
 
 export function main(filename: string, code: string) {
   const file = new MemoryFile(filename, code);
+  console.log("Running lexer...");
   const lexer = new Lexer();
   const result = lexer.run(file);
   return JSON.stringify(result);
