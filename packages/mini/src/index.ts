cb8b63824ee108d836eff6660de459116b42c5d4
Lars HVam <larshp@hotmail.com>
2026-04-05 16:41:49 +0200

    call parser

--- packages/mini/src/index.ts
+++ packages/mini/src/index.ts
@@ -1,12 +1,19 @@
 import {MemoryFile} from "../../core/src/files/memory_file";
 import {Lexer} from "../../core/src/abap/1_lexer/lexer";
+import {StatementParser} from "../../core/src/abap/2_statements/statement_parser";
+import {Version} from "../../core/src/version";
 
 export function main(filename: string, code: string) {
   const file = new MemoryFile(filename, code);
   console.log("Running lexer...");
   const lexer = new Lexer();
-  const result = lexer.run(file);
-  return JSON.stringify(result.tokens);
+  const lexerResult = lexer.run(file);
+
+  console.log("Running statement parser...");
+  const statementParser = new StatementParser(Version.v750);
+  const result = statementParser.run([lexerResult], []);
+
+  return JSON.stringify(result[0].statements);
 }
 
 console.log(main("test.abap", "WRITE 'Hello World'."));
