a71a00448aa8ab6892d0d3bbe151240e85d41118
Lars Hvam <5888506+larshp@users.noreply.github.com>
2026-05-18 14:20:58 +0200

    2.119.21: check names with only digits (#3943)

--- packages/core/src/abap/5_syntax/statements/constant.ts
+++ packages/core/src/abap/5_syntax/statements/constant.ts
@@ -3,8 +3,9 @@ import {StatementNode} from "../../nodes";
 import {BasicTypes} from "../basic_types";
 import {TypedIdentifier, IdentifierMeta} from "../../types/_typed_identifier";
 import {UnknownType} from "../../types/basic";
-import {SyntaxInput} from "../_syntax_input";
+import {CheckSyntaxKey, SyntaxInput, syntaxIssue} from "../_syntax_input";
 import {AssertError} from "../assert_error";
+import {VoidType} from "../../types/basic";
 
 export class Constant {
   public runSyntax(node: StatementNode, input: SyntaxInput): TypedIdentifier {
@@ -13,14 +14,31 @@ export class Constant {
     if (found) {
       const val = basic.findValue(node);
       const meta = [IdentifierMeta.ReadOnly, IdentifierMeta.Static];
+      if (this.isOnlyDigits(found.getName()) && this.allowOnlyDigitsName(node, input) === false) {
+        const message = "not possible to have a name with only digits";
+        input.issues.push(syntaxIssue(input, found.getToken(), message));
+        return new TypedIdentifier(found.getToken(), input.filename, VoidType.get(CheckSyntaxKey), meta, val);
+      }
       return new TypedIdentifier(found.getToken(), input.filename, found.getType(), meta, val);
     }
 
     const fallback = node.findFirstExpression(Expressions.DefinitionName);
     if (fallback) {
+      if (this.isOnlyDigits(fallback.concatTokens()) && this.allowOnlyDigitsName(node, input) === false) {
+        const message = "not possible to have a name with only digits";
+        input.issues.push(syntaxIssue(input, fallback.getFirstToken(), message));
+      }
       return new TypedIdentifier(fallback.getFirstToken(), input.filename, new UnknownType("constant, fallback"));
     }
 
     throw new AssertError("Statement Constant: unexpected structure");
+  }
+
+  private isOnlyDigits(name: string): boolean {
+    return /^[0-9]+$/.test(name);
+  }
+
+  private allowOnlyDigitsName(node: StatementNode, input: SyntaxInput): boolean {
+    return input.scope.isAnyOO() === false && node.getColon() !== undefined;
   }
 }
