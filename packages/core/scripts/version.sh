c0d4ecbd2aaa7f80f1b76496b5f470db2e018c75
Lars HVam <larshp@hotmail.com>
2026-06-11 11:01:40 +0200

    cleanup

--- packages/core/scripts/version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ABAPLINTVER=$(grep version\" package.json | cut -d\" -f4)
-if [ -f build/src/registry.js ]; then
-  sed -i "s|{{ VERSION }}|"$ABAPLINTVER"|" build/src/registry.js
-fi
-if [ -f build/web/src/registry.js ]; then
-  sed -i "s|{{ VERSION }}|"$ABAPLINTVER"|" build/web/src/registry.js
-fi
