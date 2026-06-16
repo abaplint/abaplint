0fec36bba60ef81131facbd75ce15aa571cfbb97
Lars Hvam <larshp@hotmail.com>
2026-06-16 18:23:37 +0200

    simplify

--- packages/core/src/abap/2_statements/expressions/_select_core.ts
+++ packages/core/src/abap/2_statements/expressions/_select_core.ts
@@ -82,12 +82,11 @@ export function buildSelectCore(allowInto = false, allowOrderBy = true): IStatem
   const afterFromWithInto = seq(
     optPrio(SQLFrom), client, byp, fromPackSize,
     altPrio(
-      seq(sqlFields, fae, whereClause, groupHaving, ...orderUpOff, trailingOpts, trailingInto),
       seq(intoForPackSize, optPrio(SQLPackageSize), byp, optPrio(DatabaseConnection),
           optPrio(SQLUpTo), byp, optPrio(offset), fae, whereClause, groupHaving, ...orderUpOff, trailingOpts),
       seq(intoSingle, byp, optPrio(SQLUpTo), byp, fae, optPrio(SQLUpTo), byp,
           optPrio(offset), whereClause, groupHaving, ...orderUpOff, trailingOpts),
-      seq(fae, whereClause, groupHaving, ...orderUpOff, trailingOpts, trailingInto),
+      seq(optPrio(sqlFields), fae, whereClause, groupHaving, ...orderUpOff, trailingOpts, trailingInto),
     ),
   );
 
