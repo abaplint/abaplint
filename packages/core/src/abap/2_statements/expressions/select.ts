168f418dfd7a889e3ce2c1a0d00cae6d21ed73c5
Lars Hvam <larshp@hotmail.com>
2026-06-16 15:35:27 +0200

    bugfix

--- packages/core/src/abap/2_statements/expressions/select.ts
+++ packages/core/src/abap/2_statements/expressions/select.ts
@@ -3,13 +3,16 @@ import {SQLIntoTable, SQLOrderBy, SQLIntoList, SQLSetOp} from ".";
 import {Version} from "../../../version";
 import {IStatementRunnable} from "../statement_runnable";
 import {SQLIntoStructure} from "./sql_into_structure";
-import {buildSelectCore} from "./_select_core";
+import {buildSelectCore, buildSelectSingleCore} from "./_select_core";
 
 export class Select extends Expression {
   public getRunnable(): IStatementRunnable {
     const into = altPrio(SQLIntoTable, SQLIntoStructure, SQLIntoList);
 
-    const standalone = buildSelectCore(true);
+    const standalone = altPrio(
+      seq("SINGLE", optPrio("FOR UPDATE"), buildSelectSingleCore(true)),
+      buildSelectCore(true),
+    );
 
     const unionTail = ver(Version.v750, plusPrio(SQLSetOp), Version.OpenABAP);
     const chained = seq(buildSelectCore(false, false), unionTail, optPrio(SQLOrderBy), optPrio(into));
