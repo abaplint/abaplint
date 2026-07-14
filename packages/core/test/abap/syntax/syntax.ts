1642e7a4000c73a1c774a3f0be0428f20e1f702b
Lars Hvam <larshp@hotmail.com>
2026-07-14 09:15:34 +0200

    add testcase

--- packages/core/test/abap/syntax/syntax.ts
+++ packages/core/test/abap/syntax/syntax.ts
@@ -14198,4 +14198,20 @@ WRITE / to_upper( str(4) ).`;
     expect(issues[0]?.getMessage()).to.equal(undefined);
   });
 
+  it("okay if the value is calculated", () => {
+    const abap = `
+CLASS lcl DEFINITION.
+  PUBLIC SECTION.
+    METHODS foo IMPORTING bar TYPE clike.
+ENDCLASS.
+CLASS lcl IMPLEMENTATION.
+  METHOD foo.
+    DATA lv TYPE string.
+    foo( lv+1 && lv(1) ).
+  ENDMETHOD.
+ENDCLASS.`;
+    const issues = runProgram(abap);
+    expect(issues[0]?.getMessage()).to.equal(undefined);
+  });
+
 });
