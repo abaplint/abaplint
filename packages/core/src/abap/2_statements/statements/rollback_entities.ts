b68570f08c749d76368c4e872182deed78bdd4f5
Lars Hvam <larshp@hotmail.com>
2026-07-30 16:55:10 +0200

    rollback entities

--- packages/core/src/abap/2_statements/statements/rollback_entities.ts
+++ packages/core/src/abap/2_statements/statements/rollback_entities.ts
@@ -1,5 +1,5 @@
 import {IStatement} from "./_statement";
-import {ver, verNotLang} from "../combi";
+import {ver, verNotLang, AlsoIn} from "../combi";
 import {IStatementRunnable} from "../statement_runnable";
 import {Release, LanguageVersion} from "../../../version";
 
@@ -7,7 +7,7 @@ export class RollbackEntities implements IStatement {
 
   public getMatcher(): IStatementRunnable {
     const s = "ROLLBACK ENTITIES";
-    return verNotLang(LanguageVersion.KeyUser, ver(Release.v754, s));
+    return verNotLang(LanguageVersion.KeyUser, ver(Release.v754, s, {also: AlsoIn.OpenABAP}));
   }
 
 }
