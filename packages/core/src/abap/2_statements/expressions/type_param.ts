027583c17c9fc6f2bf21e508d1f18dd17c5fc2c3
Lars Hvam <larshp@hotmail.com>
2026-08-28 12:26:15 +0200

    fix

--- packages/core/src/abap/2_statements/expressions/type_param.ts
+++ packages/core/src/abap/2_statements/expressions/type_param.ts
@@ -1,6 +1,8 @@
-import {seq, optPrio, alt, altPrio, Expression, opt} from "../combi";
-import {Default, FieldChain, TypeNameOrInfer} from ".";
+import {seq, optPrio, alt, altPrio, Expression, opt, ver, AlsoIn} from "../combi";
+import {Default, EntityAssociation, FieldChain, TypeName, TypeNameOrInfer, TypeStructure} from ".";
 import {IStatementRunnable} from "../statement_runnable";
+import {Release} from "../../../version";
+import {derivedTypesAlt} from "./_derived_types";
 
 export class TypeParam extends Expression {
   public getRunnable(): IStatementRunnable {
@@ -17,6 +19,16 @@ export class TypeParam extends Expression {
 
     const like = seq("LIKE", opt("LINE OF"), FieldChain, optPrio(Default));
 
-    return alt(seq("TYPE", alt(table, ret)), like);
+    const entity = alt(TypeName, EntityAssociation);
+    const rapTable = ver(Release.v770,
+                         seq("TYPE", "TABLE FOR", derivedTypesAlt(
+                           ver(Release.v773, seq("ACTION IMPORT", entity), {also: AlsoIn.OpenABAP}),
+                           ver(Release.v774, seq("READ LINK", entity), {also: AlsoIn.OpenABAP}),
+                           ver(Release.v787, seq("EVENT", entity), {also: AlsoIn.OpenABAP}),
+                         ), optPrio("VALUE IS INITIAL")),
+                         {also: AlsoIn.OpenABAP});
+
+    // Method parameters also allow the RAP table and response type forms.
+    return altPrio(rapTable, TypeStructure, seq("TYPE", alt(table, ret)), like);
   }
 }
